8c52f24895cd2994af72980fcc33de550a110ef9
sfiligoi <sfiligoi>
2006-09-19 22:28:19 +0000

    The program is now accepting a config file (see examples)

--- frontend/glideinFrontend.py
+++ frontend/glideinFrontend.py
@@ -1,22 +1,36 @@
-import os,sys,time
+#
+# Description:
+#   This is the main of the glideinFrontend
+#
+# Arguments:
+#   $1 = poll period (in seconds)
+#   $2 = advertize rate even if no changes (every $2 loops)
+#   $3 = config file
+#
+# Author:
+#   Igor Sfiligoi (Sept 19th 2006)
+#
+
+import os
+import sys
+import time
+sys.path.append("../lib")
+
 import glideinFrontendInterface
 import glideinFrontendLib
 
-client_name="testFrontend"
-
 ############################################################
-def iterate_one(factory_pool,
-                schedd_names,match_str,
+def iterate_one(frontend_name,factory_pool,
+                schedd_names,job_constraint,match_str,
                 max_idle,reserve_idle,
                 glidein_params):
 
     glidein_dict=glideinFrontendInterface.findGlideins(factory_pool)
-    condorq_dict=glideinFrontendLib.getIdleCondorQ(schedd_names)
+    condorq_dict=glideinFrontendLib.getIdleCondorQ(schedd_names,job_constraint)
 
     print "Match"
     count_glideins=glideinFrontendLib.countMatchIdle(match_str,condorq_dict,glidein_dict)
 
-    print count_glideins
     for glidename in count_glideins.keys():
         request_name=glidename
 
@@ -31,24 +45,31 @@ def iterate_one(factory_pool,
             glidein_min_idle=0 
 
         print "Advertize %s %i"%(request_name,glidein_min_idle)
-        glideinFrontendInterface.advertizeWork(factory_pool,client_name,request_name,glidename,glidein_min_idle,glidein_params)
+        glideinFrontendInterface.advertizeWork(factory_pool,frontend_name,request_name,glidename,glidein_min_idle,glidein_params)
 
     return
 
 ############################################################
-def iterate(factory_pool,
-            schedd_names,match_str,
+def iterate(sleep_time,
+            frontend_name,factory_pool,
+            schedd_names,job_constraint,match_str,
             max_idle,reserve_idle,
             glidein_params):
     while 1:
         print "Iteration at %s" % time.ctime()
-        done_something=iterate_one(factory_pool,schedd_names,match_str,max_idle,reserve_idle,glidein_params)
+        done_something=iterate_one(frontend_name,factory_pool,schedd_names,job_constraint,match_str,max_idle,reserve_idle,glidein_params)
         print "Sleep"
-        time.sleep(30)
+        time.sleep(sleep_time)
 
 ############################################################
-def main():
-    iterate(None,['sfiligoi@cms-xen6.fnal.gov'],'1',20,5,{"GLIDEIN_Collector":"cms-xen6.fnal.gov"})
+def main(sleep_time,advertize_rate,config_file):
+    config_dict={}
+    execfile(config_file,config_dict)
+    iterate(sleep_time,
+            config_dict['frontend_name'],config_dict['factory_pool'],
+            config_dict['schedd_names'], config_dict['job_constraint'],config_dict['match_string'],
+            20, 5,
+            config_dict['glidein_params'])
 
 ############################################################
 #
@@ -57,5 +78,5 @@ def main():
 ############################################################
 
 if __name__ == '__main__':
-    main()
+    main(int(sys.argv[1]),int(sys.argv[2]),sys.argv[3])
  
