feab78b5d947bd0dad36dac107477cfc832fb46e
sfiligoi <sfiligoi>
2006-09-19 22:27:29 +0000

    Add an optional job constraint

--- frontend/glideinFrontendLib.py
+++ frontend/glideinFrontendLib.py
@@ -13,11 +13,18 @@ import condorMonitor
 # Return a dictionary of schedds containing idle jobs
 # Each element is a condorQ
 #
-def getIdleCondorQ(schedd_names):
+# If not all the jobs of the schedd has to be considered,
+# specify the appropriate constraint
+#
+def getIdleCondorQ(schedd_names,constraint=None):
     out_condorq_dict={}
     for schedd in schedd_names:
         condorq=condorMonitor.CondorQ(schedd)
-        condorq.load("JobStatus==1")
+        idle_constraint="JobStatus==1"
+        if constraint!=None:
+            idle_constraint="(%s) && (%s)"%(idle_constraint,constraint)
+
+        condorq.load(idle_constraint)
         if len(condorq.fetchStored())>0:
             out_condorq_dict[schedd]=condorq
     return out_condorq_dict
