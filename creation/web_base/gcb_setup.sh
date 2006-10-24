78cba525698eb96b810c420067c008f729fe2664
sfiligoi <sfiligoi>
2006-10-24 22:46:13 +0000

    Add support for gcb_broker_query

--- creation/web_base/gcb_setup.sh
+++ creation/web_base/gcb_setup.sh
@@ -17,10 +17,10 @@ function append_config {
     fi    
 }
 
-# return 0 if success
-# return !=0  on recoverable error
-# fail catastrophically on unrecoverable error
-function setup_gcb {
+# return rank (higher is better) if success
+#        in stdout it returns ip:port
+# return -1  on error
+function check_gcb {
     vg_gcb_ip=`echo "$1" | awk -F ":" '{print $1}'`
     if [ -z "$vg_gcb_ip" ]; then
 	echo "GCB name is an empty string!"
@@ -33,9 +33,10 @@ function setup_gcb {
 
     if [ "$vg_gcb_port" -ne "$default_gcb_port" ]; then
 	echo "Non standard port $vg_gcb_port (not $default_gcb_port) not supported!"
-	return 1
+	return -1
     fi
 
+    # check if I can connect to that port
     which nc >/dev/null 2>&1
     ret=$?
     if [ "$ret" -eq 0 ]; then
@@ -44,9 +45,38 @@ function setup_gcb {
 	ret=$?
 	if [ $ret -ne 0 ]; then
 	    echo "Cannot talk to GCB $vg_gcb_ip:$vg_gcb_port via nc"
-	    return 1
+	    return -1
 	fi
     fi
+
+    if [ "$gcb_query_exists" -eq 1 ]; then
+	restr=`./gcb_broker_query $vg_gcb_ip freesockets`
+	ret=$?
+	if [ $ret -ne 0 ]; then
+	    echo "Cannot talk to GCB $vg_gcb_ip:$vg_gcb_port via gcb_broker_query"
+	    return -1
+	fi
+	vg_free_sockets=`echo "$restr" | awk '{print $3}'`
+    else
+	# set a pessimistic guess
+	vg_free_sockets=1
+    fi
+
+    if [ -n "$gcb_min_free" ]; then
+	if [ "$vg_free_sockets" -lt "$gcb_min_free" ]; then
+	    echo "GCB $vg_gcb_ip:$vg_gcb_port has only $vg_free_sockets free, needed $gcb_min_free"
+	    return -1
+	fi
+    fi
+
+    echo "$vg_gcb_ip:$vg_gcb_port"
+    return $vg_free_sockets
+}
+
+# fail catastrophically on unrecoverable error
+function setup_gcb {
+    sg_gcb_ip=`echo "$1" | awk -F ":" '{print $1}'`
+    sg_gcb_port=`echo "$1" | awk -F ":" '{print $2}'`
 
     # configure Condor to use it
     # will exit drastically if fails
@@ -54,11 +84,26 @@ function setup_gcb {
     append_config "BIND_ALL_INTERFACES = true"
     append_config "NET_REMAP_ENABLE = true"
     append_config "NET_REMAP_SERVICE = GCB"
-    append_config "NET_REMAP_INAGENT = $vg_gcb_ip"
+    append_config "NET_REMAP_INAGENT = $sg_gcb_ip"
 
-    echo "Using GCB $vg_gcb_ip:$vg_gcb_port"
+    echo "Using GCB $sg_gcb_ip:$sg_gcb_port"
     return 0
 }
+
+gcb_query_exists=0
+if [ -e "gcb_broker_query" ]; then
+    # may not be executable, after being transfered via wget
+    chmod u+x gcb_broker_query
+    gcb_query_exists=1
+fi
+
+gcb_min_free=`grep '^GCB_MIN_FREE ' $glidein_config | awk '{print $2}'`
+if [ -n "$gcb_min_free" ]; then
+    if [ "$gcb_query_exists" -eq 0 ]; then
+	echo "GCB_MIN_FREE defined, but gcb_broker_query not present." 1>&2
+	exit 1
+    fi
+fi
 
 gcb_list=`grep '^GCB_LIST ' $glidein_config | awk '{print $2}'`
 if [ -z "$gcb_list" ]; then
@@ -79,18 +124,51 @@ fi
 
 
 if [ "$gcb_order" == "RANDOM" ];then
+    ##########################################################
     # random order
+    ##########################################################
     gcb_els=`echo "$gcb_list" | awk 'BEGIN{srand()}{split($0,g,","); for (i in g) print rand() "\t" g[i]}' | sort -n |awk '{print $2}'`
 elif [ "$gcb_order" == "RR" -o "$gcb_order" == "ROUNDROBIN" ]; then
+    ##########################################################
     # round robin, based on the cluster number
+    ##########################################################
     nr1=`grep '^CONDORG_CLUSTER ' $glidein_config | awk '{print $2}'`
     nr2=`grep '^CONDORG_SUBCLUSTER ' $glidein_config | awk '{print $2}'`
     let nr=$nr1+$nr2
     nr_gcb_els=`echo "$gcb_list" | awk '{split($0,g,","); nr=0; for (i in g) nr=nr+1; print nr}'`
     let start_nr=$nr%$nr_gcb_els
     gcb_els=`echo "$gcb_list" | awk "{split(\\\$0,g,\",\"); for (i=1; i<=$nr_gcb_els; i=i+1) print g[((i+$start_nr)%$nr_gcb_els)+1]}"`
+elif [ "$gcb_order" == "GCBLOAD" ]; then
+    ################################################################
+    # will probe all of them and select the one that is less loaded
+    ################################################################
+    if [ "$gcb_query_exists" -eq 0 ]; then
+	echo "GCB_ORDER==GCBLOAD, but gcb_broker_query not present." 1>&2
+	exit 1
+    fi
+    # get array
+    base1_gcb_els=`echo "$gcb_list" | awk '{split($0,g,","); for (i in g) print g[i]}'`
+    # associate number of free slots
+    base2_gcb_els=""
+    for gcb in $base1_gcb_els; do
+	msg=`check_gcb $gcb`
+	score=$?
+	if [ "$score" -ne "-1" ]; then
+	    if [ -z "$base2_gcb_els" ]; then
+		base2_gcb_els="$score $msg"
+	    else
+		base2_gcb_els=`echo "$base2_gcb_els"; echo $msg`
+	    fi
+	else
+	    echo "$msg" 1>&2
+	fi
+    done
+    # sort by number of free slots
+    gcb_els=`echo "$base2_gcb_els" | sort -nr | awk '{print $2}'`
 elif [ "$gcb_order" == "SEQ" -o "$gcb_order" == "SEQUENTIAL" ]; then
+    ##########################################################
     # sequential, first always first
+    ##########################################################
     gcb_els=`echo "$gcb_list" | awk '{split($0,g,","); nr=0; for (i in g) nr=nr+1; for (i=1; i<=nr; i=i+1) print g[i]}'`
 else
     echo "Invalid GCB_ORDER specified ($gcb_order)!" 1>&2
@@ -99,10 +177,10 @@ fi
 
 gcb_configured=0
 for gcb in $gcb_els; do
-    msg=`setup_gcb $gcb`
+    msg=`check_gcb $gcb`
     ret=$?
-    if [ "$ret" -eq 0 ]; then
-	echo "$msg"
+    if [ "$ret" -ne "-1" ]; then
+	setup_gcb "$msg"
 	gcb_configured=1
 	break
     else
