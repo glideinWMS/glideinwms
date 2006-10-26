4f55bd5e57b8aa4685186d0507c8fd082db2d494
sfiligoi <sfiligoi>
2006-10-26 22:39:22 +0000

    Add proper logging

--- frontend/glideinFrontend.py
+++ frontend/glideinFrontend.py
@@ -19,17 +19,18 @@ sys.path.append("../lib")
 
 import glideinFrontendInterface
 import glideinFrontendLib
+import logSupport
 
 ############################################################
 def iterate_one(frontend_name,factory_pool,
                 schedd_names,job_constraint,match_str,
                 max_idle,reserve_idle,
                 glidein_params):
-
+    global activity_log
     glidein_dict=glideinFrontendInterface.findGlideins(factory_pool)
     condorq_dict=glideinFrontendLib.getIdleCondorQ(schedd_names,job_constraint)
 
-    print "Match"
+    activity_log.write("Match")
     count_glideins=glideinFrontendLib.countMatchIdle(match_str,condorq_dict,glidein_dict)
 
     for glidename in count_glideins.keys():
@@ -45,20 +46,28 @@ def iterate_one(frontend_name,factory_pool,
             # no idle, make sure the glideins know it
             glidein_min_idle=0 
 
-        print "Advertize %s %i"%(request_name,glidein_min_idle)
+        activity_log.write("Advertize %s %i"%(request_name,glidein_min_idle))
         glideinFrontendInterface.advertizeWork(factory_pool,frontend_name,request_name,glidename,glidein_min_idle,glidein_params)
 
     return
 
 ############################################################
-def iterate(sleep_time,
+def iterate(log_dir,sleep_time,
             frontend_name,factory_pool,
             schedd_names,job_constraint,match_str,
             max_idle,reserve_idle,
             glidein_params):
+    global activity_log,warning_log
+    activity_log=logSupport.DayLogFile(os.path.join(log_dir,"frontend_info"))
+    warning_log=logSupport.DayLogFile(os.path.join(log_dir,"frontend_err"))
+    cleanupObj=logSupport.DirCleanup(log_dir,
+                                     7*24*3600,
+                                     activity_log,warning_log)
+    
+
     is_first=1
     while 1:
-        print "Iteration at %s" % time.ctime()
+        activity_log.write("Iteration at %s" % time.ctime())
         try:
             done_something=iterate_one(frontend_name,factory_pool,schedd_names,job_constraint,match_str,max_idle,reserve_idle,glidein_params)
         except:
@@ -68,17 +77,17 @@ def iterate(sleep_time,
                 # if not the first pass, just warn
                 tb = traceback.format_exception(sys.exc_info()[0],sys.exc_info()[1],
                                                 sys.exc_info()[2])
-                print "Exception at %s: %s" % (time.ctime(),tb)
+                warning_log.write(print "Exception at %s: %s" % (time.ctime(),tb))
                 
         is_first=0
-        print "Sleep"
+        activity_log.write("Sleep")
         time.sleep(sleep_time)
 
 ############################################################
 def main(sleep_time,advertize_rate,config_file):
     config_dict={}
     execfile(config_file,config_dict)
-    iterate(sleep_time,
+    iterate(config_dict['log_dir'],sleep_time,
             config_dict['frontend_name'],config_dict['factory_pool'],
             config_dict['schedd_names'], config_dict['job_constraint'],config_dict['match_string'],
             20, 5,
