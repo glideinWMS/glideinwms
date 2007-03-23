c343f64d4d1d75f3920b1625d0408eb14096bac1
sfiligoi <sfiligoi>
2007-03-23 14:50:00 +0000

    Small improvements

--- poolwatcher/poolWatcherLog.py
+++ poolwatcher/poolWatcherLog.py
@@ -12,9 +12,11 @@ import time
 event_description={0:"VM registered",
                    1:"VM claimed",
                    4:"VM free",
-                   5:"VM disappeared"}
+                   5:"VM disappeared",
+                   12:"VM in Owner state",
+                   13:"VM left Owner state"}
 
-class watcherLog:
+class watcherLogWriter:
     def __init__(self,basename,ext=".log"):
         self.basename=basename
         self.ext=ext
@@ -24,27 +26,70 @@ class watcherLog:
         self.writeEntry("008 (*) (*) (*) %s Ping"%time_str)
         return
 
-    def logEvent(self, event, VM, factory_name, glidein_name, schedd_name, cluster_id, proc_id,
-                 comments=[]):
+    def logNewVM(self, VM, DaemonStartTime,
+                 vm_attributes={},     # attr_name:value pairs
+                 timestamp=None):   # use current time if None
+        event_comments=[]
+        attr_keys=vm_attributes.keys()
+        attr.keys.sort()
+        for k in attr_keys:
+            event_comments.append("VMAttr %s = %s"%(k,vm_attributes[k]))
+        return self.logEvent(0, VM, DaemonStartTime, event_comments,timestamp)
+    
+    def logClaimed(self, VM, DaemonStartTime,
+                   job_attributes={},     # attr_name:value pairs
+                   timestamp=None):   # use current time if None
+        event_comments=[]
+        attr_keys=job_attributes.keys()
+        attr.keys.sort()
+        for k in attr_keys:
+            event_comments.append("JobAttr %s = %s"%(k,job_attributes[k]))
+        return self.logEvent(1, VM, DaemonStartTime, event_comments,timestamp)
+    
+    def logReleased(self, VM, DaemonStartTime,
+                    last_known_claimed=None, # None means unknown
+                    timestamp=None):         # use current time if None
+        lkc_comments=[]
+        if last_known_claimed!=None:
+            lkc_comments.append("LastKnownClaimed: "+time.strftime("%m/%d %H:%M:%S",time.localtime(last_known_claimed))))
+        return self.logEvent(4, VM, DaemonStartTime,lkc_comments,timestamp)
+
+    def logDisappeared(self, VM, DaemonStartTime,
+                       last_known_existed=None, # None means unknown
+                       timestamp=None):   # use current time if None
+        lke_comments=[]
+        if last_known_existed!=None:
+            lke_comments.append("LastKnownExisted: "+time.strftime("%m/%d %H:%M:%S",time.localtime(last_known_existed))))
+        return self.logEvent(5, VM, DaemonStartTime,lke_comments,timestamp)
+    
+
+    ### PR I V A T E ###
+    def logEvent(self, event, VM, DaemonStartTime,
+                 comments=[],
+                 timestamp=None):
         global event_description
-        time_str=time.strftime("%m/%d %H:%M:%S",time.localtime())
+        if timestamp==None:
+            timestamp=time.time()
+        time_str=time.strftime("%m/%d %H:%M:%S",time.localtime(timestamp))
 
         comment_str=""
         for comment in comments:
             comment_str+="\n    "+comment #start in a new line
-        self.writeEntry("%03i (%s) (%s@%s) (%s#%i.%03i) %s %s%s"%(event,VM,glidein_name,factory_name,schedd_name,cluster_id,proc_id,time_str,event_description[event],comment_str))
+        return self.writeEntry(DaemonStartTime,"%03i (%s#%s) %s %s%s"%(event,VM,DaemonStartTime,time_str,event_description[event],comment_str))
 
-        return
-
-    ### PR I V A T E ###
-    def writeEntry(self,entry):
-        fd=open(self.logName(),"a")
+    def writeEntry(self,DaemonStartTime,entry):
+        fd=open(self.logName(DaemonStartTime),"a")
         try:
             fd.write("%s\n...\n"%entry)
         finally:
             fd.close()
         return
 
-    def logName(self):
-        time_str=time.strftime("%Y%m%d",time.localtime())
+    def logName(self,DaemonStartTime):
+        time_str=time.strftime("%Y%m%d",time.localtime(long(DaemonStartTime)))
         return "%s%s%s"%(self.basename,time_str,self.ext)
+
+class watcherLogReader:
+    def __init__(self,basename,ext=".log"):
+        self.basename=basename
+        self.ext=ext
