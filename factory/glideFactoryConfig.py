aedb04414fb3dd9cb2e2c162e956331ed965c0bb
sfiligoi <sfiligoi>
2007-04-02 20:08:31 +0000

    Bug fix

--- factory/glideFactoryConfig.py
+++ factory/glideFactoryConfig.py
@@ -96,14 +96,14 @@ class JobDescript(EntryConfigFile):
                                  repr) # convert everything in strings
 
 class JobAttributes(JoinConfigFile):
-    def __init__(self,entry_name):
+    def __init__(self):
         global factoryConfig
         JoinConfigFile.__init__(self,entry_name,factoryConfig.job_attrs_file,
                                 lambda s:s) # values are in python format
 
 
 class JobParams(JoinConfigFile):
-    def __init__(self,entry_name):
+    def __init__(self):
         global factoryConfig
         JoinConfigFile.__init__(self,entry_name,factoryConfig.job_params_file,
                                 lambda s:s) # values are in python format
