20ef3aed661d5b77bedf48df9ed6917e24319c01
sfiligoi <sfiligoi>
2007-10-04 19:03:55 +0000

    Fix typo

--- factory/glideFactoryLogParser.py
+++ factory/glideFactoryLogParser.py
@@ -13,6 +13,6 @@ import condorLogParser
 
 # for now it is just a constructor wrapper
 # Further on it will need to implement glidein exit code checks
-class dirSummaryTimings(condorLogParser.dirSummary):
+class dirSummaryTimings(condorLogParser.dirSummaryTimings):
     def __init__(self,dirname,client_name,inactive_files=None):
         condorLogParser.dirSummaryTimings.__init__(self,dirname,log_prefix="condor_activity_",log_suffix="_"+client_name+".log",inactive_files=inactive_files)
