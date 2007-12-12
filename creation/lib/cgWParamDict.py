8107028d4ba1ecc6fd6b7e3b89020229fa4c547b
sfiligoi <sfiligoi>
2007-12-12 01:10:51 +0000

    Fix params update

--- creation/lib/cgWParamDict.py
+++ creation/lib/cgWParamDict.py
@@ -138,7 +138,7 @@ class glideinDicts(cgWDictFile.glideinDicts):
                 # use one of the global ones if specific not provided
                 schedd_name=global_schedd_names[global_schedd_idx%len(global_schedd_names)]
                 global_schedd_idx=global_schedd_idx+1
-                params.entries[entry_name].schedd_name=schedd_name
+                params.subparams.data['entries'][entry_name]['schedd_name']=schedd_name
 
         for entry_name in self.entry_list:
             self.entry_dicts[entry_name].populate(params)
@@ -265,10 +265,13 @@ def add_attr_unparsed_real(attr_name,attr_obj,dicts):
 #
 # CVS info
 #
-# $Id: cgWParamDict.py,v 1.6 2007/12/12 00:54:42 sfiligoi Exp $
+# $Id: cgWParamDict.py,v 1.7 2007/12/12 01:10:51 sfiligoi Exp $
 #
 # Log:
 #  $Log: cgWParamDict.py,v $
+#  Revision 1.7  2007/12/12 01:10:51  sfiligoi
+#  Fix params update
+#
 #  Revision 1.6  2007/12/12 00:54:42  sfiligoi
 #  Fix typo
 #
