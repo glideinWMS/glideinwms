2332a52730724b0aa869e0d53f3dd26c91adabe6
sfiligoi <sfiligoi>
2007-12-14 20:51:32 +0000

    Automate constants copying

---
+++ creation/web_base/cat_consts.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+config_file=$1
+tmp_fname=${config_file}.$$.tmp
+
+entry_dir=$2
+
+function warn {
+ echo `date` $@ 1>&2
+}
+
+###################################
+# Add a line to the config file
+function add_config_line {
+    id=$1
+
+    rm -f $tmp_fname #just in case one was there
+    mv $config_file $tmp_fname
+    if [ $? -ne 0 ]; then
+	warn "Error renaming $config_file into $tmp_fname"
+	exit 1
+    fi
+    grep -v "^$id " $tmp_fname > $config_file
+    echo "$@" >> $config_file
+    rm -f $tmp_fname
+}
+
+if [ "$entry_dir" == "main" ]; then 
+    ###################################
+    # Find file names
+    descript_fname=`grep "^descript_fname " $config_file | awk '{print $3}'`
+    if [ -z "$descript_fname" ]; then
+	warn "Cannot find descript_fname in $config_file!"
+	exit 1
+    fi
+
+    consts_file=`grep "^consts_file " $descript_fname | awk '{print $2}'`
+    if [ -z "$consts_file" ]; then
+	warn "Cannot find consts_file in $descript_fname!"
+	exit 1
+    fi
+else:
+    ###################################
+    # Find file names
+    descript_fname=`grep "^descript_entry_fname " $config_file | awk '{print $3}'`
+    if [ -z "$descript_fname" ]; then
+	warn "Cannot find descript_entry_fname in $config_file!"
+	exit 1
+    fi
+
+    consts_file=`grep "^consts_file " $entry_dir/$descript_fname | awk '{print $2}'`
+    if [ -z "$consts_file" ]; then
+	warn "Cannot find consts_file in $entry_dir/$descript_fname!"
+	exit 1
+    fi
+    consts_file=$entry_dir/$consts_file
+fi
+
+##################################
+# Merge constants with config file
+if [ -n "$consts_file" ]; then
+    echo "# --- Provided $entry_dir constants  ---" >> $config_file
+    # merge constants
+    while read line
+    do
+	add_config_line $line
+    done < "$consts_file"
+fi
