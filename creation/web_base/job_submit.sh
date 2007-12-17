a991a1a04585b3298a0d2242ef001f703c39d733
sfiligoi <sfiligoi>
2007-12-17 16:28:32 +0000

    Automatically extract the schedd name. Must be always the same, so it makes no sense to pass it as a parameter.

--- creation/web_base/job_submit.sh
+++ creation/web_base/job_submit.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 8 ]; then
- echo "At least 5 args expected!" 1>&2
- echo "Usage: job_submit.sh entry_name schedd client count mode [params]*"
+if [ $# -lt 4 ]; then
+ echo "At least 4 args expected!" 1>&2
+ echo "Usage: job_submit.sh entry_name client count mode [params]*"
  1>&2
  exit 1
 fi
 GLIDEIN_ENTRY_NAME="$1"
-shift
-export GLIDEIN_SCHEDD=$1
 shift
 export GLIDEIN_CLIENT="$1"
 shift
@@ -28,5 +26,8 @@ while [ $# -ge 2 ]; do
  shift
 done
 export GLIDEIN_PARAMS
+
+export GLIDEIN_SCHEDD=`grep -i "^Schedd "  entry_${GLIDEIN_ENTRY_NAME}/job.descript | awk '{print $2}'`
+
 export GLIDEIN_LOGNR=`date +%Y%m%d`
 condor_submit -append "$GLIDEIN_GRIDOPTS" -name $GLIDEIN_SCHEDD entry_${GLIDEIN_ENTRY_NAME}/job.condor
