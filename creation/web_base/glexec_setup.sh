548c1a50324c2f7ebf489a1812d0acef37302ce6
sfiligoi <sfiligoi>
2008-01-24 20:16:24 +0000

    Add automatic OSG glexec discovery

--- creation/web_base/glexec_setup.sh
+++ creation/web_base/glexec_setup.sh
@@ -44,8 +44,13 @@ append_config "GLEXEC_USER_DIR=$glide_tmp_dir"
 #
 glexec_bin=`grep '^GLEXEC_BIN' $glidein_config | awk '{print $2}'`
 if [ -z "$glexec_bin" ]; then
-    glexec_bin="/opt/glite/sbin/glexec"
+    glexec_bin="OSG"
     echo "GLEXEC_BIN not found, using default '$glexec_bin'"
+fi
+
+if [ "$glexec_bin" == "OSG" ]; then
+    echo "GLEXEC_BIN was OSG, expand to '$OSG_GLEXEC_LOCATION'"
+    glexec_bin="$OSG_GLEXEC_LOCATION"
 fi
 
 # but first test it does exist
