b067ed76d65f049e88bab118b1bd744336917a1b
sfiligoi <sfiligoi>
2008-05-08 14:41:31 +0000

    Use helper module

--- tools/glidein_ls.py
+++ tools/glidein_ls.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 #
-# condor_ls
+# glidein_ls.py
 #
 # Description:
 #   Execute a ls command on a condor job working directory
@@ -15,31 +15,10 @@
 #  Fermitools
 #
 
-import os,os.path
-import string
-import stat
-import sys
+import sys,os.path
 sys.path.append(os.path.join(sys.path[0],"lib"))
 sys.path.append(os.path.join(sys.path[0],"../lib"))
 
-import glideinMonitor
+import glideinCmd
 
-def createDirMonitorFile(monitor_file_name,monitor_control_relname,
-                         argv,condor_status):
-    fd=open(monitor_file_name,"w")
-    try:
-        fd.write("#!/bin/sh\n")
-        fd.write("outdir=`ls -lt .. | tail -1 | awk '{print $9}'`\n")
-        fd.write("(cd ../$outdir; if [ $? -eq 0 ]; then ls %s; else echo Internal error; fi)\n"%(string.join(argv)))
-        fd.write("echo Done > %s\n"%monitor_control_relname)
-    finally:
-        fd.close()
-
-    os.chmod(monitor_file_name,stat.S_IRWXU)
-
-
-args=glideinMonitor.parseArgs(sys.argv[1:])
-
-glideinMonitor.monitor(args['jid'],args['schedd_name'],args['pool_name'],
-                       args['timeout'],
-                       createDirMonitorFile,args['argv'])
+glideinCmd.exe_cmd(lambda argv:(['ls']+argv))
