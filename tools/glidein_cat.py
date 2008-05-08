cda63e96b042de04b3aa12348a411229e9b9d973
sfiligoi <sfiligoi>
2008-05-08 16:30:36 +0000

    Change rel paths into abspaths and use helper module

--- tools/glidein_cat.py
+++ tools/glidein_cat.py
@@ -1,38 +1,24 @@
 #!/bin/env python
 #
-# glidein_cat
+# glidein_cat.py
 #
-# Execute a cat command on the job directory
+# Description:
+#   Execute a cat command on a condor job working directory
 #
 # Usage:
-#  glidein_ls.py <cluster>.<process> [-name <schedd_name>] [-pool <pool_name> ] [-timeout <nr secs>
+#  glidein_cat.py <cluster>.<process> [<file>] [-name <schedd_name>] [-pool <pool_name> ] [-timeout <nr secs>]
+#
+# Author:
+#   Igor Sfiligoi (May 2007)
+#
+# License:
+#  Fermitools
 #
 
-import os
-import string
-import stat
-import sys
-sys.path.append("lib")
-sys.path.append("../lib")
+import sys,os.path
+sys.path.append(os.path.join(sys.path[0],"lib"))
+sys.path.append(os.path.join(sys.path[0],"../lib"))
 
-import glideinMonitor
+import glideinCmd
 
-def createCatMonitorFile(monitor_file_name,monitor_control_relname,
-                         argv,condor_status):
-    fd=open(monitor_file_name,"w")
-    try:
-        fd.write("#!/bin/sh\n")
-        fd.write("outdir=`ls -lt .. | tail -1 | awk '{print $9}'`\n")
-        fd.write("(cd ../$outdir; if [ $? -eq 0 ]; then cat %s; else echo Internal error; fi)\n"%(string.join(argv)))
-        fd.write("echo Done > %s\n"%monitor_control_relname)
-    finally:
-        fd.close()
-
-    os.chmod(monitor_file_name,stat.S_IRWXU)
-
-
-args=glideinMonitor.parseArgs(sys.argv[1:])
-
-glideinMonitor.monitor(args['jid'],args['schedd_name'],args['pool_name'],
-                       args['timeout'],
-                       createCatMonitorFile,args['argv'])
+glideinCmd.exe_cmd(lambda argv:(['cat']+argv))
