c0adec7bb942f2f936cadca807e3173f7c3c6462
sfiligoi <sfiligoi>
2008-05-08 17:27:21 +0000

    A generic pseudo-interactive command

---
+++ tools/glidein_interactive.py
@@ -0,0 +1,29 @@
+#!/bin/env python
+#
+# glidein_interactive.py
+#
+# Description:
+#   Execute an arbitrary command on a condor job working directory
+#
+# Usage:
+#  glidein_interactive.py <cluster>.<process> [-name <schedd_name>] [-pool <pool_name> ] [-timeout <nr secs>] command
+#
+# Author:
+#   Igor Sfiligoi (May 2008)
+#
+# License:
+#  Fermitools
+#
+
+import sys,os.path
+sys.path.append(os.path.join(sys.path[0],"lib"))
+sys.path.append(os.path.join(sys.path[0],"../lib"))
+
+import glideinCmd
+
+def argv_interactive(argv):
+    if len(argv)<1:
+        raise RuntimeError, "Please specify the command to run"
+    return argv
+
+glideinCmd.exe_cmd(argv_interactive)
