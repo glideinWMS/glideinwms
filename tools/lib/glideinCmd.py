df81537f04253e6b7ac0ede75f6c8c8430d6bae9
sfiligoi <sfiligoi>
2008-06-12 16:46:24 +0000

    Refactor and add exe_cmd_simple and exe_script

--- tools/lib/glideinCmd.py
+++ tools/lib/glideinCmd.py
@@ -14,7 +14,8 @@ import glideinMonitor
 # based on the passed arguments
 #
 # argv_func is a callback that transforms the unrecognized arguments
-# into a list of arguments to pass to the command line
+# into a list of arguments to pass to the command line after it has
+# moved into the monitored job dir
 #
 def exe_cmd(argv_func):
     try:
@@ -26,31 +27,84 @@ def exe_cmd(argv_func):
         print e
         sys.exit(1)
 
+# Try to execute the pesudo interactive command
+# based on the passed arguments
+#
+# argv_func is a callback that transforms the unrecognized arguments
+# into a list of arguments to pass to the command line
+#
+def exe_cmd_simple(argv_func):
+    try:
+        args=glideinMonitor.parseArgs(sys.argv[1:])
+        glideinMonitor.monitor(args['jid'],args['schedd_name'],args['pool_name'],
+                               args['timeout'],
+                               createCmdMonitorFileSimple,argv_func(args['argv']))
+    except RuntimeError,e:
+        print e
+        sys.exit(1)
+
+# Try to execute the pesudo interactive command
+# based on the passed arguments
+#
+# argv_func is a callback that creates the monitoring script
+# arguments are (file_name,out_relname,argv,condor_status)
+# where argv is the list of unrecognized arguments
+#
+def exe_script(argv_func):
+    try:
+        args=glideinMonitor.parseArgs(sys.argv[1:])
+        glideinMonitor.monitor(args['jid'],args['schedd_name'],args['pool_name'],
+                               args['timeout'],
+                               argv_func,args['argv'])
+    except RuntimeError,e:
+        print e
+        sys.exit(1)
+
 ######################################
 # INTERNAL - Do not use directly
 ######################################
 
-# callback function for glideinMonitor.monitor
-# changes to the work dir and executes the command
-def createCmdMonitorFile(monitor_file_name,monitor_control_relname,
-                         argv,condor_status):
+# create a file usable by a callback function for glideinMonitor.monitor
+def createCmdMonitorFileScript(monitor_file_name,monitor_control_relname,
+                               script_list):
+    # create the command file
+    fd=open(monitor_file_name,"w")
+    try:
+        fd.write("#!/bin/sh\n")
+        fd.write("glidein_cmd_startdir=$PWD\n")
+        for script_line in script_list:
+            fd.write("%s\n"%script_line)
+        fd.write('echo Done > "$glidein_cmd_startdir/%s"\n'%monitor_control_relname)
+    finally:
+        fd.close()
+
+    os.chmod(monitor_file_name,stat.S_IRWXU)
+
+# convert a list into a command line string
+def argv2cmd(argv):
     # escape the spaces
     # everything else should be passed to the cmsdline as it is
     eargv=[]
     for arg in argv:
         eargv.append(string.replace(arg,' ','\ '))
+    return string.join(eargv)
 
-    # create the command file
-    fd=open(monitor_file_name,"w")
-    try:
-        fd.write("#!/bin/sh\n")
-        # find out work dir
-        fd.write("outdir=`ls -lt .. | tail -1 | awk '{print $9}'`\n")
-        # execute command in work dir
-        fd.write("(cd ../$outdir; if [ $? -eq 0 ]; then %s; else echo Internal error; fi)\n"%(string.join(eargv)))
-        fd.write("echo Done > %s\n"%monitor_control_relname)
-    finally:
-        fd.close()
+# callback function for glideinMonitor.monitor
+# changes to the work dir and executes the command
+def createCmdMonitorFile(monitor_file_name,monitor_control_relname,
+                         argv,condor_status):
+    script_lines=[]
+    script_lines.append("outdir=`ls -lt .. | tail -1 | awk '{print $9}'`")
+    # execute command in work dir
+    script_lines.append("(cd ../$outdir; if [ $? -eq 0 ]; then %s; else echo Internal error; fi)"%argv2cmd(argv))
+    return createCmdMonitorFileScript(monitor_file_name,monitor_control_relname,script_list)
 
-    os.chmod(monitor_file_name,stat.S_IRWXU)
+# callback function for glideinMonitor.monitor
+# executes the command
+def createCmdMonitorFileSimple(monitor_file_name,monitor_control_relname,
+                         argv,condor_status):
+    script_lines=[]
+    script_lines.append(argv2cmd(argv))
+    return createCmdMonitorFileScript(monitor_file_name,monitor_control_relname,script_list)
+
 
