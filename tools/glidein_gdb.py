52e583375c911ad4aaf3f9c9fe71b5d6c212e0e6
sfiligoi <sfiligoi>
2008-06-13 05:03:32 +0000

    Fix typo

--- tools/glidein_gdb.py
+++ tools/glidein_gdb.py
@@ -46,7 +46,7 @@ def argv_gdb(argv):
     script_lines.append(gdb_cmd)
     script_lines.append('quit')
     script_lines.append('EOF')
-    script_lines.append('gdb -command %s'%Gdbcommand) 
+    script_lines.append('gdb -command %s'%gdbcommand) 
     script_lines.append('res=$?')
     script_lines.append('rm -f %s'%gdbcommand)
     script_lines.append('exit $res')
