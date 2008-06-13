2f8b58cc6d415ff263deb09c5e86e3914587c800
sfiligoi <sfiligoi>
2008-06-13 05:15:24 +0000

    Fix bug

--- tools/glidein_gdb.py
+++ tools/glidein_gdb.py
@@ -46,7 +46,7 @@ def argv_gdb(argv):
     script_lines.append(gdb_cmd)
     script_lines.append('quit')
     script_lines.append('EOF')
-    script_lines.append('gdb -command %s'%gdbcommand) 
+    script_lines.append('gdb -command %s /proc/%s/exe %s'%(gdbcommand,pid,pid)) 
     script_lines.append('rm -f %s'%gdbcommand)
 
     return script_lines
