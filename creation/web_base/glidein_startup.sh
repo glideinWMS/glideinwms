9cf73b150b20c2a260ac721a31af7b9a63395e3f
sfiligoi <sfiligoi>
2008-07-28 20:33:24 +0000

    Fix entry_ign handling

--- creation/web_base/glidein_startup.sh
+++ creation/web_base/glidein_startup.sh
@@ -218,7 +218,7 @@ echo "proxy_url         = '$proxy_url'"
 echo "descript_fname    = '$descript_file'"
 echo "descript_entry_fname = '$descript_entry_file'"
 echo "sign_id           = '$sign_id'"
-echo "entry_sign_id     = '$entry_sign_id'"
+echo "entry_sign_id     = '$sign_entry_id'"
 echo
 echo "Running on `uname -n`"
 echo "System: `uname -a`"
@@ -376,7 +376,7 @@ echo "PROXY_URL $proxy_url" >> glidein_config
 echo "DESCRIPTION_FILE $descript_file" >> glidein_config
 echo "DESCRIPTION_ENTRY_FILE $descript_entry_file" >> glidein_config
 echo "GLIDEIN_Signature $sign_id" >> glidein_config
-echo "GLIDEIN_Entry_Signature $entry_sign_id" >> glidein_config
+echo "GLIDEIN_Entry_Signature $sign_entry_id" >> glidein_config
 echo "# --- User Parameters ---" >> glidein_config
 params2file $params
 
