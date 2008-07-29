c17dd5412f7b66e2cef36bff3345afdde92b6e32
sfiligoi <sfiligoi>
2008-07-29 20:07:43 +0000

    Fix typo

--- creation/web_base/cat_consts.sh
+++ creation/web_base/cat_consts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-config_file=$1
-tmp_fname=${config_file}.$$.tmp
+glidein_config=$1
+tmp_fname=${glidein_config}.$$.tmp
 
 entry_dir=$2
 
@@ -10,23 +10,23 @@ function warn {
 }
 
 # import add_config_line function
-add_config_line_source=`grep '^ADD_CONFIG_LINE_SOURCE ' $config_file | awk '{print $2}'`
+add_config_line_source=`grep '^ADD_CONFIG_LINE_SOURCE ' $glidein_config | awk '{print $2}'`
 source $add_config_line_source
 
 if [ "$entry_dir" == "main" ]; then 
     ###################################
     # Find file names
-    consts_file=`grep "^CONSTS_FILE " $config_file | awk '{print $2}'`
+    consts_file=`grep "^CONSTS_FILE " $glidein_config | awk '{print $2}'`
     if [ -z "$consts_file" ]; then
-	warn "Cannot find CONSTS_FILE in $config_file!"
+	warn "Cannot find CONSTS_FILE in $glidein_config!"
 	exit 1
     fi
 else
     ###################################
     # Find file names
-    consts_file=`grep "^CONSTS_ENTRY_FILE " $config_file | awk '{print $2}'`
+    consts_file=`grep "^CONSTS_ENTRY_FILE " $glidein_config | awk '{print $2}'`
     if [ -z "$consts_file" ]; then
-	warn "Cannot find CONSTS_ENTRY_FILE in $config_file!"
+	warn "Cannot find CONSTS_ENTRY_FILE in $glidein_config!"
 	exit 1
     fi
 fi
@@ -34,11 +34,11 @@ fi
 ##################################
 # Merge constants with config file
 if [ -n "$consts_file" ]; then
-    echo "# --- Provided $entry_dir constants  ---" >> $config_file
+    echo "# --- Provided $entry_dir constants  ---" >> $glidein_config
     # merge constants
     while read line
     do
 	add_config_line $line
     done < "$consts_file"
-    echo "# --- End $entry_dir constants       ---" >> $config_file
+    echo "# --- End $entry_dir constants       ---" >> $glidein_config
 fi
