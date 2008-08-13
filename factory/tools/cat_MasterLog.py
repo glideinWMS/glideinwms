02c21a70529c90e3c96c948e13e45ef5203575a3
sfiligoi <sfiligoi>
2008-08-13 17:22:32 +0000

    Add support for monitor logs

--- factory/tools/cat_MasterLog.py
+++ factory/tools/cat_MasterLog.py
@@ -13,11 +13,18 @@ STARTUP_DIR=sys.path[0]
 sys.path.append(os.path.join(STARTUP_DIR,"lib"))
 import gWftLogParser
 
-USAGE="Usage: cat_MasterLog.py <logname>"
+USAGE="Usage: cat_MasterLog.py [-monitor] <logname>"
 
 def main():
+    if sys.argv[1]=='-monitor':
+        fname=sys.argv[2]
+        condor_log_id="MasterLog.monitor"
+    else:
+        fname=sys.argv[1]
+        condor_log_id="MasterLog"
+        
     try:
-        print gWftLogParser.get_CondorLog(sys.argv[1],"MasterLog")
+        print gWftLogParser.get_CondorLog(fname,condor_log_id)
     except:
         sys.stderr.write("%s\n"%USAGE)
         sys.exit(1)
--- factory/tools/cat_StartdLog.py
+++ factory/tools/cat_StartdLog.py
@@ -13,11 +13,18 @@ STARTUP_DIR=sys.path[0]
 sys.path.append(os.path.join(STARTUP_DIR,"lib"))
 import gWftLogParser
 
-USAGE="Usage: cat_StartdLog.py <logname>"
+USAGE="Usage: cat_StartdLog.py [-monitor] <logname>"
 
 def main():
+    if sys.argv[1]=='-monitor':
+        fname=sys.argv[2]
+        condor_log_id="StartdLog.monitor"
+    else:
+        fname=sys.argv[1]
+        condor_log_id="StartdLog"
+        
     try:
-        print gWftLogParser.get_CondorLog(sys.argv[1],"StartdLog")
+        print gWftLogParser.get_CondorLog(fname,condor_log_id)
     except:
         sys.stderr.write("%s\n"%USAGE)
         sys.exit(1)
