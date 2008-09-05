32acca09a7ceb8b7092e95b51572460134d9648a
sfiligoi <sfiligoi>
2008-09-05 20:22:26 +0000

    Warning now writes both in err and info

--- factory/glideFactoryLib.py
+++ factory/glideFactoryLib.py
@@ -92,16 +92,18 @@ class FactoryConfig:
                 self.activity_log.write(str+"\n")
             except:
                 # logging must never throw an exception!
-                self.logWarning("logActivity failed, was logging: %s"+str)
+                self.logWarning("logActivity failed, was logging: %s"+str,False)
 
-    def logWarning(self,str):
+    def logWarning(self,str, log_in_activity=True):
         if self.warning_log!=None:
             try:
                 self.warning_log.write(str+"\n")
             except:
                 # logging must throw an exception!
                 # silently ignore
-                pass 
+                pass
+        if log_in_activity:
+            self.logActivity("WARNING: %s"%srt)
 
 
 # global configuration of the module
@@ -655,10 +657,13 @@ def releaseGlideins(schedd_name,jid_list):
 #
 # CVS info
 #
-# $Id: glideFactoryLib.py,v 1.31.2.1 2008/09/05 20:19:50 sfiligoi Exp $
+# $Id: glideFactoryLib.py,v 1.31.2.2 2008/09/05 20:22:26 sfiligoi Exp $
 #
 # Log:
 #  $Log: glideFactoryLib.py,v $
+#  Revision 1.31.2.2  2008/09/05 20:22:26  sfiligoi
+#  Warning now writes both in err and info
+#
 #  Revision 1.31.2.1  2008/09/05 20:19:50  sfiligoi
 #  Protect from condor_submit failures
 #
