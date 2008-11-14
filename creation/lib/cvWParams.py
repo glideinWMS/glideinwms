08070680cb863094698986044440da3d409eacfc
sfiligoi <sfiligoi>
2008-11-14 17:39:11 +0000

    Use commentedOrderedDict

--- creation/lib/cvWParams.py
+++ creation/lib/cvWParams.py
@@ -36,15 +36,15 @@ class VOFrontendParams(cWParams.CommonParams):
         self.attr_defaults["conditional"]=("True","Bool","Should I only publish if the factory tell us it supports it?",None)
         self.attr_defaults["type"][1]="string|int|expr"
 
-        group_config_defaults=xmlParse.OrderedDict()
+        group_config_defaults=cWParams.commentedOrderedDict()
         group_config_defaults['max_running_jobs']=('10000',"nr_jobs","What is the max number of running jobs I want to get to",None)
         
-        group_config_glideins_defaults=xmlParse.OrderedDict()
+        group_config_glideins_defaults=cWParams.commentedOrderedDict()
         group_config_glideins_defaults["max"]=['100',"nr_jobs","How much pressure should I apply to the entry points",None]
         group_config_glideins_defaults["reserve"]=['5',"nr_jobs","How much to overcommit.",None]
         group_config_defaults['idle_glideins_per_entry']=group_config_glideins_defaults
 
-        group_config_vms_defaults=xmlParse.OrderedDict()
+        group_config_vms_defaults=cWParams.commentedOrderedDict()
         group_config_vms_defaults["max"]=['100',"nr_jobs","How many idle VMs should I tollerate, before stopping submitting glideins",None]
         group_config_vms_defaults["curb"]=['5',"nr_jobs","When to start curbing submissions.",None]
         group_config_defaults['idle_vms_per_entry']=group_config_vms_defaults
@@ -53,16 +53,16 @@ class VOFrontendParams(cWParams.CommonParams):
         sub_defaults={'attrs':(xmlParse.OrderedDict(),'Dictionary of attributes',"Each attribute group contains",self.attr_defaults),
                       'files':([],'List of files',"Each file group contains",self.file_defaults)}
 
-        query_attrs_defaults=xmlParse.OrderedDict()
+        query_attrs_defaults=cWParams.commentedOrderedDict()
         query_attrs_defaults['type']=('s','s|i|r|b','Attribute type',None)
 
-        match_defaults=xmlParse.OrderedDict()
+        match_defaults=cWParams.commentedOrderedDict()
         match_defaults["factory_query_expr"]=('True','CondorExpr','Expression for selecting user jobs',None)
         match_defaults["job_query_expr"]=['True','CondorExpr','Expression for selecting user jobs',None]
         match_defaults["job_match_attrs"]=(xmlParse.OrderedDict(),"Dictionary of ClassAd attributes","Each attribute contains",query_attrs_defaults)
         match_defaults["match_expr"]=('True','PythonExpr', 'Expression for matching jobs to factory entries',None)
 
-        self.group_defaults=xmlParse.OrderedDict()
+        self.group_defaults=cWParams.commentedOrderedDict()
         self.group_defaults["match"]=match_defaults
         self.group_defaults["enabled"]=("True","Bool","Is this group enabled?",None)
         self.group_defaults["config"]=group_config_defaults
@@ -75,11 +75,11 @@ class VOFrontendParams(cWParams.CommonParams):
         # Start defining the defaults
         self.defaults["frontend_name"]=(socket.gethostname(),'ID', 'VO Frontend name',None)
 
-        work_defaults=xmlParse.OrderedDict()
+        work_defaults=cWParams.commentedOrderedDict()
         work_defaults["base_dir"]=(os.environ["HOME"],"base_dir","Frontend base dir",None)
         self.defaults["work"]=work_defaults
 
-        log_retention_defaults=xmlParse.OrderedDict()
+        log_retention_defaults=cWParams.commentedOrderedDict()
         log_retention_defaults["min_days"]=["3.0","days","Min number of days the logs must be preserved (even if they use too much space)",None]
         log_retention_defaults["max_days"]=["7.0","days","Max number of days the logs should be preserved",None]
         log_retention_defaults["max_mbytes"]=["100.0","Mbytes","Max number of Mbytes the logs can use",None]
@@ -88,25 +88,25 @@ class VOFrontendParams(cWParams.CommonParams):
         self.defaults['loop_delay']=('60','seconds', 'Number of seconds between iterations',None)
         self.defaults['advertise_delay']=('5','NR', 'Advertize evert NR loops',None)
 
-        stage_defaults=xmlParse.OrderedDict()
+        stage_defaults=cWParams.commentedOrderedDict()
         stage_defaults["base_dir"]=("/var/www/html/vofrontend/stage","base_dir","Stage base dir",None)
         stage_defaults["web_base_url"]=("http://%s/vofrontend/stage"%socket.gethostname(),'base_url','Base Web server URL',None)
         stage_defaults["use_symlink"]=("True","Bool","Can I symlink stage dir from work dir?",None)
         self.defaults["stage"]=stage_defaults
 
-        monitor_opts_default=xmlParse.OrderedDict()
+        monitor_opts_default=cWParams.commentedOrderedDict()
         monitor_opts_default["want_split_graphs"]=("True","Bool","Should create split graphs?",None)
         monitor_opts_default["want_trend_graphs"]=("True","Bool","Should create trend graphs?",None)
         monitor_opts_default["want_infoage_graphs"]=("True","Bool","Should create infoage graphs?",None)
 
         
-        monitor_default=xmlParse.OrderedDict()
+        monitor_default=cWParams.commentedOrderedDict()
         monitor_default["base_dir"]=("/var/www/html/vofrontend/monitor","base_dir","Monitoring base dir",None)
         monitor_default["frontend"]=copy.deepcopy(monitor_opts_default)
         monitor_default["group"]=copy.deepcopy(monitor_opts_default)
         self.defaults["monitor"]=monitor_default
         
-        security_default=xmlParse.OrderedDict()
+        security_default=cWParams.commentedOrderedDict()
         security_default["sym_key"]=("aes_256_cbc","sym_algo","Type of symetric key system used for secure message passing",None)
         security_default["x509_proxy"]=(None,"fname","Where the x509 proxy will be found (if None, use factory one)",None)
         
