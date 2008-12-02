55d372b4887bc9461d9e5ea958a5703ed19bdcae
sfiligoi <sfiligoi>
2008-12-02 16:43:41 +0000

    Rewrite based on factory one

--- frontend/checkFrontend.py
+++ frontend/checkFrontend.py
@@ -4,21 +4,19 @@
 #   Check if a glideinFrontend is running
 # 
 # Arguments:
-#   $1 = config file
+#   $1 = work_dir
 #
 # Author:
-#   Igor Sfiligoi Jul 17th 2008
+#   Igor Sfiligoi
 #
 
 import sys,os.path
 sys.path.append(os.path.join(sys.path[0],"../lib"))
 import glideinFrontendPidLib
 
-config_dict={}
 try:
-    execfile(sys.argv[1],config_dict)
-    log_dir=config_dict['log_dir']
-    frontend_pid=glideinFrontendPidLib.get_frontend_pid(log_dir)
+    work_dir=sys.argv[1]
+    frontend_pid=glideinFrontendPidLib.get_frontend_pid(work_dir)
 except:
     print "Not running"
     sys.exit(1)
