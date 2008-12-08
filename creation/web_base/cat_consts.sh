627edb8b26ed47605d85c6b3a7c448ff895ea20a
sfiligoi <sfiligoi>
2008-12-08 22:15:50 +0000

    Use proper id names, rely on get_prefix

--- creation/web_base/cat_consts.sh
+++ creation/web_base/cat_consts.sh
@@ -3,7 +3,7 @@
 glidein_config=$1
 tmp_fname=${glidein_config}.$$.tmp
 
-entry_dir=$2
+dir_id=$2
 
 function warn {
  echo `date` $@ 1>&2
@@ -13,32 +13,28 @@ function warn {
 add_config_line_source=`grep '^ADD_CONFIG_LINE_SOURCE ' $glidein_config | awk '{print $2}'`
 source $add_config_line_source
 
-if [ "$entry_dir" == "main" ]; then 
-    ###################################
-    # Find file names
-    consts_file=`grep "^CONSTS_FILE " $glidein_config | awk '{print $2}'`
-    if [ -z "$consts_file" ]; then
-	warn "Cannot find CONSTS_FILE in $glidein_config!"
-	exit 1
-    fi
-else
-    ###################################
-    # Find file names
-    consts_file=`grep "^CONSTS_ENTRY_FILE " $glidein_config | awk '{print $2}'`
-    if [ -z "$consts_file" ]; then
-	warn "Cannot find CONSTS_ENTRY_FILE in $glidein_config!"
-	exit 1
-    fi
+# import get_prefix function
+get_id_selectors_source=`grep '^GET_ID_SELECTORS_SOURCE ' $glidein_config | awk '{print $2}'`
+source $get_id_selectors_source
+
+id_prefix=`get_prefix $dir_id`
+
+###################################
+# Find file names
+consts_file=`grep "^${id_prefix}CONSTS_FILE " $glidein_config | awk '{print $2}'`
+if [ -z "$consts_file" ]; then
+    warn "Cannot find ${id_prefix}CONSTS_FILE in $glidein_config!"
+    exit 1
 fi
 
 ##################################
 # Merge constants with config file
 if [ -n "$consts_file" ]; then
-    echo "# --- Provided $entry_dir constants  ---" >> $glidein_config
+    echo "# --- Provided $dir_id constants  ---" >> $glidein_config
     # merge constants
     while read line
     do
 	add_config_line $line
     done < "$consts_file"
-    echo "# --- End $entry_dir constants       ---" >> $glidein_config
+    echo "# --- End $dir_id constants       ---" >> $glidein_config
 fi
