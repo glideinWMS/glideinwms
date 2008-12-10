3d0cd42f1a28be461e05aadf95c51640a0457e4d
sfiligoi <sfiligoi>
2008-12-10 15:13:57 +0000

    Fix typo

--- creation/web_base/glidein_startup.sh
+++ creation/web_base/glidein_startup.sh
@@ -908,9 +908,10 @@ done
 
 ###################################################
 # get last_script, as it is used by the fetch_file
-last_script=`grep "^last_script " "$descript_file"`
+gs_id_descript_file=`get_descript_file main`
+last_script=`grep "^last_script " "$gs_id_descript_file"`
 if [ $? -ne 0 ]; then
-    warn "last_script not in description file $descript_file." 1>&2
+    warn "last_script not in description file $gs_id_descript_file." 1>&2
     glidein_exit 1
 fi
 
