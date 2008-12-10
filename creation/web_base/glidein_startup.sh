cb75726e8f3e312da093e0290eb00f4a50feeada
sfiligoi <sfiligoi>
2008-12-10 20:10:48 +0000

    Fix typo

--- creation/web_base/glidein_startup.sh
+++ creation/web_base/glidein_startup.sh
@@ -810,7 +810,7 @@ function fetch_file_base {
 	    warn "Error making '$ffb_outname' executable" 1>&2
 	    return 1
 	fi
-	if [ "$ffb_id" == "main" -a "$ffb_real_fname" == "$last_script" ]; then # last_script global for simplicity
+	if [ "$ffb_id" == "main" -a "$ffb_target_fname" == "$last_script" ]; then # last_script global for simplicity
 	    echo "Skipping last script $last_script" 1>&2
 	else
             echo "Executing $ffb_outname"
@@ -936,7 +936,7 @@ done
 # get last_script, as it is used by the fetch_file
 gs_id_work_dir=`get_work_dir main`
 gs_id_descript_file=`get_descript_file main`
-last_script=`grep "^last_script " "${gs_id_work_dir}/$gs_id_descript_file"`
+last_script=`grep "^last_script " "${gs_id_work_dir}/$gs_id_descript_file" | awk '{print $2}'`
 if [ $? -ne 0 ]; then
     warn "last_script not in description file ${gs_id_work_dir}/$gs_id_descript_file." 1>&2
     glidein_exit 1
