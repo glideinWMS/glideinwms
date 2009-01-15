378e3ce416a96db4fb381f86f034ed62217504c5
sfiligoi <sfiligoi>
2009-01-15 20:41:16 +0000

    Improve formatting

--- tools/glidein_status.py
+++ tools/glidein_status.py
@@ -104,7 +104,7 @@ def fmt_time(t):
 print_mask="%-39s %-9s"
 if want_gk:
     print_mask+=" %-5s %-43s"
-print_mask+=" %-24s %-14s"
+print_mask+=" %-19s %-19s"
 if want_glidecluster:
     print_mask+=" %-39s %-14s"
 print_mask+=" %-9s %-8s %-10s"
