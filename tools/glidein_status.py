8fc1400f9989cf115d562344276e85f0f99b58ea
sfiligoi <sfiligoi>
2009-01-15 19:14:30 +0000

    Fix typo

--- tools/glidein_status.py
+++ tools/glidein_status.py
@@ -69,7 +69,7 @@ def fmt_time(t):
 
 print_mask="%-39s %-9s"
 if want_gk:
-    print_mask+=" %-5s %43"
+    print_mask+=" %-5s %-43s"
 print_mask+=" %-24s %-14s %-9s %-8s %-10s"
 
 header=('Name','Site')
