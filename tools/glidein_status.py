79fa439c974eb6a9fff2d913d77ee1e8d85ce9c0
sfiligoi <sfiligoi>
2009-01-15 19:13:57 +0000

    Add -gk

--- tools/glidein_status.py
+++ tools/glidein_status.py
@@ -6,7 +6,7 @@
 #   Equivalent to condor_status, but with glidein specific info
 #
 # Usage:
-#  glidein_status.py 
+#  glidein_status.py [-gk]
 #
 # Author:
 #   Igor Sfiligoi
@@ -21,10 +21,23 @@ import condorMonitor
 pool_name=None
 constraint=None
 
+want_gk=False
+
+
+for arg in sys.argv:
+    if arg=='-gk':
+        want_gk=True
+
 format_list=[('Machine','s'),('State','s'),('Activity','s'),
              ('GLIDEIN_Site','s'),
              ('GLIDEIN_Factory','s'),('GLIDEIN_Name','s'),('GLIDEIN_Entry_Name','s'),('EnteredCurrentActivity','i')]
 attrs=['State','Activity','GLIDEIN_Site','GLIDEIN_Factory','GLIDEIN_Name','GLIDEIN_Entry_Name','EnteredCurrentActivity']
+
+if want_gk:
+    format_list.append(('GLIDEIN_Gatekeeper','s'))
+    format_list.append(('GLIDEIN_GridType','s'))
+    attrs.append('GLIDEIN_Gatekeeper')
+    attrs.append('GLIDEIN_GridType')
 
 cs=condorMonitor.CondorStatus(pool_name=pool_name)
 cs.load(constraint=constraint,format_list=format_list)
@@ -54,10 +67,18 @@ def fmt_time(t):
     return "%i+%02i:%02i:%02i"%(diff_days,diff_hours,diff_mins,diff_secs)
 
 
-print_mask="%-39s %-9s %-24s %-14s %-9s %-8s %-10s"
+print_mask="%-39s %-9s"
+if want_gk:
+    print_mask+=" %-5s %43"
+print_mask+=" %-24s %-14s %-9s %-8s %-10s"
+
+header=('Name','Site')
+if want_gk:
+    header+=('Grid','Gatekeeper')
+header+=('Factory','Entry','State','Activity','ActvtyTime')
 
 print
-print print_mask%('Name','Site','Factory','Entry','State','Activity','ActvtyTime')
+print print_mask%header
 print
 
 for vm_name in keys:
@@ -72,6 +93,11 @@ for vm_name in keys:
     if cel['EnteredCurrentActivity']!='???':
         cel['EnteredCurrentActivity']=fmt_time(long(cel['EnteredCurrentActivity']))
     
-    print print_mask%(vm_name,cel['GLIDEIN_Site'],"%s@%s"%(cel['GLIDEIN_Name'],cel['GLIDEIN_Factory']),cel['GLIDEIN_Entry_Name'],cel['State'],cel['Activity'],cel['EnteredCurrentActivity'])
+    print_arr=(vm_name,cel['GLIDEIN_Site'])
+    if want_gk:
+        print_arr+=(cel['GLIDEIN_GridType'],cel['GLIDEIN_Gatekeeper'])
+    print_arr+=("%s@%s"%(cel['GLIDEIN_Name'],cel['GLIDEIN_Factory']),cel['GLIDEIN_Entry_Name'],cel['State'],cel['Activity'],cel['EnteredCurrentActivity'])
+
+    print print_mask%print_arr
 
 print
