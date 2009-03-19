741e1e61fe78cfcb903b6638d5f86d3401db2b3a
sfiligoi <sfiligoi>
2009-03-19 19:38:08 +0000

    Fix typo

--- frontend/glideinFrontendMonitoring.py
+++ frontend/glideinFrontendMonitoring.py
@@ -160,8 +160,8 @@ class groupStats:
         xml_str=('<?xml version="1.0" encoding="ISO-8859-1"?>\n\n'+
                  '<VOFrontendGroupStats>\n'+
                  self.get_xml_updated(indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n"+
-                 xmlFormat.class2string(self.data['Jobs'],'Jobs',indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n",
-                 xmlFormat.class2string(self.data['Slots'],'Slots',indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n",
+                 xmlFormat.class2string(self.data['Jobs'],'Jobs',indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n"+
+                 xmlFormat.class2string(self.data['Slots'],'Slots',indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n"+
                  "</VOFrontendGroupStats>\n")
         monitoringConfig.write_file("frontend_status.xml",xml_str)
 
@@ -191,6 +191,243 @@ class groupStats:
         self.files_updated=self.updated        
         return
 
+########################################################################
+    
+class factoryStats:
+    def __init__(self):
+        self.data={}
+        self.updated=time.time()
+
+        self.files_updated=None
+        self.attributes={'Jobs':("Idle","OldIdle","Running","Total"),
+                         'Matched':("Idle","OldIdle","Running","Total"),
+                         'Requested':("Idle","MaxRun"),
+                         'Slots':("Idle","Running","Total")}
+
+
+    def logJobs(self,client_name,qc_status):
+        if self.data.has_key(client_name):
+            t_el=self.data[client_name]
+        else:
+            t_el={}
+            self.data[client_name]=t_el
+
+        el={}
+        t_el['Status']=el
+
+        status_pairs=((1,"Idle"), (2,"Running"), (5,"Held"), (1001,"Wait"),(1002,"Pending"),(1010,"StageIn"),(1100,"IdleOther"),(4010,"StageOut"))
+        for p in status_pairs:
+            nr,str=p
+            if qc_status.has_key(nr):
+                el[str]=qc_status[nr]
+            else:
+                el[str]=0
+        self.updated=time.time()
+
+    def logRequest(self,client_name,requests,params):
+        """
+        requests is a dictinary of requests
+        params is a dictinary of parameters
+
+        At the moment, it looks only for
+          'IdleGlideins'
+          'MaxRunningGlideins'
+        """
+        if self.data.has_key(client_name):
+            t_el=self.data[client_name]
+        else:
+            t_el={}
+            self.data[client_name]=t_el
+
+        el={}
+        t_el['Requested']=el
+
+        if requests.has_key('IdleGlideins'):
+            el['Idle']=requests['IdleGlideins']
+        if requests.has_key('MaxRunningGlideins'):
+            el['MaxRun']=requests['MaxRunningGlideins']
+
+        el['Parameters']=copy.deepcopy(params)
+
+        self.updated=time.time()
+
+    def logClientMonitor(self,client_name,client_monitor,client_internals):
+        """
+        client_monitor is a dictinary of monitoring info
+        client_internals is a dictinary of internals
+
+        At the moment, it looks only for
+          'Idle'
+          'Running'
+          'GlideinsIdle'
+          'GlideinsRunning'
+          'GlideinsTotal'
+          'LastHeardFrom'
+        """
+        if self.data.has_key(client_name):
+            t_el=self.data[client_name]
+        else:
+            t_el={}
+            self.data[client_name]=t_el
+
+        el={}
+        t_el['ClientMonitor']=el
+
+        for karr in (('Idle','JobsIdle'),('Running','JobsRunning'),('GlideinsIdle','GlideIdle'),('GlideinsRunning','GlideRunning'),('GlideinsTotal','GlideTotal')):
+            ck,ek=karr
+            if client_monitor.has_key(ck):
+                el[ek]=client_monitor[ck]
+
+        if client_internals.has_key('LastHeardFrom'):
+            el['InfoAge']=int(time.time()-long(client_internals['LastHeardFrom']))
+            el['InfoAgeAvgCounter']=1 # used for totals since we need an avg in totals, not absnum 
+
+        self.updated=time.time()
+
+    def get_data(self):
+        data1=copy.deepcopy(self.data)
+        for f in data1.keys():
+            fe=data1[f]
+            for w in fe.keys():
+                el=fe[w]
+                for a in el.keys():
+                    if a[-10:]=='AvgCounter': # do not publish avgcounter fields... they are internals
+                        del el[a]
+            
+        return data1
+
+    def get_xml_data(self,indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=""):
+        data=self.get_data()
+        return xmlFormat.dict2string(data,
+                                     dict_name="frontends",el_name="frontend",
+                                     subtypes_params={"class":{'subclass_params':{'Requested':{'dicts_params':{'Parameters':{'el_name':'Parameter'}}}}}},
+                                     indent_tab=indent_tab,leading_tab=leading_tab)
+
+    def get_total(self):
+        total={'Status':None,'Requested':None,'ClientMonitor':None}
+
+        for f in self.data.keys():
+            fe=self.data[f]
+            for w in fe.keys():
+                if total.has_key(w): # ignore eventual not supported classes
+                    el=fe[w]
+                    tel=total[w]
+
+                    if tel==None:
+                        # first one, just copy over
+                        total[w]={}
+                        tel=total[w]
+                        for a in el.keys():
+                            if type(el[a])==type(1): # copy only numbers
+                                tel[a]=el[a]
+                    else:
+                        # successive, sum 
+                        for a in el.keys():
+                            if type(el[a])==type(1): # consider only numbers
+                                if tel.has_key(a):
+                                    tel[a]+=el[a]
+                            # if other frontends did't have this attribute, ignore
+                        # if any attribute from prev. frontends are not in the current one, remove from total
+                        for a in tel.keys():
+                            if not el.has_key(a):
+                                del tel[a]
+                            elif type(el[a])!=type(1):
+                                del tel[a]
+        
+        for w in total.keys():
+            if total[w]==None:
+                del total[w] # remove entry if not defined
+            else:
+                tel=total[w]
+                for a in tel.keys():
+                    if a[-10:]=='AvgCounter':
+                        # this is an average counter, calc the average of the referred element
+                        # like InfoAge=InfoAge/InfoAgeAvgCounter
+                        aorg=a[:-10]
+                        tel[aorg]=tel[aorg]/tel[a]
+                        # the avgcount totals are just for internal purposes
+                        del tel[a]
+
+        return total
+    
+    def get_xml_total(self,indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=""):
+        total=self.get_total()
+        return xmlFormat.class2string(total,
+                                      inst_name="total",
+                                      indent_tab=indent_tab,leading_tab=leading_tab)
+
+    def get_updated():
+        return self.updated
+
+    def get_xml_updated(self,indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=""):
+        xml_updated={"UTC":{"unixtime":timeConversion.getSeconds(self.updated),
+                            "ISO8601":timeConversion.getISO8601_UTC(self.updated),
+                            "RFC2822":timeConversion.getRFC2822_UTC(self.updated)},
+                     "Local":{"ISO8601":timeConversion.getISO8601_Local(self.updated),
+                              "RFC2822":timeConversion.getRFC2822_Local(self.updated),
+                              "human":timeConversion.getHuman(self.updated)}}
+        return xmlFormat.dict2string(xml_updated,
+                                     dict_name="updated",el_name="timezone",
+                                     subtypes_params={"class":{}},
+                                     indent_tab=indent_tab,leading_tab=leading_tab)
+
+
+    def write_file(self):
+        global monitoringConfig
+
+        if (self.files_updated!=None) and ((self.updated-self.files_updated)<5):
+            # files updated recently, no need to redo it
+            return 
+        
+
+        # write snaphot file
+        xml_str=('<?xml version="1.0" encoding="ISO-8859-1"?>\n\n'+
+                 '<glideFactoryEntryQStats>\n'+
+                 self.get_xml_updated(indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n"+
+                 self.get_xml_data(indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n"+
+                 self.get_xml_total(indent_tab=xmlFormat.DEFAULT_TAB,leading_tab=xmlFormat.DEFAULT_TAB)+"\n"+
+                 "</glideFactoryEntryQStats>\n")
+        monitoringConfig.write_file("schedd_status.xml",xml_str)
+
+        data=self.get_data()
+        total_el=self.get_total()
+
+        # update RRDs
+        type_strings={'Status':'Status','Requested':'Req','ClientMonitor':'Client'}
+        for fe in [None]+data.keys():
+            if fe==None: # special key == Total
+                fe_dir="total"
+                fe_el=total_el
+            else:
+                fe_dir="frontend_"+fe
+                fe_el=data[fe]
+
+            val_dict={}
+            monitoringConfig.establish_dir(fe_dir)
+            for tp in fe_el.keys():
+                # type - Status, Requested or ClientMonitor
+                if not (tp in self.attributes.keys()):
+                    continue
+
+                tp_str=type_strings[tp]
+
+                attributes_tp=self.attributes[tp]
+                for a in attributes_tp:
+                    val_dict["%s%s"%(tp_str,a)]=None #init, so that gets created properly
+                
+                fe_el_tp=fe_el[tp]
+                for a in fe_el_tp.keys():
+                    if a in attributes_tp:
+                        a_el=fe_el_tp[a]
+                        if type(a_el)!=type({}): # ignore subdictionaries
+                            val_dict["%s%s"%(tp_str,a)]=a_el
+                
+            monitoringConfig.write_rrd_multi("%s/Status_Attributes"%fe_dir,
+                                             "GAUGE",self.updated,val_dict)
+
+        self.files_updated=self.updated        
+        return
+    
 ############### P R I V A T E ################
 
 ##################################################
