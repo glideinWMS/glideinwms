e9cd400931ae3d43fc0d39d4a8c8ed3350c29138
sfiligoi <sfiligoi>
2009-03-20 19:49:12 +0000

    Use the proper names

--- creation/web_base/frontend_support.js
+++ creation/web_base/frontend_support.js
@@ -6,23 +6,22 @@
  *
  */
 
-// To be finished... mostly a placeholder for now
 
-// Load FrontendQStats XML file and return the object
-function loadFrontendQStats() {
+// Load FrontendStats XML file and return the object
+function loadFrontendStats() {
   var request =  new XMLHttpRequest();
-  request.open("GET", "schedd_status.xml",false);
+  request.open("GET", "frontend_status.xml",false);
   request.send(null);
   
-  var frontendQStats=request.responseXML.firstChild;
-  return frontendQStats;
+  var frontendStats=request.responseXML.firstChild;
+  return frontendStats;
 }
 
-// Extract group names from a frontendQStats XML object
-function getFrontendGroups(frontendQStats) {
+// Extract group names from a frontendStats XML object
+function getFrontendGroups(frontendStats) {
   groups=new Array();
-  for (var elc=0; elc<frontendQStats.childNodes.length; elc++) {
-    var el=frontendQStats.childNodes[elc];
+  for (var elc=0; elc<frontendStats.childNodes.length; elc++) {
+    var el=frontendStats.childNodes[elc];
     if ((el.nodeType==1) && (el.nodeName=="groups")) {
       for (var etc=0; etc<el.childNodes.length; etc++) {
 	var group=el.childNodes[etc];
