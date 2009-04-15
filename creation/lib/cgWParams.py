883300c01ad89fbd68e3350f0c3ad0f61b26bcdb
sfiligoi <sfiligoi>
2009-04-15 18:41:57 +0000

    Fix typo

--- creation/lib/cgWParams.py
+++ creation/lib/cgWParams.py
@@ -170,7 +170,7 @@ class GlideinParams(cWParams.CommonParams):
             if not cWParams.is_valid_name(attr_name):
                 raise RuntimeError, "Invalid global attribute name '%s'."%attr_name
         for entry_name in entry_names:
-            attr_names=self.enties[entry_name].attrs.keys()
+            attr_names=self.entries[entry_name].attrs.keys()
             for attr_name in attr_names:
                 if not cWParams.is_valid_name(attr_name):
                     raise RuntimeError, "Invalid entry '%s' attribute name '%s'."%(entry_name,attr_name)
