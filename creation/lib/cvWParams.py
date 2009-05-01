61ce267a02406b86c6e566910c572e15425715f1
sfiligoi <sfiligoi>
2009-05-01 19:09:46 +0000

    Increase default running.fraction to 1.15

--- creation/lib/cvWParams.py
+++ creation/lib/cvWParams.py
@@ -44,7 +44,7 @@ class VOFrontendParams(cWParams.CommonParams):
         
         group_config_running_defaults=cWParams.commentedOrderedDict()
         group_config_running_defaults["max"]=['10000',"nr_jobs","What is the max number of running glideins I want to get to",None]
-        group_config_running_defaults["relative_to_queue"]=['1.05',"fraction","Max relative to number of matching jobs in the queue.",None]
+        group_config_running_defaults["relative_to_queue"]=['1.15',"fraction","Max relative to number of matching jobs in the queue.",None]
         group_config_defaults['running_glideins_per_entry']=group_config_running_defaults
 
         group_config_idle_defaults=cWParams.commentedOrderedDict()
