9d8ea1e2f4e62fdfb48791a23215cc9b88911f16
sfiligoi <sfiligoi>
2009-05-08 20:11:30 +0000

    Copy over factoryCompletedStats.html

--- creation/lib/cgWParamDict.py
+++ creation/lib/cgWParamDict.py
@@ -110,7 +110,8 @@ class glideinMainDicts(cgWDictFile.glideinMainDicts):
 
         for mfarr in ((params.src_dir,'factoryRRDBrowse.html'),
                       (params.src_dir,'factoryStatus.html'),
-                      (params.src_dir,'factoryLogStatus.html')):
+                      (params.src_dir,'factoryLogStatus.html'),
+                      (params.src_dir,'factoryCompletedStats.html')):
             mfdir,mfname=mfarr
             mfobj=cWDictFile.SimpleFile(mfdir,mfname)
             mfobj.load()
