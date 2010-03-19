f5249771166d889f8598c0ce27864ac7140dfc1c
sfiligoi <sfiligoi>
2010-03-19 22:30:46 +0000

    Add RSA key generation by default

--- creation/lib/cgWParams.py
+++ creation/lib/cgWParams.py
@@ -131,7 +131,7 @@ class GlideinParams(cWParams.CommonParams):
 
         
         security_default=cWParams.commentedOrderedDict()
-        security_default["pub_key"]=("None","None|RSA","Type of public key system used for secure message passing",None)
+        security_default["pub_key"]=("RSA","None|RSA","Type of public key system used for secure message passing",None)
         security_default["key_length"]=("2048","bits","Key length in bits",None)
         security_default["allow_proxy"]=("frontend","list","What proxies can be used for glidein submission? (list combination of factory,frontend)",None)
         security_default["frontends"]=(xmlParse.OrderedDict(),"Dictionary of frontend","Each frontend contains",self.frontend_defaults)
