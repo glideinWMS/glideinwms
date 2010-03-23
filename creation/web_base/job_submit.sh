733c89be8e0473d3b645d90165688829a1dde06d
sfiligoi <sfiligoi>
2010-03-23 18:45:26 +0000

    Save condor_submit errors into a log file

--- creation/web_base/job_submit.sh
+++ creation/web_base/job_submit.sh
@@ -33,4 +33,11 @@ export GLIDEIN_SCHEDD=`grep -i "^Schedd "  entry_${GLIDEIN_ENTRY_NAME}/job.descr
 export GLIDEIN_VERBOSITY=`grep -i "^Verbosity "  entry_${GLIDEIN_ENTRY_NAME}/job.descript | awk '{print $2}'`
 
 export GLIDEIN_LOGNR=`date +%Y%m%d`
-condor_submit -name $GLIDEIN_SCHEDD entry_${GLIDEIN_ENTRY_NAME}/job.condor
+
+log_base_dir=`grep -i "^ClientLogBaseDir " glidein.descript | awk '{print $2}'`
+glidein_name=`grep -i "^GlideinName " glidein.descript | awk '{print $2}'`
+log_dir="${log_base_dir}/user_${GLIDEIN_USER}/glidein_${glidein_name}/entry_${GLIDEIN_ENTRY_NAME}"
+error_log_fname="${log_dir}/submit_${GLIDEIN_LOGNR}_${GLIDEIN_CLIENT}.log"
+
+echo "`date '+%y/%m/%d %H:%M'` Submitting ${GLIDEIN_COUNT} glideins using X509 id ${GLIDEIN_X509_ID=}" >> "$error_log_fname"
+condor_submit -name $GLIDEIN_SCHEDD entry_${GLIDEIN_ENTRY_NAME}/job.condor 2>> "$error_log_fname"
