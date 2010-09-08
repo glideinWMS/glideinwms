4071816cf7666aa4f43d2c843bc77b12e96f18da
sfiligoi <sfiligoi>
2010-09-08 00:22:05 +0000

    Add advertise_with_multiple config option and the related AdvertiseWithMultiple in the glidein descript

--- creation/lib/cgWParamDict.py
+++ creation/lib/cgWParamDict.py
@@ -3,7 +3,7 @@
 #   glideinWMS
 #
 # File Version: 
-#   $Id: cgWParamDict.py,v 1.123.2.5.8.3.4.1 2010/09/02 20:05:56 sfiligoi Exp $
+#   $Id: cgWParamDict.py,v 1.123.2.5.8.3.4.2 2010/09/08 00:22:05 sfiligoi Exp $
 #
 # Description:
 #   Glidein creation module
@@ -543,6 +543,7 @@ def populate_factory_descript(work_dir,
         glidein_dict.add('LoopDelay',params.loop_delay)
         glidein_dict.add('AdvertiseDelay',params.advertise_delay)
         glidein_dict.add('AdvertiseWithTCP',params.advertise_with_tcp)
+        glidein_dict.add('AdvertiseWithMultiple',params.advertise_with_multiple)
         glidein_dict.add('RestartAttempts',params.restart_attempts)
         glidein_dict.add('RestartInterval',params.restart_interval)
         glidein_dict.add('AdvertiseDelay',params.advertise_delay)
