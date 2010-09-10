23fa2aa92f5f112ca03d48d3772db8787587a2d7
sfiligoi <sfiligoi>
2010-09-10 22:04:52 +0000

    Make it more generic; don't assume the values are integers - merge from v2plus

--- lib/rrdSupport.py
+++ lib/rrdSupport.py
@@ -3,7 +3,7 @@
 #   glideinWMS
 #
 # File Version: 
-#   $Id: rrdSupport.py,v 1.8.26.1 2010/08/31 18:49:17 parag Exp $
+#   $Id: rrdSupport.py,v 1.8.26.1.6.1 2010/09/10 22:04:52 sfiligoi Exp $
 #
 # Description:
 #   This module implements the basic functions needed
@@ -127,7 +127,7 @@ class BaseRRDSupport:
 
         lck=self.get_disk_lock(rrdfname)
         try:
-            self.rrd_obj.update(str(rrdfname),'%li:%i'%(time,val))
+            self.rrd_obj.update(str(rrdfname),'%li:%s'%(time,val))
         finally:
             lck.close()
 
@@ -156,7 +156,7 @@ class BaseRRDSupport:
         ds_vals=[]
         for ds_name in ds_names:
             if val_dict[ds_name]!=None:
-                ds_vals.append("%i"%val_dict[ds_name])
+                ds_vals.append("%s"%val_dict[ds_name])
                 ds_names_real.append(ds_name)
 
         if len(ds_names_real)==0:
