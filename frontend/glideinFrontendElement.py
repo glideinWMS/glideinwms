dde52c967a2b01de8a3b5c4e96c08ecb903fd8fe
sfiligoi <sfiligoi>
2010-11-19 23:44:42 +0000

    Print RunningHere in logs - merge from tag_v2_4_2_jeff_fdxml_3_log_1

--- frontend/glideinFrontendElement.py
+++ frontend/glideinFrontendElement.py
@@ -4,7 +4,7 @@
 #   glideinWMS
 #
 # File Version: 
-#   $Id: glideinFrontendElement.py,v 1.52.2.14 2010/11/19 23:15:13 sfiligoi Exp $
+#   $Id: glideinFrontendElement.py,v 1.52.2.15 2010/11/19 23:44:42 sfiligoi Exp $
 #
 # Description:
 #   This is the main of the glideinFrontend
@@ -67,7 +67,7 @@ def log_and_sum_factory_line(factory,is_down,factory_stat_arr,old_factory_stat_a
     else:
         down_str="Up  "
 
-    glideinFrontendLib.log_files.logActivity(("%s(%s %s) %s(%s) | %s %s %s | %s %s "%tuple(form_arr))+
+    glideinFrontendLib.log_files.logActivity(("%s(%s %s) %s(%s %s) | %s %s %s | %s %s "%tuple(form_arr))+
                                              ("%s %s"%(down_str,factory)))
 
     new_arr=[]
@@ -77,13 +77,13 @@ def log_and_sum_factory_line(factory,is_down,factory_stat_arr,old_factory_stat_a
 
 def init_factory_stats_arr():
     new_arr=[]
-    for i in range(10):
+    for i in range(11):
         new_arr.append(0)
     return new_arr
 
 def log_factory_header():
-    glideinFrontendLib.log_files.logActivity("     Jobs in schedd queues      |      Glideins     |   Request   ")
-    glideinFrontendLib.log_files.logActivity("Idle ( eff   old )  Run ( max ) | Total Idle   Run  | Idle MaxRun Down Factory")
+    glideinFrontendLib.log_files.logActivity("        Jobs in schedd queues         |      Glideins     |   Request   ")
+    glideinFrontendLib.log_files.logActivity("Idle ( eff   old )  Run ( here  max ) | Total Idle   Run  | Idle MaxRun Down Factory")
 
 ############################################################
 def iterate_one(client_name,elementDescript,paramsDescript,signatureDescript,x509_proxy_plugin,stats):
@@ -327,7 +327,7 @@ def iterate_one(client_name,elementDescript,paramsDescript,signatureDescript,x50
             glidein_min_idle=0 
         # we don't need more slots than number of jobs in the queue (unless the fraction is positive)
         glidein_max_run=int((count_jobs['Idle']+count_jobs['Running'])*fraction_running+1)
-        this_stats_arr=(count_jobs['Idle'],effective_idle,count_jobs['OldIdle'],count_jobs['Running'],max_running,
+        this_stats_arr=(count_jobs['Idle'],effective_idle,count_jobs['OldIdle'],count_jobs['Running'],count_real[glideid],max_running,
                         count_status['Total'],count_status['Idle'],count_status['Running'],
                         glidein_min_idle,glidein_max_run)
 
