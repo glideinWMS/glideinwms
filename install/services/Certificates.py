2aa39666e1c7a5db7d62cc976baea45c7061ac2a
weigand <weigand>
2010-12-20 20:41:07 +0000

    Changed the 'certificates' option name to 'x509_cert_dir'. . This affects the WMSCollector, UserCollector, Submit and VOFrontend ini file sections. . If the CAs are installed from the VDT, the installer will look for the presence of *.0 and *.r0 files.

--- install/services/Certificates.py
+++ install/services/Certificates.py
@@ -3,7 +3,14 @@
 import common
 from VDT import VDT
 #---------------------
-import sys,os,os.path,string,time,glob,pwd
+import sys
+import os
+import os.path
+import string
+import time
+import glob
+import pwd
+import commands
 
 #STARTUP_DIR=sys.path[0]
 #sys.path.append(os.path.join(STARTUP_DIR,"../lib"))
@@ -13,31 +20,66 @@ class Certificates(VDT):
   def __init__(self,inifile,section):
     self.ini_section = section
     VDT.__init__(self,self.ini_section,inifile)
-
+    self.option = "x509_cert_dir"
     self.package = "%s:CA-Certificates" % (self.vdt_cache())
     self.vdt_services = ["fetch-crl", "vdt-rotate-logs", "vdt-update-certs",]
    
   #-------------------
-  def certificates(self):
-    return self.option_value(self.ini_section,"certificates")
+  def x509_cert_dir(self):
+    """ Returns the full path the X509 certificates directory.  """
+    cert_dir = self.option_value(self.ini_section,self.option)
+    if len(cert_dir) == 0:
+      common.logerr("""The %s option was empty and required.""" % self.option)  
+    return cert_dir
+
+    ## ---------------------------------------------------------------------
+    ## --- leaving this code here in the event it becomes a requirement the
+    ## --- x509_cert_dir option is allowed to be empty.  I think it is not
+    ## --- a good idea to do this.  It hides things when one does.
+##        Population of the  x509_cert_dir option is optional.
+##        If populated, it will be used.
+##        If not populated, it will be assumed to be an OSG/VDT CA installation
+##        and the value will be obtained from the X509_CERT_DIR variable set
+##        in the VDT setup.sh script.
+    ##if len(cert_dir) == 0:
+    ##  cert_dir = self.get_x509_cert_dir_value()
+    ##  if len(cert_dir) == 0:
+    ##    common.logerr("""The %(option)s was empty and certificates do not appear to be 
+##installed as a part of a VDT install based on your vdt_location option: 
+##  %(vdt_location)s""" % \
+##            { "option"       : self.option,
+##              "vdt_location" : self.vdt_location(),})
+    ## ---------------------------------------------------------------------
 
   #-------------------
   def install(self):
+    """ Installs the VDT CA package if X509 CA certiificates do not already
+        exist. 
+    """
     common.logit("\nVerifying CA Certificates installation")
     if self.certificates_exist():
-      common.logit("... CA Certificates found in: %s" % self.certificate_dir())
+      common.logit("... CA Certificates (%(option)s) exist: %(dir)s" % \
+       { "option" : self.option,
+         "dir"    : self.x509_cert_dir()})
       return
-    common.ask_continue("""... CA Certificates not found in: %s
+    common.ask_continue("""... CA Certificates (%(option)s) not found: 
+  %(dir)s
 This script is checking for the presence of CA (*.0) and CRL (*.r0) files.
-Is it OK to install it in this location""" % self.certificate_dir())
-    if common.not_writeable(self.certificates()):
-      common.logerr("You do not have permissions to write in this directory: %s" % self.certificates())
-    common.logit("\nCA certificates install starting")
-    common.logit("The packages that will be installed are:")
-    common.logit("  %s" % self.package)
+Is it OK to install it in this location""" % \
+       { "option" : self.option,
+         "dir"    : self.x509_cert_dir(),})
+    if common.not_writeable(os.path.dirname(self.x509_cert_dir())):
+      common.logerr("""You do not have permissions to write in the directory specified 
+by the %(option)s: %(dir)s""" % \
+       { "option" : self.option,
+         "dir"    : self.x509_cert_dir(),})
+    common.logit(""" CA certificates install starting. The packages that will be installed are:
+   %(package)s""" % { "package" : self.package,})
     self.install_vdt_package(self.package)
     common.logit("... retrieving certificates") 
-    common.run_script("source %s/setup.sh; %s/vdt/bin/vdt-ca-manage setupca --location %s --url osg" % (self.vdt_location(),self.vdt_location(),self.certificates()))
+    common.run_script("source %(vdt_location)s/setup.sh; %(vdt_location)s/vdt/bin/vdt-ca-manage setupca --location %(dir)s --url osg" % \
+       { "vdt_location" : self.vdt_location(),
+         "dir"          : os.path.dirname(self.x509_cert_dir())})
     self.create_crontab()
     if self.certificates_exist():
       common.logit("... certificate installation looks good")
@@ -56,19 +98,26 @@ Is it OK to install it in this location""" % self.certificate_dir())
       non_root_arg = " --non-root"
 
     for service in self.vdt_services:
-      common.logit("\n...... %s" % service)
-      common.run_script("source %s/setup.sh;vdt-control %s --enable %s;vdt-control %s --on %s" % (self.vdt_location(),non_root_arg,service,non_root_arg,service))
+      common.logit("\n...... %(service)s" % { "service" :service,})
+      common.run_script("source %(vdt_location)s/setup.sh;vdt-control %(non_root_arg)s --enable %(service)s;vdt-control %(non_root_arg)s --on %(service)s" % \
+           { "vdt_location" : self.vdt_location(),
+             "non_root_arg" : non_root_arg,
+             "service"      : service,} )
     common.logit("\nvdt-control --list")
-    os.system("source %s/setup.sh;vdt-control --list" % self.vdt_location())
+    os.system("source %(vdt_location)s/setup.sh;vdt-control --list" % \
+           { "vdt_location" : self.vdt_location(),})
 
     #-- show the cron entries added - extract the lines put in cron
-    common.logit("\n... %s crontab entries:" % pwd.getpwuid(os.getuid())[0])
-    services_file = "%s/vdt/services/state" % (self.vdt_location())
+    common.logit("\n... %(user)s crontab entries:" % \
+         { "user" : pwd.getpwuid(os.getuid())[0],})
+    services_file = "%(vdt_location)s/vdt/services/state" % \
+         { "vdt_location" : self.vdt_location(),}
     try:
       fd = open(services_file,'r')
       lines = fd.readlines()
     except:
-      common.logerr("Unable to read VDT services file: %s" % services_file)
+      common.logerr("Unable to read VDT services file: %(services_file)s" % 
+         { "services_file" : services_file,})
     fd.close()
     fetch_crl_script = None
     for line in lines:
@@ -76,7 +125,9 @@ Is it OK to install it in this location""" % self.certificate_dir())
       if (els[1] != 'cron'):
         continue # not a cron line
       if els[0] in self.vdt_services:
-         common.logit("  %s %s" % (els[4],els[5].rstrip()))
+         common.logit("  %(cron_time)s %(cron_process)s" % \
+             { "cron_time"    : els[4],
+               "cron_process" : els[5].rstrip(),})
       if els[0] == "fetch-crl":
         fetch_crl_script = els[5].rstrip()
     common.ask_continue("""\n... the glidein services require that CRL files (*.r0) be present
@@ -88,32 +139,43 @@ in the certificates directory.  Is it OK to run the script now?""")
 
   #-------------------
   def certificates_exist(self):
-    """ Returns true of certificates already exist. """
-    ca_files = glob.glob(os.path.join(self.certificate_dir(), '*.0'))
-    crl_files = glob.glob(os.path.join(self.certificate_dir(), '*.r0'))
+    """ Returns true if certificates already exist. 
+        This is determined by loooking for *.r0 and *.0 files in the
+        directory.
+    """
+    ca_files = glob.glob(os.path.join(self.x509_cert_dir(), '*.0'))
+    crl_files = glob.glob(os.path.join(self.x509_cert_dir(), '*.r0'))
     if len(ca_files) > 0:
       if len(crl_files) > 0:
         return True
-      common.logerr("""The certificates directory contains CA (*.0) files but 
+      common.logerr("""The %(option)s directory contains CA (*.0) files but
 no CRL (*.r0) files.  This is not a satifactory condition.
 Suggest you check this out before proceeding:
-   %s""" % self.certificate_dir())
+   %(dir)s""" % \
+        { "option" : self.option,
+           "dir"   : self.x509_cert_dir(),})
     #-- checking for other files to insure we are in the correct directory
-    files = glob.glob(os.path.join(self.certificate_dir(), '*'))
+    files = glob.glob(os.path.join(self.x509_cert_dir(), '*'))
     if len(files) > 0:
-      common.logerr("""CA Certificates files (*.0) do not exist in certificates
-directory BUT some other files do exist.  This does not make sense. 
+      common.logerr("""CA Certificates (%(option)s) files (*.0) do not exist in 
+certificates directory BUT some other files do exist.  This does not make sense.
 Suggest you check this out before proceeding:
-   %s""" % self.certificate_dir())
+   %(dir)s""" % \
+        { "option" : self.option,
+           "dir"   : self.x509_cert_dir(),})
     #-- looks good.. we can proceed with an install
     return False
 
   #-------------------
-  def certificate_dir(self):
-    """ The certificates directory is determined by the VDT. 
-        We have to get it from the X509 vaariable.
-    """
-    return os.path.join(self.certificates(), 'certificates')
+  def get_x509_cert_dir_value(self):
+    """ Returns the X509_CERT_DIR variable from an OSG/VDT CA install.""" 
+    cert_dir = ""
+    vdt_script = "%s/setp.sh" % self.vdt_location()
+    if os.path.exists(vdt_script):
+      (status, cert_dir) = commands.getstatusoutput("source %s;echo $X509_CERT_DIR" % vdt_script)
+      if status > 0:
+        cert_dir = ""
+    return cert_dir
 
 ##########################################
 def main(argv):
