98a7acdfbce1d21c9309fab61f1609b2815885d5
tiradani <tiradani>
2011-04-19 19:10:48 +0000

    Adding support for python's built in logging module.  For now, it will be used for development debugging, but it can be the basis for replacing the custom logging.  It does implement the custom log rotation based on file age AND file size.

--- lib/logSupport.py
+++ lib/logSupport.py
@@ -2,18 +2,24 @@
 # Project:
 #   glideinWMS
 #
-# File Version: 
-#   $Id: logSupport.py,v 1.21.10.2 2010/09/08 03:10:47 parag Exp $
+# File Version:
+#   $Id: logSupport.py,v 1.21.10.2.4.1 2011/04/19 19:10:48 tiradani Exp $
 #
 # Description: log support module
 #
 # Author:
 #  Igor Sfiligoi (Oct 25th 2006)
 #
-import os,os.path,stat
+import os
+import os.path
+import stat
+import sys
 import timeConversion
 import time
 import re
+import logging
+from logging import config
+from logging.handlers import TimedRotatingFileHandler
 
 # this class can be used instead of a file for writing
 class DayLogFile:
@@ -41,7 +47,7 @@ class DayLogFile:
                 raise
         finally:
             fd.close()
-        
+
         return
 
     ##########################
@@ -50,7 +56,7 @@ class DayLogFile:
 
     def write_on_exception(self,exception_msg,msg):
         print "%s: %s" % (exception_msg,msg)
-        return 
+        return
 
     def get_fname(self,timestamp):
         return "%s.%s.%s"%(self.base_fname,time.strftime("%Y%m%d",time.localtime(timestamp)),self.extension)
@@ -109,7 +115,7 @@ class DirCleanup:
         for fname in fnames:
             if self.fname_expression_obj.match(fname)==None:
                 continue # ignore files that do not match
-            
+
             fpath=os.path.join(self.dirname,fname)
             fstat=os.lstat(fpath)
             fmode=fstat[stat.ST_MODE]
@@ -140,7 +146,7 @@ class DirCleanupWSpace(DirCleanup):
     def cleanup(self):
         count_removes=0
         count_removes_bytes=0L
-        
+
         min_treshold_time=time.time()-self.minlife
         treshold_time=time.time()-self.maxlife
 
@@ -150,7 +156,7 @@ class DirCleanupWSpace(DirCleanup):
         fpaths.sort(lambda i,j:cmp(files_wstats[i][stat.ST_MTIME],files_wstats[j][stat.ST_MTIME]))
 
         # first calc the amount of space currently used
-        used_space=0L        
+        used_space=0L
         for fpath in fpaths:
             fstat=files_wstats[fpath]
             fsize=fstat[stat.ST_SIZE]
@@ -172,9 +178,134 @@ class DirCleanupWSpace(DirCleanup):
                 except:
                    if self.warning_log!=None:
                        self.warning_log.write("Could not remove %s"%fpath)
-                
+
         if count_removes>0:
             if self.activity_log!=None:
                 self.activity_log.write("Removed %i files for %.2fMB."%(count_removes,count_removes_bytes/(1024.0*1024.0)))
         return
 
+# Adding in the capability to use the built in Python logging Module
+# This will allow us to log anything, anywhere
+#
+# Note:  We may need to create a custom class later if we need to handle
+#        logging with privilege separation
+
+class GlideinHandler(TimedRotatingFileHandler):
+    """
+    Custom logging handler class for glideinWMS.  It combines the decision tree
+    for log rotation from the TimedRotatingFileHandler with the decision tree
+    from the RotatingFileHandler.  This allows us to specify a lifetime AND
+    file size to determine when to rotate the file.
+
+    This class assumes that the lifetime specified is in days. (24 hour periods)
+
+    @type filename: string
+    @ivar filename: Full path to the log file.  Includes file name.
+    @type interval: int
+    @ivar interval: Number of days to keep log file before rotating
+    @type maxBytes: int
+    @ivar maxBytes: Maximum file size before rotating the log file
+    @type backupCount: int
+    @ivar backupCount: How many backups to keep
+    """
+    def __init__(self, filename, interval=1, maxBytes=0, backupCount=0):
+        """
+        Initialize the Handler.  We assume the following:
+
+            1. Interval is in days
+            2. No special encoding
+            3. No delays are set
+            4. Timestamps are not in UTC
+
+        @type filename: string
+        @param filename: The full path of the log file
+        @type interval: int
+        @param interval: Number of days before file rotation
+        @type maxBytes: int
+        @param maxBytes: Maximum size of the logfile bytes before file rotatation
+        @type backupCount: int
+        @param backupCount: Number of backups to keep
+
+        """
+        when='D'
+        TimedRotatingFileHandler.__init__(self, filename, when, interval, backupCount, encoding=None)
+        self.maxBytes = maxBytes * 1024.0 * 1024.0
+
+    def shouldRollover(self, record):
+        """
+        Determine if rollover should occur.
+
+        Basically, we are combining the checks for size and time interval
+
+        @type record: string
+        @param record: The message that will be logged.
+        """
+        do_timed_rollover = logging.handlers.TimedRotatingFileHandler.shouldRollover(self, record)
+        do_size_rollover = 0
+        if self.stream is None:                 # delay was set...
+            self.stream = self._open()
+        if self.maxBytes > 0:                   # are we rolling over?
+            msg = "%s\n" % self.format(record)
+            self.stream.seek(0, 2)  #due to non-posix-compliant Windows feature
+            if self.stream.tell() + len(msg) >= self.maxBytes:
+                do_size_rollover = 1
+
+        return do_timed_rollover or do_size_rollover
+
+
+def add_glideinlog_handler(logger_name, log_dir, maxDays, maxBytes, debug=False):
+    """
+    Setup python's built-in logging module.  This is designed to mimic the
+    original logging in GlideinWMS, but allow logging in every module.
+
+    In order to use this logging first import the logging module:
+
+        C{import logging}
+
+    Then get the logger by name:
+
+        C{log = logging.getLogger(name)}
+
+    finally:
+
+        C{log.info("message")}
+
+    @type logger_name: string
+    @param logger_name: The name of the logger
+    @type log_dir: string
+    @param log_dir: The directory where the log files will be placed
+    @type MaxDays: int
+    @param MaxDays: Maximum age of the logfile in days before it will be rotated
+    @type maxBytes: int
+    @param maxBytes: Maximum size in bytes of the logfile before it will be rotated
+
+    """
+    log_conf_file = "%s/logging.conf" % os.path.join(sys.path[0], "../lib")
+    config.fileConfig(log_conf_file)
+
+    mylog = logging.getLogger(logger_name)
+
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s:  %(message)s')
+
+    # Error Logger
+    logfile = os.path.expandvars("%s/%s.err.log" % (log_dir, logger_name))
+    handler = GlideinHandler(logfile, maxDays, maxBytes, backupCount=5)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.ERROR)
+    mylog.addHandler(handler)
+
+    # INFO Logger
+    logfile = os.path.expandvars("%s/%s.info.log" % (log_dir, logger_name))
+    handler = GlideinHandler(logfile, maxDays, maxBytes, backupCount=5)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    mylog.addHandler(handler)
+
+    # DEBUG Logger
+    if debug:
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)s:::%(module)s::%(lineno)d: %(message)s ')
+        logfile = os.path.expandvars("%s/%s.debug.log" % (log_dir, logger_name))
+        handler = GlideinHandler(logfile, maxDays, maxBytes, backupCount=5)
+        handler.setFormatter(formatter)
+        handler.setLevel(logging.DEBUG)
+        mylog.addHandler(handler)
