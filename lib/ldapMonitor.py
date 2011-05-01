2bb9bd8d8a9d14b01b059cdaa37d6653e30721f8
sfiligoi <sfiligoi>
2011-05-01 17:29:36 +0000

    Make BDIICEQuery more flexible, by making the LDAP base string an argument (with the default what was once hardcoded). Add SearchBDII class that can be used for more complex BDII searches. - Contributed by Ian MacNeill

--- lib/ldapMonitor.py
+++ lib/ldapMonitor.py
@@ -3,7 +3,7 @@
 #   glideinWMS
 #
 # File Version: 
-#   $Id: ldapMonitor.py,v 1.2.12.1 2010/09/08 03:10:47 parag Exp $
+#   $Id: ldapMonitor.py,v 1.2.12.1.18.1 2011/05/01 17:29:36 sfiligoi Exp $
 #
 # Description:
 #   This module implements classes to query the ldap server
@@ -63,14 +63,14 @@ class LDAPQuery:
 class BDIICEQuery(LDAPQuery):
     def __init__(self,
                  bdii_url,bdii_port=2170,     # where to find LDAP server
-                 additional_filter_str=None): # what to read
+                 additional_filter_str=None,base_string="Mds-vo-name=local,o=grid"): # what to read
         if additional_filter_str==None:
             additional_filter_str=""
             
         filter_str="&(GlueCEInfoContactString=*)%s"%additional_filter_str
 
         LDAPQuery.__init__(self,bdii_url,bdii_port,
-                           "mds-vo-name=local,o=grid",filter_str)
+                           base_string,filter_str)
 
     def fetch(self,additional_filter_str=None):
         out_data=LDAPQuery.fetch(self,additional_filter_str)
@@ -90,3 +90,93 @@ class BDIICEQuery(LDAPQuery):
                 new_data[k]=old_data[k]
         self.stored_data=new_data
 
+
+
+class SearchBDII:
+    def __init__(self,url,port=None,VO=None,CE=None,
+                 GlueCEUniqueID=None,ldapSearchStr=None,searchStr=None):
+        
+        #Store Initialization Variables
+        self.bdiiUrl=url
+        self.port=port
+        self.VO=VO
+        self.CE=CE
+        self.GlueCEUniqueID=GlueCEUniqueID
+        self.ldapSearchStr=ldapSearchStr
+        self.searchStr=searchStr
+        
+        #Initial Storage Variables
+        self.bdiiData=None
+
+        #Run Initializing Functions
+        self.query_bdii()
+        self.search_bdii_data()
+
+    def query_bdii(self):
+        """ Retrieve the information from BDII. Apply appropriate ldap filter strings.
+        """
+        selectionStr=""
+        baseStr="Mds-vo-name=local,o=grid"
+        if self.VO:
+            selectionStr=selectionStr+"(|(GlueCEAccessControlBaseRule=VO:" + self.VO + ")(GlueCEAccessControlBaseRule=VOMS:/" + self.VO + "/Role=pilot))"
+
+        if self.CE:
+            selectionStr=selectionStr+"(GlueCEInfoContactString="+self.CE+")"
+
+        if self.GlueCEUniqueID:
+            baseStr=self.GlueCEUniqueID
+        if self.ldapSearchStr:
+            selectionStr=selectionStr+self.ldapSearchStr
+        if self.port:
+            bdii_obj=BDIICEQuery(self.bdiiUrl,bdii_port=int(self.port),additional_filter_str=selectionStr,base_string=baseStr)
+        else:    
+            bdii_obj=BDIICEQuery(self.bdiiUrl,additional_filter_str=selectionStr,base_string=baseStr)
+        bdii_obj.load()
+        self.bdiiData=bdii_obj.fetchStored()
+        del bdii_obj
+    
+
+    def search_bdii_data(self):
+        """searches the base string of each retrieved entry for designated string.
+        """
+   
+        dict={}
+        ### only modify bdiiData if there is a search criteria
+        if self.searchStr:
+            found=False
+            for key in self.bdiiData.keys():
+                if self.searchStr in key:
+                    dict[key]=self.bdiiData[key]
+                    found=True
+            if found==False:
+                print "\n No entry found for search term \""+self.searchStr+"\" .\n"
+            self.bdiiData.clear()
+            self.bdiiData=dict
+        
+
+    def return_bdii_data(self):
+        """Returns a dictionary of bdii information
+        """
+        return self.bdiiData
+
+
+    def display_bdii_data(self,file=None):
+        """Display entries.
+        Args (file=None): file is the specified output file name. If blank, output is on command line.
+        """
+        if file:
+            try:
+                outFile=open(file,'w')
+                for key in self.bdiiData.keys():
+                    outFile.write("\n%s\n%s\n\n"%(key,self.bdiiData[key]))
+            except:
+                print "Error opening or closing specified file."
+        else:
+            for key in self.bdiiData.keys():
+                print "\n"
+                print key
+                print "\n"
+                print self.bdiiData[key]
+                print "\n\n"
+               
+        
