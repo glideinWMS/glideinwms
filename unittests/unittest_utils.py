01b4cf63f63ea60118fb5362a041e5c5d019a6d8
tiradani <tiradani>
2011-05-27 20:17:29 +0000

    Added explanation for why python path modification is done the way it is.  Also added in the factory and frontend directories to the python path.

--- unittests/unittest_utils.py
+++ unittests/unittest_utils.py
@@ -2,10 +2,23 @@ import os
 import sys
 import unittest
 
+"""
+Check to see if $GLIDEINWMS_LOCATION is defined.  If it is, use that as the
+base directory for glideinWMS source code.  If not, then assume the source is
+one level above the current directory.  The reason of this is so that a
+developer can write and execute unittests without having to setup a special
+environment.  However, on nmi, the location of the tests may or may not be
+tied to the location of glideinWMS source.  On nmi, the $GLIDEINWMS_LOCATION
+will be defined instead.
+"""
 if "GLIDEINWMS_LOCATION" in os.environ:
     sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"lib"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"factory"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"frontend"))
 else:
     sys.path.append(os.path.join(sys.path[0],"../lib"))
+    sys.path.append(os.path.join(sys.path[0],"../factory"))
+    sys.path.append(os.path.join(sys.path[0],"../frontend"))
 
 def runTest(cls):
     """
