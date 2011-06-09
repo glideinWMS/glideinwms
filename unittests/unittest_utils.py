06c509fd4fb57d826c421ae970ca67857175a8c9
tiradani <tiradani>
2011-06-09 14:24:12 +0000

    Added FakeLogger so that the unittest can set the logger for modules that do logging.  This allows these modules to be tested without adding code to disable logging.

--- unittests/unittest_utils.py
+++ unittests/unittest_utils.py
@@ -18,25 +18,25 @@ tied to the location of glideinWMS source.  On nmi, the $GLIDEINWMS_LOCATION
 will be defined instead.
 """
 if "GLIDEINWMS_LOCATION" in os.environ:
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"lib"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"factory"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"frontend"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"factory/tools"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"install"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"install/services"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"poolwatcher"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"tools"))
-    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"],"tools/lib"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "lib"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "factory"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "frontend"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "factory/tools"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "install"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "install/services"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "poolwatcher"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "tools"))
+    sys.path.append(os.path.join(os.environ["GLIDEINWMS_LOCATION"], "tools/lib"))
 else:
-    sys.path.append(os.path.join(unittest_dir,"../lib"))
-    sys.path.append(os.path.join(unittest_dir,"../factory"))
-    sys.path.append(os.path.join(unittest_dir,"../frontend"))
-    sys.path.append(os.path.join(unittest_dir,"../factory/tools"))
-    sys.path.append(os.path.join(unittest_dir,"../install"))
-    sys.path.append(os.path.join(unittest_dir,"../install/services"))
-    sys.path.append(os.path.join(unittest_dir,"../poolwatcher"))
-    sys.path.append(os.path.join(unittest_dir,"../tools"))
-    sys.path.append(os.path.join(unittest_dir,"../tools/lib"))
+    sys.path.append(os.path.join(unittest_dir, "../lib"))
+    sys.path.append(os.path.join(unittest_dir, "../factory"))
+    sys.path.append(os.path.join(unittest_dir, "../frontend"))
+    sys.path.append(os.path.join(unittest_dir, "../factory/tools"))
+    sys.path.append(os.path.join(unittest_dir, "../install"))
+    sys.path.append(os.path.join(unittest_dir, "../install/services"))
+    sys.path.append(os.path.join(unittest_dir, "../poolwatcher"))
+    sys.path.append(os.path.join(unittest_dir, "../tools"))
+    sys.path.append(os.path.join(unittest_dir, "../tools/lib"))
 
 def runTest(cls):
     """
@@ -71,6 +71,57 @@ def runAllTests():
     modules = map(__import__, test_modules)
     for test in modules:
         test.main()
+
+class FakeLogger:
+    """
+    Super simple logger for the unittests
+    """
+
+    def __init__(self):
+        pass
+
+    def debug(self, msg, *args):
+        """
+        Pass a debug message to stderr.
+        
+        Prints out msg % args.
+        
+        @param msg: A message string.
+        @param args: Arguments which should be evaluated into the message.
+        """
+        print >> sys.stderr, str(msg) % args
+
+    def info(self, msg, *args):
+        """
+        Pass an info-level message to stderr.
+        
+        @see: debug
+        """
+        print >> sys.stderr, str(msg) % args
+
+    def warning(self, msg, *args):
+        """
+        Pass a warning-level message to stderr.
+
+        @see: debug
+        """
+        print >> sys.stderr, str(msg) % args
+
+    def error(self, msg, *args):
+        """
+        Pass an error message to stderr.
+
+        @see: debug
+        """
+        print >> sys.stderr, str(msg) % args
+
+    def exception(self, msg, *args):
+        """
+        Pass an exception message to stderr.
+
+        @see: debug
+        """
+        print >> sys.stderr, str(msg) % args
 
 if __name__ == "__main__":
     runAllTests()
