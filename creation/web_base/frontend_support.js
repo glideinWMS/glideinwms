6c9322a633ff53e76e2b7e89330296d21ca8697c
sfiligoi <sfiligoi>
2011-07-02 01:29:57 +0000

    Implement getFrontendGroupFactories - Comitted for Alison

--- creation/web_base/frontend_support.js
+++ creation/web_base/frontend_support.js
@@ -3,7 +3,7 @@
  *   glideinWMS
  * 
  * File Version: 
- *   $Id: frontend_support.js,v 1.2.8.1 2010/09/08 03:30:00 parag Exp $
+ *   $Id: frontend_support.js,v 1.2.8.1.18.1 2011/07/02 01:29:57 sfiligoi Exp $
  *
  * Support javascript module for the frontend monitoring
  * Part of the gldieinWMS package
@@ -39,4 +39,52 @@ function getFrontendGroups(frontendStats) {
     }
   }
   return groups;
+}
+
+//Extract factory names from frontendStats XML obj
+function getFrontendGroupFactories(frontendStats, group_name) {
+  factories=new Array();
+
+  if(group_name=="total") {
+    for (var i=0; i<frontendStats.childNodes.length; i++) {
+      var el=frontendStats.childNodes[i];
+      if ((el.nodeType==1) && (el.nodeName=="factories")) {
+        for (var j=0; j<el.childNodes.length; j++) {
+  	  var group=el.childNodes[j];
+            if ((group.nodeType==1)&&(group.nodeName=="factory")) {
+              var group_name=group.attributes.getNamedItem("name");
+	      factories.push(group_name.value);
+	    }
+          }
+       }
+    } 
+    return factories;
+  }
+
+  for (var i=0; i<frontendStats.childNodes.length; i++) {
+    var el=frontendStats.childNodes[i];
+    if ((el.nodeType==1) && (el.nodeName=="groups")) {
+      for (var j=0; j<el.childNodes.length; j++) {
+	var group=el.childNodes[j];
+	if ((group.nodeType==1)&&(group.nodeName=="group")) {
+	  var group_name1=group.attributes.getNamedItem("name").value;
+          if(group_name1==group_name) {
+             for(var k=0; k<group.childNodes.length; k++) { 
+               var el2 = group.childNodes[k];
+               if (el2.nodeName=="factories") {
+                  for(var m=0; m<el2.childNodes.length; m++) { 
+                     var factory = el2.childNodes[m];
+                     if(factory.nodeName=="factory") {
+                        factory_name=factory.attributes.getNamedItem("name");
+	                factories.push(factory_name.value);
+                     }
+                  }
+               }               
+             }
+          }
+	}
+      }
+    }
+  }
+  return factories;
 }
