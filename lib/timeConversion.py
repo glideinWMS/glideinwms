5fcb84c43feabe7e6c92c88b4705e348658a780a
tiradani <tiradani>
2011-07-13 20:45:30 +0000

    added a function that returns the time in the format that is passed in

--- lib/timeConversion.py
+++ lib/timeConversion.py
@@ -3,7 +3,7 @@
 #   glideinWMS
 #
 # File Version: 
-#   $Id: timeConversion.py,v 1.6.12.1 2010/09/08 03:10:47 parag Exp $
+#   $Id: timeConversion.py,v 1.6.12.1.4.1 2011/07/13 20:45:30 tiradani Exp $
 #
 # Description:
 #   This module implements time2string functions
@@ -12,63 +12,72 @@
 #   Igor Sfiligoi (Mar 15th 2007)
 #
 
-import time,calendar
+import time, calendar
 
 def getSeconds(now=None):
-    if now==None:
-        now=time.time()
-    return "%li"%long(now)
+    if now == None:
+        now = time.time()
+    return "%li" % long(now)
 
-def extractSeconds(str):
-    return long(str)
+def extractSeconds(time_str):
+    return long(time_str)
 
 def getHuman(now=None):
-    if now==None:
-        now=time.time()
-    return time.strftime("%c",time.localtime(now))
+    if now == None:
+        now = time.time()
+    return time.strftime("%c", time.localtime(now))
 
-def extractHuman(str):
-    return time.mktime(time.strptime(str,"%c"))
+def extractHuman(time_str):
+    return time.mktime(time.strptime(time_str, "%c"))
 
 def getISO8601_UTC(now=None):
-    if now==None:
-        now=time.time()
-    return time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime(now))
+    if now == None:
+        now = time.time()
+    return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(now))
 
-def extractISO8601_UTC(str):
-    return calendar.timegm(time.strptime(str,"%Y-%m-%dT%H:%M:%SZ"))
+def extractISO8601_UTC(time_str):
+    return calendar.timegm(time.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ"))
 
 def getISO8601_Local(now=None):
-    if now==None:
-        now=time.time()
-    tzval=getTZval()
-    return time.strftime("%Y-%m-%dT%H:%M:%S",time.localtime(now))+("%+03i:%02i"%((-tzval/3600),(-tzval%3600/60)))
+    if now == None:
+        now = time.time()
+    tzval = getTZval()
+    return time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime(now)) + ("%+03i:%02i" % ((-tzval / 3600), (-tzval % 3600 / 60)))
 
-def extractISO8601_Local(str):
-    timestr=str[:-6]
-    tzstr=str[-6:]
-    tzval=(long(tzstr[:3])*60+long(tzstr[4:]))*60
-    return calendar.timegm(time.strptime(timestr,"%Y-%m-%dT%H:%M:%S"))-tzval
+def extractISO8601_Local(time_str):
+    timestr = time_str[:-6]
+    tzstr = time_str[-6:]
+    tzval = (long(tzstr[:3]) * 60 + long(tzstr[4:])) * 60
+    return calendar.timegm(time.strptime(timestr, "%Y-%m-%dT%H:%M:%S")) - tzval
 
 def getRFC2822_UTC(now=None):
-    if now==None:
-        now=time.time()
-    return time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.gmtime(now))
+    if now == None:
+        now = time.time()
+    return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime(now))
 
-def extractRFC2822_UTC(str):
-    return calendar.timegm(time.strptime(str,"%a, %d %b %Y %H:%M:%S +0000"))
+def extractRFC2822_UTC(time_str):
+    return calendar.timegm(time.strptime(time_str, "%a, %d %b %Y %H:%M:%S +0000"))
 
 def getRFC2822_Local(now=None):
-    if now==None:
-        now=time.time()
-    tzval=getTZval()
-    return time.strftime("%a, %d %b %Y %H:%M:%S ",time.localtime(now))+("%+03i%02i"%((-tzval/3600),(-tzval%3600/60)))
+    if now == None:
+        now = time.time()
+    tzval = getTZval()
+    return time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime(now)) + ("%+03i%02i" % ((-tzval / 3600), (-tzval % 3600 / 60)))
 
-def extractRFC2822_Local(str):
-    timestr=str[:-6]
-    tzstr=str[-5:]
-    tzval=(long(tzstr[:3])*60+long(tzstr[3:]))*60
-    return calendar.timegm(time.strptime(timestr,"%a, %d %b %Y %H:%M:%S"))-tzval
+def extractRFC2822_Local(time_str):
+    timestr = time_str[:-6]
+    tzstr = time_str[-5:]
+    tzval = (long(tzstr[:3]) * 60 + long(tzstr[3:])) * 60
+    return calendar.timegm(time.strptime(timestr, "%a, %d %b %Y %H:%M:%S")) - tzval
+
+def get_time_in_format(now=None, time_format=None):
+    if now == None:
+        now = time.time()
+    if time_format == None:
+        time_str = getHuman()
+    else:
+        time_str = time.strftime(time_format, time.localtime(now))
+    return time_str
 
 #########################
 # Internal
