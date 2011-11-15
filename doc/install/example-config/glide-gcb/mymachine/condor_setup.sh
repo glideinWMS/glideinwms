b29a2c8d6ffbdda7e2e6501611ff7e49b70290ac
Krista Larson <klarson1@fnal.gov>
2011-11-15 09:36:21 -0600

    Removed files and dirs from /doc/install/ that were no longer used anywhere.

--- doc/install/example-config/glide-gcb/mymachine/condor_setup.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$CONDOR_BASE_PATH" ]
-then
- export CONDOR_BASE_PATH=/home/gcbuser/condor/dist
- #export CONDOR_IDS=4732.5063
- export CONDOR_CONFIG=$CONDOR_BASE_PATH/etc/condor_config
-
- export PATH=$PATH:$CONDOR_BASE_PATH/bin/
- export MANPATH=$MANPATH:$CONDOR_BASE_PATH/man
-fi
--- doc/install/example-config/glide-schedd/start_condor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/opt/glidecondor/sbin/condor_master
-sleep 1
-/opt/glidecondor/start_master_schedd.sh jobs1
-/opt/glidecondor/start_master_schedd.sh jobs2
-/opt/glidecondor/start_master_schedd.sh jobs3
-/opt/glidecondor/start_master_schedd.sh jobs4
