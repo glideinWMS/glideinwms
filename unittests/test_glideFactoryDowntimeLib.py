6bf3d23b0d417f3d2d3db4265efbe20843f8593a
Doug Strain <dstrain@fnal.gov>
2012-03-15 10:40:38 -0500

    Unittests: more downtime tests

--- unittests/test_glideFactoryDowntimeLib.py
+++ unittests/test_glideFactoryDowntimeLib.py
@@ -44,13 +44,33 @@ class TestDowntimes(unittest.TestCase):
         self.assertFalse(self.downtime.checkDowntime(entry="All",check_time=now+1))
         #addPeriod(self,start_time,end_time,entry="All",frontend="All",security_class="All",comment="",create_if_empty=True):
 
-    def test_setdelaytime(self):
+    def test_setperiodwithendtime(self):
         now=long(time.time())
         self.downtime.startDowntime(start_time=now-60,end_time=now+3600,entry="All",frontend="All",security_class="All",comment="unittest downtime",create_if_empty=True)
         self.assertTrue(self.downtime.checkDowntime(entry="All",check_time=None))
         self.assertTrue(self.downtime.checkDowntime(entry="James",check_time=None))
         self.downtime.endDowntime(entry="All",comment="end unittest downtime")
         self.assertFalse(self.downtime.checkDowntime(entry="All",check_time=now+1))
+    
+    def test_entryonlydowntime(self):
+        now=long(time.time())
+        self.downtime.startDowntime(start_time=now-60,end_time=now+3600,entry="DougEntry",frontend="All",security_class="All",comment="unittest downtime",create_if_empty=True)
+        self.assertFalse(self.downtime.checkDowntime(entry="All",check_time=None))
+        self.assertFalse(self.downtime.checkDowntime(entry="James",check_time=None))
+        self.assertTrue(self.downtime.checkDowntime(entry="DougEntry",check_time=None))
+        self.downtime.endDowntime(entry="All",comment="end unittest downtime")
+        self.assertFalse(self.downtime.checkDowntime(entry="All",check_time=now+1))
+        self.assertFalse(self.downtime.checkDowntime(entry="DougEntry",check_time=now+1))
+
+    def test_setdelayedperiod(self):
+        now=long(time.time())
+        self.downtime.startDowntime(start_time=now+7200,end_time=now+10800,entry="All",frontend="All",security_class="All",comment="unittest delayed downtime",create_if_empty=True)
+        self.assertFalse(self.downtime.checkDowntime(entry="All",check_time=None))
+        self.assertTrue(self.downtime.checkDowntime(entry="All",check_time=now+9600))
+        self.downtime.endDowntime(entry="All",comment="end unittest downtime")
+        self.assertFalse(self.downtime.checkDowntime(entry="All",check_time=None))
+        self.assertTrue(self.downtime.checkDowntime(entry="All",check_time=now+9600))
+
 
 def main():
     return runTest(TestDowntimes)
