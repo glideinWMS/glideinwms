f312a175ae070ddb50c5eb6517332320a4b2d110
Parag Mhashilkar <parag@fnal.gov>
2012-08-02 16:22:06 -0500

    Factory config advertise_with_multiple and advertise_with_tcp default to true. Issue #2862

--- creation/lib/cgWParams.py
+++ creation/lib/cgWParams.py
@@ -145,8 +145,8 @@ class GlideinParams(cWParams.CommonParams):
         log_retention_defaults["logs"]=copy.deepcopy(one_log_retention_defaults)
         log_retention_defaults["job_logs"]=copy.deepcopy(one_log_retention_defaults)
         log_retention_defaults["job_logs"]["min_days"][0]="2.0"
-        self.defaults['advertise_with_tcp']=('False','Bool', 'Should condor_advertise use TCP connections?',None)
-        self.defaults['advertise_with_multiple']=('False','Bool', 'Should condor_advertise use -multiple?',None)
+        self.defaults['advertise_with_tcp']=('True','Bool', 'Should condor_advertise use TCP connections?',None)
+        self.defaults['advertise_with_multiple']=('True','Bool', 'Should condor_advertise use -multiple?',None)
         log_retention_defaults["summary_logs"]=copy.deepcopy(one_log_retention_defaults)
         log_retention_defaults["summary_logs"]["max_days"][0]="31.0"
         log_retention_defaults["condor_logs"]=copy.deepcopy(one_log_retention_defaults)
