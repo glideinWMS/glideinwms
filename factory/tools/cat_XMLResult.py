54653b673bf62588f856933d49aa3c03b8d55adc
Igor Sfiligoi <isfiligoi@ucsd.edu>
2012-10-25 09:48:09 -0700

    Add -raw and -forcemulti attributes to make it work better with xargs

--- factory/tools/cat_XMLResult.py
+++ factory/tools/cat_XMLResult.py
@@ -8,7 +8,9 @@
 # Description:
 #   Print out the XML Result for a glidein output file
 #
-# Usage: cat_XMLResult.py logname
+# Usage: cat_XMLResult.py [-raw] [-forcemulti] logname+
+#        If -raw is present, do not wrap multiple XMLs into a ResultSet
+#        If -forcemulti is present, make it a ResultSet even if only one file present
 #
 
 import os.path
@@ -18,22 +20,35 @@ STARTUP_DIR=sys.path[0]
 sys.path.append(os.path.join(STARTUP_DIR,"lib"))
 import gWftLogParser
 
-USAGE="Usage: cat_XMLResult.py -h|<logname>+"
+USAGE="Usage: cat_XMLResult.py -h|[-raw] [-forcemulti] <logname>+"
 
-def main():
-    if (len(sys.argv)<2):
-        sys.stderr.write("Missing logname.\n")
-        sys.stderr.write("%s\n"%USAGE)
-        sys.exit(1)        
-    if (sys.argv[1]=="-h"):
-        print USAGE
-        sys.exit(0)
+def main(args):
+    raw_out=False
+    force_multi=False
 
-    if (len(sys.argv)==2):
+    while 1:
+        if (len(args)<1):
+            sys.stderr.write("Missing logname.\n")
+            sys.stderr.write("%s\n"%USAGE)
+            sys.exit(1)        
+
+        if (args[0]=="-h"):
+            print USAGE
+            sys.exit(0)
+        elif (args[0]=="-raw"):
+            raw_out=True
+            args=args[1:]
+        elif (args[0]=="-forcemulti"):
+            force_multi=True
+            args=args[1:]
+        else:
+            break # looks like I found a log name
+    
+    if (len(args)==1) and (not force_multi):
         # single file, just pass through
         try:
-            fname=sys.argv[1]
-            print gWftLogParser.get_XMLResult(fname)
+            fname=args[0]
+            out=gWftLogParser.get_XMLResult(fname)
         except OSError,e:
             sys.stderr.write("Error reading file: %s\n"%e)
             sys.exit(1)
@@ -41,12 +56,21 @@ def main():
             raise
             sys.stderr.write("%s\n"%USAGE)
             sys.exit(1)
+
+        for l in string.split(out,"\n"):
+            if raw_out and (l[:2]=="<?"):
+                #skip comments for raw output
+                continue
+            if l[:15]=="<OSGTestResult ":
+                # insert file name
+                l=l[:15]+('logname="%s" '%fname)+l[15:]
+            print l
     else:
         # multiple files, combine in a set
         xmls=[]
-        for i in range(1,len(sys.argv)):
+        for i in range(len(args)):
             try:
-                fname=sys.argv[i]
+                fname=args[i]
                 rawx=gWftLogParser.get_XMLResult(fname)
                 if (rawx==""):
                     # nothing found, warn
@@ -76,13 +100,17 @@ def main():
         if (len(xmls)==0):
             sys.stderr.write("Could not read a single file!")
             sys.exit(1)
-        sys.stdout.write('<?xml version="1.0"?>\n')
-        sys.stdout.write("<OSGTestResultSet>\n")
+        if not raw_out:
+            sys.stdout.write('<?xml version="1.0"?>\n')
+            sys.stdout.write("<OSGTestResultSet>\n")
+        
         for l in xmls:
             sys.stdout.write(l+"\n")
-        sys.stdout.write("</OSGTestResultSet>\n")
+        
+        if not raw_out:
+            sys.stdout.write("</OSGTestResultSet>\n")
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
  
