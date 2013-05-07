c43aaa43a0a23a8e86b6c9b1c12c303152141b5b
Parag Mhashilkar <parag@fnal.gov>
2013-05-07 18:37:03 -0500

    Updated the default log files to cleanup

--- factory/glideFactoryLib.py
+++ factory/glideFactoryLib.py
@@ -192,10 +192,17 @@ class LogFiles:
         self.admin_log=logSupport.DayLogFile(os.path.join(log_dir,file_name),"admin.log")
         # no need to use the privsep version
         # Don't clean up admin log.  More important to have record
-        self.cleanupObjs=[logSupport.DirCleanupWSpace(log_dir,"(%s\.[0-9]*\.info\.log)|(factory\.[0-9]*\.err\.log)|(factory\.[0-9]*\.debug\.log)"%file_name,
-                                                      int(max_days*24*3600),int(min_days*24*3600),
-                                                      long(max_mbs*(1024.0*1024.0)),
-                                                      self.activity_log,self.warning_log)]
+        cleanup_regex = ''
+        for ext in ('info', 'debug', 'err', 'admin'):
+            cleanup_regex = '%s|(%s\.[0-9]*\.%s\.log)' % (cleanup_regex,
+                                                          file_name, ext)
+        self.cleanupObjs=[
+            logSupport.DirCleanupWSpace(
+                log_dir, cleanup_regex.strip('|'),
+                int(max_days*24*3600),int(min_days*24*3600),
+                long(max_mbs*(1024.0*1024.0)),
+                self.activity_log, self.warning_log)
+        ]
 
     def logActivity(self,str):
         try:
