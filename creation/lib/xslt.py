b98e6f28d3348948a4389124d2a61f0e41d58524
Burt Holzman <burt@fnal.gov>
2013-08-15 09:06:08 -0500

    2811: Move xslt_xml into lib function, check for xsltproc

---
+++ creation/lib/xslt.py
@@ -0,0 +1,32 @@
+from distutils.spawn import find_executable
+import os
+import subprocess
+
+def xslt_xml(old_xmlfile, xslt_plugin_dir):
+    ''' Take an XML file, transform it via any XSLT in the
+    xslt_plugin_dir, and return the output.'''
+
+    old_xml_fd = open(old_xmlfile)
+    if not xslt_plugin_dir:
+        return old_xml_fd.read()
+
+    try:
+        plugins = [os.path.join(xslt_plugin_dir, f) for f in os.listdir(xslt_plugin_dir)]
+    except OSError, e:
+        print "Error opening %s directory: %s" % (xslt_plugin_dir, e.strerror)
+        return old_xml_fd.read()
+
+    plugins.sort()
+
+    procs = [subprocess.Popen(["cat", "%s" % old_xmlfile], stdout=subprocess.PIPE)]
+
+    if plugins and not find_executable('xsltproc'):
+        raise RuntimeError('Cannot reconfig: plugins defined but xsltproc not in path')
+
+    for i, plugin in enumerate(plugins):
+        previous_stdout = procs[i].stdout
+        procs.append(subprocess.Popen(["xsltproc", "%s" % plugin, "-"], stdin=previous_stdout, stdout=subprocess.PIPE))
+        previous_stdout.close()
+
+    output = procs[-1].communicate()[0]
+    return output
