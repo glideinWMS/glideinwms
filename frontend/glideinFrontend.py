2632c9156e026e814a1193097c00cb8c9b3b32a8
Burt Holzman <burt@fnal.gov>
2013-09-04 12:43:47 -0500

    4617: Let frontend become a process group leader
    With this change, the frontend can be the process group leader
    for any children it forks -- useful for controlled stopping
    of the frontend.

--- frontend/glideinFrontend.py
+++ frontend/glideinFrontend.py
@@ -328,6 +328,8 @@ def termsignal(signr, frame):
     raise KeyboardInterrupt, "Received signal %s" % signr
 
 if __name__ == '__main__':
+    if os.getsid(os.getpid()) != os.getpgrp():
+        os.setpgid(0, 0)
     signal.signal(signal.SIGTERM, termsignal)
     signal.signal(signal.SIGQUIT, termsignal)
 
