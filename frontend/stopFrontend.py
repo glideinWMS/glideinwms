783e66de1259b6b793762d116d5f9626d3ad5139
Burt Holzman <burt@fnal.gov>
2013-09-04 12:44:51 -0500

    4617: Change stopFrontend to use process groups
    This allows the stopFrontend code to just send a TERM
    to the process group leader and let unix propagate the signal.

--- frontend/stopFrontend.py
+++ frontend/stopFrontend.py
@@ -25,31 +25,9 @@ sys.path.append(os.path.join(sys.path[0],"../.."))
 
 from glideinwms.frontend import glideinFrontendPidLib
 from glideinwms.frontend import glideinFrontendConfig
-
-# this one should  never throw an exeption
-def get_element_pids(work_dir, frontend_pid):
-    # get element pids
-    frontendDescript = glideinFrontendConfig.FrontendDescript(work_dir)
-    groups = string.split(frontendDescript.data['Groups'], ',')
-    groups.sort()
-
-    element_pids = {}
-    for group in groups:
-        try:
-            element_pid, element_ppid = glideinFrontendPidLib.get_element_pid(work_dir, group)
-        except RuntimeError, e:
-            print e
-            continue # report error and go to next group
-        if element_ppid != frontend_pid:
-            print "Group '%s' has an unexpected Parent PID: %s!=%s" % (group, element_ppid, frontend_pid)
-            continue # report error and go to next group
-        element_pids[group] = element_pid
-
-    return element_pids
+from glideinwms.lib.pidSupport import kill_and_check_pgid
 
 def main(work_dir, force=False):
-    retries_count = 100
-    sleep_in_retries = 0.2
     # get the pids
     try:
         frontend_pid = glideinFrontendPidLib.get_frontend_pid(work_dir)
@@ -58,61 +36,30 @@ def main(work_dir, force=False):
         return 1
     #print frontend_pid
 
+    frontend_pgid = os.getpgid(frontend_pid)
+
     if not glideinFrontendPidLib.pidSupport.check_pid(frontend_pid):
         # Frontend already dead
         return 0
 
     # kill processes
     # first soft kill the frontend (20s timeout)
-    try:
-        os.kill(frontend_pid, signal.SIGTERM)
-    except OSError:
-        pass # frontend likely already dead
-
-    for retries in range(retries_count):
-        if glideinFrontendPidLib.pidSupport.check_pid(frontend_pid):
-            time.sleep(sleep_in_retries)
-        else:
-            return 0 # frontend dead
+    if (kill_and_check_pgid(frontend_pgid) == 0):
+        return 0
 
     if not force:
-        print "Frontend did not die after the timeout of %s sec" % (retries_count * sleep_in_retries)
+        print "Frontend did not die within the timeout"
         return 1
 
     # Retry soft kill the frontend ... should exit now
     print "Frontend still alive ... retrying soft kill"
-    try:
-        os.kill(frontend_pid, signal.SIGTERM)
-    except OSError:
-        pass # frontend likely already dead
-
-    for retries in range(retries_count):
-        if glideinFrontendPidLib.pidSupport.check_pid(frontend_pid):
-            time.sleep(sleep_in_retries)
-        else:
-            return 0 # frontend dead
+    if (kill_and_check_pgid(frontend_pgid, retries=25) == 0):
+        return 0
 
     print "Frontend still alive ... sending hard kill"
 
-    element_pids = get_element_pids(work_dir, frontend_pid)
-    #print element_pids
-
-    element_keys = element_pids.keys()
-    element_keys.sort()
-
-    for element in element_keys:
-        if glideinFrontendPidLib.pidSupport.check_pid(element_pids[element]):
-            print "Hard killing element %s" % element
-            try:
-                os.kill(element_pids[element], signal.SIGKILL)
-            except OSError:
-                pass # ignore already dead processes
-
-    if not glideinFrontendPidLib.pidSupport.check_pid(frontend_pid):
-        return 0 # Frontend died
-
     try:
-        os.kill(frontend_pid, signal.SIGKILL)
+        os.killpg(frontend_pgid, signal.SIGKILL)
     except OSError:
         pass # ignore problems
     return 0
