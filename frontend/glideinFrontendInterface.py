6445211e3757771989d4a3d55b399545c8f72062
Igor Sfiligoi <isfiligoi@ucsd.edu>
2014-03-20 17:19:16 -0700

    Improve comments

--- frontend/glideinFrontendInterface.py
+++ frontend/glideinFrontendInterface.py
@@ -692,6 +692,7 @@ class MultiAdvertizeWork:
         Advertize globals with credentials
         Returns a dictionary of files that still need to be advertised.
           The key is the factory pool, while the element is a list of file names
+        Expects that the credentials have been already loaded.
         """
         unpublished_files={}
         if reset_unique_id:
@@ -705,6 +706,7 @@ class MultiAdvertizeWork:
         """
         Advertize globals with credentials to one factory
         Returns the list of files that still need to be advertised.
+        Expects that the credentials have been already loaded.
         """
         if not (factory_pool in self.global_pool):
             # nothing to be done, prevent failure
@@ -734,8 +736,8 @@ class MultiAdvertizeWork:
     def createGlobalAdvertizeWorkFile(self, factory_pool):
             """
             Create the advertize file for globals with credentials
-            Expects the object variable
-             adname
+            Expects the object variables
+             adname and x509_proxies_data
             to be set.
             """
             # the different indentation is due to code refactoring
@@ -800,6 +802,7 @@ class MultiAdvertizeWork:
         Do the advertizing of the requests
         Returns a dictionary of files that still need to be advertised.
           The key is the factory pool, while the element is a list of file names
+        Expects that the credentials have already been loaded.
         """
         if file_id_cache is None:
             file_id_cache=CredentialCache()
@@ -816,6 +819,7 @@ class MultiAdvertizeWork:
             """
             Do the advertizing of requests for one factory
             Returns the list of files that still need to be advertised.
+            Expects that the credentials have already been loaded.
             """
             # the different indentation is due to code refactoring
             # this way the diff was minimized
@@ -871,7 +875,7 @@ class MultiAdvertizeWork:
         """
         Create the advertize file
         Expects the object variables
-          adname and unique_id
+          adname, unique_id and x509_proxies_data
         to be set.
         """
         global frontendConfig
