9ad3cb744b7d0f185652412440a3a186fa8614f8
Jeff Dost <jdost@ucsd.edu>
2015-10-20 14:11:55 -0700

    begin replacing minidom with sax

--- creation/lib/xmlConfig.py
+++ creation/lib/xmlConfig.py
@@ -1,5 +1,7 @@
+import copy
 import os
 import collections
+import xml.sax
 
 INDENT_WIDTH = 3
 
@@ -9,12 +11,38 @@ LIST_TAGS = set([
 ])
 
 TAG_CLASS_MAPPING = {}
+DOCUMENT_ROOT = None
+
+class Handler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.root = None
+        self.ancestry = []
+
+    def startElement(self, name, attrs):
+        if name in LIST_TAGS:
+            el = ListElement(name)
+        elif name in TAG_CLASS_MAPPING:
+            el = TAG_CLASS_MAPPING[name](name)
+            for k in attrs.keys():
+                el.attrs[k] = attrs[k]
+        else:
+            el = DictElement(name)
+            for k in attrs.keys():
+                el.attrs[k] = attrs[k]
+        
+        if name == DOCUMENT_ROOT:
+            self.root = el
+        else:
+            self.ancestry[-1].add_child(el)
+
+        self.ancestry.append(el)
+
+    def endElement(self, name):
+        self.ancestry.pop()
 
 class Element(object):
-    def __init__(self, doc, xml, level):
-        self.doc = doc
-        self.xml = xml
-        self.level = level
+    def __init__(self, tag):
+        self.tag = tag
 
     def __str__(self):
         return self.xml.toxml()
@@ -22,11 +50,10 @@ class Element(object):
     def first_line(self):
         return self.xml.toxml().splitlines()[0]
 
-    def unlink(self):
-        self.doc.unlink()
-
     # children should override these
-    def build_tree(self):
+    def add_child(self):
+        pass
+    def clear_lists(self):
         pass
     def merge_defaults(self):
         pass
@@ -35,39 +62,29 @@ class Element(object):
     def validate_tree(self):
         pass
 
-class DictElementIterator(collections.Iterator):
-    def __init__(self, attributes):
-        self.i = 0
-        self.attributes = attributes
-    def next(self):
-        if self.i >= self.attributes.length:
-            raise StopIteration
-        cur_key = self.attributes.item(self.i).name
-        self.i += 1
-        return cur_key
-
 class DictElement(Element, collections.MutableMapping):
-    def __init__(self, doc, xml, level):
-        super(DictElement, self).__init__(doc, xml, level)
+    def __init__(self, tag):
+        super(DictElement, self).__init__(tag)
+        self.attrs = {}
         self.children = {}
 
     def __getitem__(self, key):
-        return self.xml.getAttribute(key)
+        return self.attrs[key]
 
     def __setitem__(self, key, value):
-        self.xml.setAttribute(key, value)
+        self.attrs[key] = value
 
     def __delitem__(self, key):
-        self.xml.removeAttribute(key)
+        del(self.attrs[key])
 
     def __contains__(self, key):
-        return self.xml.hasAttribute(key)
+        return key in self.attrs
 
     def __iter__(self):
-        return DictElementIterator(self.xml.attributes)
+        return iter(self.attrs)
 
     def __len__(self):
-        return self.xml.attributes.length
+        return len(self.attrs)
 
     def has_child(self, tag):
         return tag in self.children
@@ -78,21 +95,12 @@ class DictElement(Element, collections.MutableMapping):
     def get_child_list(self, tag):
         return self.get_child(tag).get_children()
 
+    def add_child(self, child):
+        self.children[child.tag] = child
+
     def clear_lists(self):
         for tag in self.children:
             self.children[tag].clear_lists()
-
-    def build_tree(self):
-        for c in self.xml.childNodes:
-            if c.nodeType == c.ELEMENT_NODE:
-                if c.tagName in LIST_TAGS:
-                    self.children[c.tagName] = ListElement(self.doc, c, self.level + 1)
-                elif c.tagName in TAG_CLASS_MAPPING:
-                    self.children[c.tagName] = (TAG_CLASS_MAPPING[c.tagName](self.doc, c, self.level + 1)) 
-                else:
-                    self.children[c.tagName] = DictElement(self.doc, c, self.level + 1)
-
-                self.children[c.tagName].build_tree()
 
     def merge_default_attrs(self, default):
         for key in default:
@@ -106,20 +114,12 @@ class DictElement(Element, collections.MutableMapping):
             if tag not in self.children:
                 # if its an xml list that is missing just create a new empty one
                 if isinstance(default.children[tag], ListElement):
-                    new_child = self.doc.createElement(tag)
-                    self.children[tag] = type(default.children[tag])(self.doc, new_child, self.level + 1)
+                    self.children[tag] = type(default.children[tag])(tag)
                 # otherwise clone from default
                 else:
-                    new_child = self.doc.importNode(default.children[tag].xml, True)
-                    self.children[tag] = type(default.children[tag])(self.doc, new_child, self.level + 1)
-                    self.children[tag].build_tree()
+                    self.children[tag] = copy.deepcopy(default.children[tag])
                     # zero out any xml lists
                     self.children[tag].clear_lists()
-                # put new xml element in the top of parent
-                self.xml.insertBefore(new_child, self.xml.firstChild)
-                # insert line break in front for readability
-                line_break = self.doc.createTextNode(u'\n%*s' % (INDENT_WIDTH * (self.level + 1),' '))
-                self.xml.insertBefore(line_break, self.xml.firstChild)
             # or continue down the tree
             else:
                 self.children[tag].merge_defaults(default.children[tag])
@@ -138,29 +138,18 @@ class DictElement(Element, collections.MutableMapping):
             self.children[tag].validate_tree()
 
 class ListElement(Element):
-    def __init__(self, doc, xml, level):
-        super(ListElement, self).__init__(doc, xml, level)
+    def __init__(self, tag):
+        super(ListElement, self).__init__(tag)
         self.children = []
 
     def get_children(self):
         return self.children
 
+    def add_child(self, child):
+        self.children.append(child)
+
     def clear_lists(self):
-        while self.xml.firstChild is not None:
-            self.xml.removeChild(self.xml.firstChild).unlink()
         self.children = []
-
-    def build_tree(self):
-        for c in self.xml.childNodes:
-            if c.nodeType == c.ELEMENT_NODE:
-                # we are assuming you wont' have a list element directly inside another list elemement
-                # so assume its either a custom mapping or a dict element
-                if c.tagName in TAG_CLASS_MAPPING:
-                    self.children.append(TAG_CLASS_MAPPING[c.tagName](self.doc, c, self.level + 1)) 
-                else:
-                    self.children.append(DictElement(self.doc, c, self.level + 1))
-
-                self.children[-1].build_tree()
 
     def merge_defaults(self, default):
         for child in self.children:
@@ -221,6 +210,10 @@ TAG_CLASS_MAPPING.update({u'file': FileElement})
 # any modules that choose to subclass from xmlConfig should register new xml tags
 # and either flag them as being a list element, or associate with respective class type
 # as needed
+def register_root(tag):
+    global DOCUMENT_ROOT
+    DOCUMENT_ROOT = tag
+
 def register_list_elements(tag_list):
     LIST_TAGS.update(tag_list)
 
