1c18d29e3249bc54f7a8f694369156bb267254c2
Marco Mambelli <marcom@fnal.gov>
2016-02-24 14:09:03 -0600

    Added feature extracting all the condor logs and saving them to a directory or posting them to a URL
    First version of what could be used to feed to logstash/elastic search the
    condor logs form the glideins
    Dump to folder is tested and working
    A timestamp file marking wchich files were forwarded (consummed) makes the script cron friendly

--- factory/tools/gwms-logcat.sh
+++ factory/tools/gwms-logcat.sh
@@ -16,6 +16,7 @@ function help_msg {
   cat << EOF
 $0 [options] LOG_TYPE LOGFILE
 $0 [options] LOG_TYPE ENTRY [JOB_ID]
+$0 -f URL [options] LOG_TYPE
 $0 -r [options] LOG_TYPE JOB_ID
 $0 -l
   LOG_TYPE HTCondor log to extract from the job logfile: 
@@ -30,6 +31,7 @@ $0 -l
   -u USER  to use a different user (job owner) from the default frontend one
   -r       Remote running jobs. pilot_launcher.log is fetched form the VM 
   -c FNAME Factory configuration file (default: /etc/gwms-factory/glideinWMS.xml)
+  -f URL   Forward the information (to a fodler: file:///path/ via copy or a URL http:// via post)
 EOF
 }
 
@@ -64,9 +66,10 @@ function get_last_log {
 }
 
 function list_all_entries {
+  # list or forward entries depending on FORWARD_URL being defined
   ulist=`ls "$JOBLOGROOTPREFIX/"`
   if [ "$ulist" = "user_frontend" ]; then
-    list_entries
+    [ -n "$FORWARD_URL" ] && forward_entries || list_entries
     return
   fi
   echo "#USERS LIST:"
@@ -74,12 +77,12 @@ function list_all_entries {
   for i in $ulist; do
     echo "#ENTRY LIST for USER: $i"
     JOBLOGPREFIX="$JOBLOGROOTPREFIX/$i/glidein_gfactory_instance/entry_"
-    list_entries
+    [ -n "$FORWARD_URL" ] && forward_entries || list_entries
   done
 }
 
 function list_entries {
-  elist=`ls -d $JOBLOGPREFIX*`
+  local elist=`ls -d $JOBLOGPREFIX*`
   for i in $elist; do
     entry_count="`ls $i/job*err 2>/dev/null | wc -l`"
     [ -n "$ACTIVE" ] && [ "$entry_count" -eq 0 ] && continue
@@ -89,7 +92,76 @@ function list_entries {
   done
 }
 
-while getopts "lhc:o:u:rav" option
+function get_unique_name {
+  # $1 file name 
+  # $2 log type
+  dir_name="`dirname $1`"
+  file_name="`basename $1`"
+  entry_part="`basename "$dir_name"`"
+  log_type=$2
+  [ -z "$log_type" ] && log_type="$logoption"
+  # prefix-host-user?-entry-jobID-logType
+  echo "job_log-`hostname`-${entry_part#entry_}-${file_name%.err}-$log_type"
+}
+
+function forward_file {
+  # 1. file name (logid) 
+  # 2. log type (logoption)
+  # Using: LOGNAME was set using log type
+  local logid=$1
+  local logoption=$2
+  if [ ! -s "$logid" ]; then
+    echo "Check Entry and Job IDs. File not found or zero length: $logid"
+    return 1
+  fi
+  if [[ "$FORWARD_URL" =~ file://.* ]]; then
+    # If starts w/ file:// consider it an output directory
+    ${TOOLDIR}/${LOGNAME} $logid > "${FORWARD_URL#file://}/`get_unique_name $logid $logoption`"
+  elif [[ "$FORWARD_URL" =~ http://.* ]]; then
+    # http:// URL trigger a post to the URL
+    # TODO: elaborate more (e.g. use logstash to forward to elastic search)
+    # TODO: may need to re-encode the file before sending it
+    fname="$FORWARD_TMP_DIR/`get_unique_name $logid $logoption`"
+    ${TOOLDIR}/${LOGNAME} $logid > "$fname"
+    curl -F fname="`get_unique_name $logid $logoption`" -F logfile=@$fname $FORWARD_URL
+  else
+    echo "ERROR: Don't know how to handle $FORWARD_URL (for $i)"
+  fi
+}
+
+FORWARD_STATS_FNAME=forwarding_stats_last_
+function forward_entry {
+  # 1. entry directory
+  # 2. log option 
+  local stats_fname="$1/$FORWARD_STATS_FNAME$2" 
+  touch "${stats_fname}.new"
+  date_opts=""
+  if [ -f "$stats_fname" ]; then
+    date_opts="-newer $stats_fname "
+  fi
+  FORWARD_TMP_DIR="`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`"
+  flist="`find "$1" -type f $date_opts -size +1 -name 'job*err' -printf '%T@ %p\n'  | sort -nk1 | sed 's/^[^ ]* //'`"
+  for i in $flist; do
+    forward_file "$i" "$2"
+  done
+  rm -r "$FORWARD_TMP_DIR"
+  mv "${stats_fname}.new" "${stats_fname}"
+}
+
+function forward_entries {
+  # Forwarding all the entries from a user
+  # 1. entry name
+  # Using: $logoption - log option
+  local elist=`ls -d $JOBLOGPREFIX*`
+  for i in $elist; do
+    # forward all the info for that entry
+    [ -n "$VERBOSE" ] && echo "Forwarding entry: ${i#$JOBLOGPREFIX}"
+    forward_entry "$i" $logoption
+  done
+}
+
+
+while getopts "lhc:o:u:f:rav" option
 do
   case "${option}"
   in
@@ -99,6 +171,7 @@ do
   r) REMOTE=yes;;
   c) CONFIG_FNAME=$OPTARG;;
   u) FEUSER=$OPTARG;;
+  f) FORWARD_URL=$OPTARG;; 
   a) ACTIVE=yes
   esac
 done
@@ -121,6 +194,8 @@ shift $((OPTIND-1))
 logoption=$1
 logid="$2"
 
+[ -n "$FORWARD_URL" ] && logid=ALL
+
 if [ -z "$logid" ]; then
   echo "You must specify a log type and an entry (or log file)"
   help_msg
@@ -136,6 +211,13 @@ case $logoption in
   startdhistory) LOGNAME=cat_StartdHistoryLog.py;;
   *) echo "Unknown LOG_TYPE: $logoption"; help_msg; exit 1;;
 esac
+
+
+if [ -n "$FORWARD_URL" ]; then
+  # forward info using list_all_entries (after LOGNAME evaluation)
+  list_all_entries
+  exit 0
+fi
 
 
 if [ -n "$REMOTE" ]; then
@@ -175,7 +257,7 @@ if [ ! -s "$logid" ]; then
   echo "Check Entry and Job IDs. File not found or zero length: $logid"
   exit 1
 fi
-[ -n "$VERBOSE" ] && echo -e "Available logs:\n`grep "======== gzip | uuencode =============" -B 1  /tmp/pilot_launcher.log | grep -v "======== gzip | uuencode =============" | grep -v "\-\-"`"
+[ -n "$VERBOSE" ] && echo -e "Available logs:\n`grep "======== gzip | uuencode =============" -B 1  "$logid" | grep -v "======== gzip | uuencode =============" | grep -v "\-\-"`"
 [ -n "$VERBOSE" ] && echo "Log $logoption from $logid:"
 
 # TODO: I'd like to verify the output but am aftraid it may be too big (and being cut)
