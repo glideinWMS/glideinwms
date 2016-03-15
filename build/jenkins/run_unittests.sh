8e7d4ee5097cd08922317ba7e89cb63756d124f8
Parag Mhashilkar <parag@fnal.gov>
2016-03-15 11:38:50 -0500

    Fixed bugs and added xmlrunner to list for pip install

--- build/jenkins/run_unittests.sh
+++ build/jenkins/run_unittests.sh
@@ -6,23 +6,23 @@ function log_nonzero_rc {
 
 WORKSPACE=`pwd`
 VIRTUALENV_TARBALL=virtualenv-12.0.7.tar.gz
-VIRTUALENV_URL=https://pypi.python.org/packages/source/v/virtualenv/$VIRTUALENV_TARBALL
+VIRTUALENV_URL="https://pypi.python.org/packages/source/v/virtualenv/$VIRTUALENV_TARBALL"
 VIRTUALENV_EXE=$WORKSPACE/virtualenv-12.0.7/virtualenv.py
 VENV=$WORKSPACE/venv
 
 export GLIDEINWMS_SRC=$WORKSPACE/glideinwms
-export ERRORS=$WORKSPACE/errors
+#export ERRORS=$WORKSPACE/errors
 
 # Following is useful for running the script outside jenkins
 if [ ! -d "$WORKSPACE" ]; then
     mkdir $WORKSPACE
 fi
 
-echo > $ERRORS
+#echo > $ERRORS
 
 echo "SETTING UP VIRTUAL ENVIRONMENT ..."
 # Get latest virtualenv package that works with python 2.6
-wget $VIRTUALENV_TARBALL
+wget $VIRTUALENV_URL
 tar xzf $VIRTUALENV_TARBALL
 if [ ! -d venv ] ; then
    #virtualenv --python=python2.6 --always-copy $VENV
@@ -38,7 +38,7 @@ export PYTHONPATH="$PWD:$PYTHONPATH"
 # 1. rrdtool-devel
 # 2. openssl-devel
 # 3. swig
-pip_packages="astroid==1.2.1 pylint==1.3.1 pep8 unittest2 coverage rrdtool pyyaml mock"
+pip_packages="astroid==1.2.1 pylint==1.3.1 pep8 unittest2 coverage rrdtool pyyaml mock xmlrunner"
 for package in $pip_packages; do
     echo "Installing $package ..."
     status="DONE"
@@ -64,6 +64,7 @@ export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/tools/lib
 cd $GLIDEINWMS_SRC/unittests
 files="test_frontend.py test_frontend_element.py"
 files="test_frontend.py"
+#files="test_frontend_element.py"
 
 for file in $files ; do
     ./$file || log_nonzero_rc "$file" $?
