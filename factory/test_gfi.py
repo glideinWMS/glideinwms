55577a2bcace11342629e7bcd91e865f182ce323
Marco Mambelli <marcom@fnal.gov>
2016-03-25 18:05:35 -0500

    fixed test_gfi.py, silenced pylint for os.remove(), removed blank line on top of classad files, fixed glidein_stats parameter passing, inproved pep8 compliance and comments

--- factory/test_gfi.py
+++ factory/test_gfi.py
@@ -7,11 +7,14 @@
 
 import os,sys
 from glideinwms.factory import glideFactoryInterface
+from glideinwms.lib import classadSupport
 
 glideFactoryInterface.factoryConfig.activity_log=sys.stdout
 glideFactoryInterface.factoryConfig.warining_log=sys.stdout
 
-glideFactoryInterface.advertizeGlidein("factory_name", 
+tmpnam = classadSupport.generate_classad_filename(prefix='gfi_ad_gf')
+
+gf_classad = glideFactoryInterface.EntryClassad("factory_name",
                                        "glidein_name", 
                                        "entry_name", 
                                        "trust_domain", 
@@ -21,3 +24,14 @@ glideFactoryInterface.advertizeGlidein("factory_name",
                                        "myJobAttributes",
                                        "jobParams.data.copy()",
                                        "glidein_monitors.copy()")
+
+try:
+    gf_classad.writeToFile(tmpnam, append=False)
+    glideFactoryInterface.exe_condor_advertise(tmpnam, "UPDATE_MASTER_AD", factory_collector=glideFactoryInterface.DEFAULT_VAL)
+finally:
+    # Unable to write classad
+    try:
+        os.remove(tmpnam)
+    except:
+        # Do the possible to remove the file if there
+        pass
