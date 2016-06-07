02361515f08e0ab82ca9e15a257c394b74486eaf
Parag Mhashilkar <parag@fnal.gov>
2016-06-07 15:14:57 -0500

    Use common tasks from utils.sh

--- build/jenkins/run_pylint.sh
+++ build/jenkins/run_pylint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-function log_nonzero_rc {
-echo "`date` ERROR: $1 failed with non zero exit code ($2)" 1>&2
-}
-
-
 WORKSPACE=`pwd`
-VENV=$WORKSPACE/venv
+export GLIDEINWMS_SRC=$WORKSPACE/glideinwms
 
-GLIDEINWMS_SRC=$WORKSPACE/glideinwms
+source $GLIDEINWMS_SRC/build/jenkins/utils.sh
+
 ERRORS=$WORKSPACE/errors
-
 # pylint related variables
 PYLINT_LOG=$WORKSPACE/pylint.log
 PYLINT_RCFILE=/dev/null
@@ -36,63 +31,20 @@ PEP8_LOG=$WORKSPACE/pep8.log
 
 PEP8_OPTIONS="--ignore=E121,E123,E126,E226,E24,E704,E501,E251,E303,E225,E231,E228,E302,E221,E261,E111,W293,W291,E265"
 
-# Following is useful for running the script outside jenkins
-if [ ! -d "$WORKSPACE" ]; then
-    mkdir $WORKSPACE
-fi
-
 # Initialize logs
 echo > $ERRORS
 echo > $PYLINT_LOG
 echo > $PEP8_LOG
 
-
-echo "SETTING UP VIRTUAL ENVIRONMENT ..."
-wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.0.7.tar.gz
-tar xzf virtualenv-12.0.7.tar.gz
-if [ ! -d venv ] ; then
-   # virtualenv available on the node is old and does not work
-   #virtualenv --python=python2.6 --always-copy $VENV
-   $WORKSPACE/virtualenv-12.0.7/virtualenv.py $VENV
-fi
-
-source $VENV/bin/activate
-
-export PYTHONPATH="$PWD:$PYTHONPATH"
-
-#pip install pylint
-# Install dependancies first so we don't get uncompatible ones
-easy_install astroid==1.2.1
-easy_install pylint==1.3.1
-easy_install pep8
-
-echo "========================="
-pylint --version
-echo "========================="
+setup_python_venv $WORKSPACE
 
 # Generate pylint config file
 pylint --generate-rcfile > $PYLINT_RCFILE
 #cat $PYLINT_RCFILE
 
-
-## Need this because some strange control sequences when using default TERM=xterm
-export TERM="linux"
-
-
-## PYTHONPATH for glideinwms source code
-# pythonpath for pre-packaged only
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/lib
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/creation/lib
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/factory
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/frontend
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/tools
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/tools/lib
-
 # get list of python scripts without .py extension
 scripts=`find glideinwms -path glideinwms/.git -prune -o -exec file {} \; -a -type f | grep -i python | grep -vi '\.py' | cut -d: -f1`
 echo $scripts
-pwd
-ls -la
 pylint $PYLINT_OPTIONS -e F0401 ${scripts}  >> $PYLINT_LOG || log_nonzero_rc "pylint" $?
 pep8 $PEP8_OPTIONS ${scripts} >> $PEP8_LOG || log_nonzero_rc "pep8" $?
 
--- build/jenkins/run_unittests.sh
+++ build/jenkins/run_unittests.sh
@@ -1,67 +1,11 @@
 #!/bin/sh
 
-function log_nonzero_rc {
-    echo "`date` ERROR: $1 failed with non zero exit code ($2)" 1>&2
-}
-
 WORKSPACE=`pwd`
-VIRTUALENV_TARBALL=virtualenv-12.0.7.tar.gz
-VIRTUALENV_URL="https://pypi.python.org/packages/source/v/virtualenv/$VIRTUALENV_TARBALL"
-VIRTUALENV_EXE=$WORKSPACE/virtualenv-12.0.7/virtualenv.py
-VENV=$WORKSPACE/venv
-
 export GLIDEINWMS_SRC=$WORKSPACE/glideinwms
-#export ERRORS=$WORKSPACE/errors
 
-# Following is useful for running the script outside jenkins
-if [ ! -d "$WORKSPACE" ]; then
-    mkdir $WORKSPACE
-fi
+source $GLIDEINWMS_SRC/build/jenkins/utils.sh
 
-#echo > $ERRORS
-
-echo "SETTING UP VIRTUAL ENVIRONMENT ..."
-# Get latest virtualenv package that works with python 2.6
-curl -o $WORKSPACE/$VIRTUALENV_TARBALL $VIRTUALENV_URL
-tar xzf $WORKSPACE/$VIRTUALENV_TARBALL
-if [ ! -d $VENV ] ; then
-   #virtualenv --python=python2.6 --always-copy $VENV
-   $WORKSPACE/virtualenv-12.0.7/virtualenv.py --system-site-packages $VENV
-   #$WORKSPACE/virtualenv-12.0.7/virtualenv.py $VENV
-fi
-
-source $VENV/bin/activate
-
-export PYTHONPATH="$PWD:$PYTHONPATH"
-
-# Install dependancies first so we don't get uncompatible ones
-# Following RPMs need to be installed on the machine:
-# 1. rrdtool-devel
-# 2. openssl-devel
-# 3. swig
-pip_packages="astroid==1.2.1 pylint==1.3.1 pep8 unittest2 coverage rrdtool pyyaml mock xmlrunner"
-for package in $pip_packages; do
-    echo "Installing $package ..."
-    status="DONE"
-    pip install --quiet $package
-    if [ $? -ne 0 ]; then
-        status="FAILED"
-    fi
-    echo "Installing $package ... $status"
-done
-#pip install M2Crypto==0.20.2
-
-## Need this because some strange control sequences when using default TERM=xterm
-export TERM="linux"
-
-## PYTHONPATH for glideinwms source code
-# pythonpath for pre-packaged only
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/lib
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/creation/lib
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/factory
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/frontend
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/tools
-export PYTHONPATH=${PYTHONPATH}:${GLIDEINWMS_SRC}/tools/lib
+setup_python_venv $WORKSPACE
 
 cd $GLIDEINWMS_SRC/unittests
 files="test_frontend.py"
