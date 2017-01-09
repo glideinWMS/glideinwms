48af2fce6e04a91c37ac0277af65470ab2a02422
Parag Mhashilkar <parag@fnal.gov>
2017-01-09 13:47:47 -0600

    remove unused import

--- frontend/glideinFrontendElement.py
+++ frontend/glideinFrontendElement.py
@@ -31,7 +31,6 @@ import re
 sys.path.append(os.path.join(sys.path[0],"../.."))
 
 from glideinwms.lib import symCrypto,pubCrypto
-from glideinwms.lib import glideinWMSVersion
 from glideinwms.lib import logSupport
 from glideinwms.lib import cleanupSupport
 from glideinwms.lib import servicePerformance
