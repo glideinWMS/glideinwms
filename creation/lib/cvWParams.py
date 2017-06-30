c3b77b8ce8ec5f8d7e6c7d3ede1bfe0f66e5ccd3
Thomas Hein <thomas@onlineth.com>
2017-06-30 15:01:28 -0500

    Fix for libfuturize.fixes.fix_raise - ValueError for string exceptions
    Python 3 does not support string exceptions, now using ValueError()

--- creation/lib/cvWParams.py
+++ creation/lib/cvWParams.py
@@ -269,7 +269,7 @@ class VOFrontendParams(cWParams.CommonParams):
     # validate data and add additional attributes if needed
     def derive(self):
         if len(self.groups.keys())==0:
-            raise "No groups defined!"
+            raise ValueError("No groups defined!")
             
         self.validate_names()
 
