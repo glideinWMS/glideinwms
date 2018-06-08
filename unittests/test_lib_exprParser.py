918668197bf8e812bb4631a5d6cf35f0b3d6acd4
Dennis Box <dbox@fnal.gov>
2018-06-08 18:15:18 -0500

    more coverage

--- unittests/test_lib_exprParser.py
+++ unittests/test_lib_exprParser.py
@@ -10,9 +10,11 @@ from glideinwms.unittests.unittest_utils import runTest
 
 import glideinwms.lib.exprParser as ep
 
-TEST_LIST = ['a or b', '3',  'None', 'False', 'a + b',
+TEST_LIST = ['a or b', 'a or b', '3',  'None', 'False', 'a + b',
              'a*b', 'a/b', 'not a', 'x[:1]', 'str(a)', 'a<<3',
-             '(a,b,x)', '[a,b,x]', 'a<3', 'a+b>4', 'a**b', ]
+             '(a,b,x)', '[a,b,x]', 'a<3', 'a+b>4', 'a**b', 
+             'a>>3', 'a/b', 'a/3', 'lambda a,b:hash((a,b))',
+             'a-b', 'a in x', 'x[0]', 'd[a]', 'a in d',   ]
 
 TEST_RAISE_LIST = ['a^b', 'a&b', 'a|b', 'a+=3', ]
 
@@ -32,10 +34,17 @@ class TestExprParserSymmetric(unittest.TestCase):
         a = 3
         b = 4
         x = [a, b]
+        d = {a:b}
 
         # just test that nothing in TEST_LIST throws an exception when compiled
         for itm in TEST_LIST:
-            eval(ep.compile(ep.parse(itm)))
+            try:
+                eval(ep.compile(ep.parse(itm)))
+            except Exception as err:
+                bad_itm = str(err)
+                bad_itm += " for expr:"
+                bad_itm += itm
+                raise RuntimeError(bad_itm)
 
 
 if __name__ == '__main__':
