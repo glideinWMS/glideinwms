2f883369e559f6163737d494adadec47f180f480
Dennis Box <dbox@fnal.gov>
2018-09-28 18:15:53 -0500

    unit test for validate_node

--- unittests/test_creation_lib_cWDictFile_validate_node.py
+++ unittests/test_creation_lib_cWDictFile_validate_node.py
@@ -14,35 +14,35 @@ Project:
 
 from __future__ import absolute_import
 from __future__ import print_function
-import os
-import copy
 import unittest2 as unittest
 import xmlrunner
 
 from glideinwms.creation.lib.cWDictFile import validate_node
 
-GOOD_NODES = ['fermicloudui.fnal.gov:9618-9620', 'fermicloudui.fnal.gov:9618?sock=collector30-40',
- 'fermicloudui.fnal.gov:9618-9630', 'fermicloudui.fnal.gov:9618?sock=collector30-50',
- 'fermicloudui.fnal.gov:9618?sock=collector10-20', 'fermicloudui.fnal.gov:9618?sock=collector',
- 'fermicloudui.fnal.gov:9618?sock=collector30-40', 'fermicloudui.fnal.gov:9618?sock=collector30',
- 'fermicloudui.fnal.gov:9618?sock=collector', 'fermicloudui.fnal.gov:9618?sock=collector&key1=val1',
- 'name@fermicloudui.fnal.gov:9618?sock=schedd', 'fermicloudui.fnal.gov:9618?sock=my5alpha0num',
- 'fermicloudui.fnal.gov:9618?key1=val1&sock=collector&key2=val2']
+GOOD_NODES = ['fermicloudui.fnal.gov:9618-9620',
+              'fermicloudui.fnal.gov:9618?sock=collector30-40',
+              'fermicloudui.fnal.gov:9618-9630',
+              'fermicloudui.fnal.gov:9618?sock=collector30-50',
+              'fermicloudui.fnal.gov:9618?sock=collector10-20',
+              'fermicloudui.fnal.gov:9618?sock=collector',
+              'fermicloudui.fnal.gov:9618?sock=collector30-40',
+              'fermicloudui.fnal.gov:9618?sock=collector30',
+              'fermicloudui.fnal.gov:9618?sock=collector',
+              'fermicloudui.fnal.gov:9618?sock=collector&key1=val1',
+              'name@fermicloudui.fnal.gov:9618?sock=schedd',
+              'fermicloudui.fnal.gov:9618?sock=my5alpha0num',
+              'fermicloudui.fnal.gov:9618?key1=val1&sock=collector&key2=val2',
+              ]
 
 BAD_NODES = ['fermicloudui.fnal.gov:9618-9620-9999'
- 'fermicloudui.fnal.gov:9648-9630', 
- 'fermicloudui.fnal.gov:9618?sock=collector30-20', 
- 'fermicloudui.fnal.gov:9618-9620?sock=collector30-40']
-
-
+             'fermicloudui.fnal.gov:9648-9630',
+             'fermicloudui.fnal.gov:9618?sock=collector30-20',
+             'fermicloudui.fnal.gov:9618-9620?sock=collector30-40',
+             'I.dont.exist:9618',
+             ]
 
 
 class Test_validate_node(unittest.TestCase):
-    """test class for DictFileTwoKeys from
-    glideinwms/creation/lib/cWDictFile.py
-    TODO  File cWDictFile.py is 1700 lines long and contains too
-    many classes.  It should be split up
-    """
 
     def test_good(self):
         for node in GOOD_NODES:
@@ -59,7 +59,6 @@ class Test_validate_node(unittest.TestCase):
             except RuntimeError as e:
                 pass
 
-   
     def test_range_not_allowed(self):
         for node in GOOD_NODES:
             try:
@@ -67,6 +66,7 @@ class Test_validate_node(unittest.TestCase):
                 raise RuntimeError('node %s validated, it should not')
             except RuntimeError as e:
                 pass
+
 
 if __name__ == '__main__':
     OFL = 'unittests-reports'
