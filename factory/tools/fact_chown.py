17c722f4c2c76ccfed52d34a4efe053d1991d99d
Marco Mascheroni <marco.mascheroni@cern.ch>
2018-11-07 11:47:51 -0600

    Add condor to the script that change user of old jobs

--- factory/tools/fact_chown.py
+++ factory/tools/fact_chown.py
@@ -7,8 +7,10 @@ import pwd
 import logging
 import tarfile
 import argparse
+import subprocess
 from contextlib import closing  # python 2.6 tarfile does not support with
 
+import htcondor
 from glideinwms.creation.lib import factoryXmlConfig
 
 def parse_opts():
@@ -61,26 +63,33 @@ def parse_opts():
     return options
 
 
-def find_ownerid(filename):
-    return getpwuid(os.stat(filename).st_uid).pw_name
-
-
 def backup_dir(output_filename, source_dir):
+    """ Backup the directory "source_dir" into "output_filename"
+    """
     with closing(tarfile.open(output_filename, "w:gz")) as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
 def mychown(path, uid, gid, fuid, test):
-        cid = os.stat(path).st_uid  # current id
-        if cid != fuid:
-            logging.warning("Not changing %s since it's owner id is not %s" % (path, fuid))
-        if test:
-            logging.info("Test mode: not changing %s" % path)
-        else:
-            os.chown(path, uid, gid)
+    """ Change the owner id of "path" to "uid":"gid". Before doing that
+        check if the current owner of the file is "fuid". If not print a warning
+        If "test: is set don't chown but just print a message
+    """
+    cid = os.stat(path).st_uid  # current id
+    if cid != fuid:
+        logging.warning("Not changing %s since it's owner id is not %s" % (path, fuid))
+    if test:
+        logging.info("Test mode: not changing %s" % path)
+    else:
+        os.chown(path, uid, gid)
 
 
 def chown_dir(path, uid, gid, fuid, test):
+    """ Recursively go through the direcotry "path" and change the permission
+        of its files to "uid":"gid". Before doing that check if the current
+        owner of the file is fuid.
+        If "test: is set don't chown but just print a message
+    """
     mychown(path, uid, gid, fuid, test)
     for dirpath, dirnames, filenames in os.walk(path):
         for dname in dirnames:
@@ -91,9 +100,23 @@ def chown_dir(path, uid, gid, fuid, test):
             pass
 
 
+def fix_jobqueue(jobqueue_file, user, test):
+    """ Replace the username in the condor job_queue file to user
+    """
+
+    if test:
+        logging.info("Test mode: not fixing %s" % jobqueue_file)
+    else:
+        cmd = 'sed -i.bk -e \'s/103 \([-0-9.]*\) Owner ".*"/103 \\1 Owner "\'%s\'"/\' %s' % (user, jobqueue_file)
+        subprocess.call(cmd, shell=True)
+
+
 def main():
+    """ The main
+    """
     options = parse_opts()
 
+    # Factory side
     conf_file = '/etc/gwms-factory/glideinWMS.xml' #TODO
     logging.info('Loading configuration file %s' % conf_file)
     conf = factoryXmlConfig.parse(conf_file)
@@ -110,6 +133,19 @@ def main():
                 backup_dir(fe_user + '.tar.gz', fe_client_dir)
             fuid = pwd.getpwnam(fe_user).pw_uid
             chown_dir(fe_client_dir, uid, gid, fuid, options.test)
+
+    # Condor side
+    spooldir = htcondor.param['SPOOL']
+    if not 'JOBQUEUE' in htcondor.param:
+        jobqueue_file = os.path.join(spooldir, 'job_queue.log')
+    fix_jobqueue(jobqueue_file, options.user, options.test)
+    fix_jobqueue(jobqueue_file, options.user, False)
+    for i in xrange(1,9):
+        try:
+            jobqueue_file = htcondor.param['SCHEDD.SCHEDDGLIDEINS%s.JOB_QUEUE_LOG' % i]
+        except KeyError:
+            logging.warning("Cannot find 'SCHEDD.SCHEDDGLIDEINS%s.JOB_QUEUE_LOG'. Skipping it." % i)
+        fix_jobqueue(jobqueue_file, options.user, options.test)
 
 
 if __name__ == '__main__':
