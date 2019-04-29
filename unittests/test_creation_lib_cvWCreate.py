2a3a4e0389e33f9cd1f6591308c9b0d815db8ae4
Dennis D. Box <dbox@fnal.gov>
2019-04-29 11:23:26 -0500

    fixed error in unit test, added some documentation to tests per reviewers commments

--- unittests/test_creation_lib_cvWCreate.py
+++ unittests/test_creation_lib_cvWCreate.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
+"""
+Project:
+   glideinWMS
+
+Description:
+   unit test for glideinwms/creation/lib/cvWCreate.py
+
+Author:
+   Dennis Box <dbox@fnal.gov>
+"""
+
 from __future__ import absolute_import
 from __future__ import print_function
 import os
 import mock
 import unittest2 as unittest
 import xmlrunner
-
-# unittest_utils will handle putting the appropriate directories on the python
-# path for us.
-from glideinwms.unittests.unittest_utils import runTest
 
 from glideinwms.creation.lib.cvWCreate import create_client_mapfile
 from glideinwms.creation.lib.cvWCreate import create_client_condor_config
@@ -17,6 +24,7 @@ from glideinwms.creation.lib.cvWCreate import get_template
 
 
 class Test_cvWCreate(unittest.TestCase):
+
     def test_create_client_mapfile(self):
         mapfile_fname = 'condor_mapfile'
         my_DN = "/DC=org/DC=incommon/C=US/ST=IL/L=Batavia/O=Fermi Research Alliance/OU=Fermilab/CN=fermicloud001.fnal.gov"
@@ -27,7 +35,8 @@ class Test_cvWCreate(unittest.TestCase):
                       "/DC=org/DC=incommon/C=US/ST=IL/L=Batavia/O=Fermi Research Alliance/OU=Fermilab/CN=fermicloud021.fnal.gov",
                       ]
         collector_DNs = [
-            "/DC=org/DC=incommon/C=US/ST=IL/L=Batavia/O=Fermi Research Alliance/OU=Fermilab/CN=fermicloud030.fnal.gov"]
+            "/DC=org/DC=incommon/C=US/ST=IL/L=Batavia/O=Fermi Research Alliance/OU=Fermilab/CN=fermicloud030.fnal.gov"
+        ]
         pilot_DNs = []
         create_client_mapfile(
             mapfile_fname,
@@ -36,7 +45,12 @@ class Test_cvWCreate(unittest.TestCase):
             schedd_DNs,
             collector_DNs,
             pilot_DNs)
+
         self.assertTrue(os.path.exists(mapfile_fname))
+
+        # the first 6 entries of the mapfile should have
+        # these names, everything after that should be
+        # named anonymous
         m_dat = [
             "me",
             "factory0",
@@ -51,17 +65,19 @@ class Test_cvWCreate(unittest.TestCase):
             self.assertEqual(parts[-1], m_dat[idx])
             if idx + 1 < len(m_dat):
                 idx += 1
-        # dont remove it yet, we need it for
-        # test_create_client_condor_config
 
-        # os.remove(mapfile_fname)
+        os.remove(mapfile_fname)
 
     def test_get_template(self):
+
+        # test that we can fetch an existing template
         glideinWMS_dir = ".."
         template_name = 'gwms-factory.service'
         tmp = get_template(template_name, glideinWMS_dir)
         self.assertNotEqual(tmp, "")
 
+        # test that fetching a nonexistent template throws
+        # the correct Exception
         try:
             bad = get_template('I-dont-exist', glideinWMS_dir)
             assert False
@@ -69,23 +85,23 @@ class Test_cvWCreate(unittest.TestCase):
             pass
 
     def test_create_client_condor_config(self):
+        # use mock output from condor_config_val -dump
+        # to create a condor_mapfile
         config_fname = 'condor_config'
         mapfile_fname = 'condor_mapfile'
         collector_nodes = ["fermicloud001.fnal.gov", "fermicloud002.fnal.gov"]
         classad_proxy = "/tmp/classad_proxy"
 
         with mock.patch('glideinwms.lib.condorExe.exe_cmd') as m_exe_cmd:
-            f = open('fixtures/frontend/ccvd.fixture')
-            m_exe_cmd.return_value = f.readlines()
-            create_client_condor_config(
-                config_fname,
-                mapfile_fname,
-                collector_nodes,
-                classad_proxy)
+            with open('fixtures/frontend/ccvd.fixture', 'r') as fil:
+                m_exe_cmd.return_value = fil.readlines()
+                create_client_condor_config(
+                    config_fname,
+                    mapfile_fname,
+                    collector_nodes,
+                    classad_proxy)
 
-        self.assertTrue(os.path.exists(mapfile_fname))
         self.assertTrue(os.path.exists(config_fname))
-        os.remove(mapfile_fname)
         os.remove(config_fname)
 
 
