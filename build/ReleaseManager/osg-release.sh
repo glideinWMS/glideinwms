a250111afdecbb0acd3732c5d814ef23fdfc6de4
Lorena Lobato <llobato@fnal.gov>
2019-05-20 13:36:49 -0500

    Revert "Updated the osg-release from ticket 22599"
    This reverts commit e714eee3de6e1ba4d2e41d7b383eedf8573fef62.

--- build/ReleaseManager/osg-release.sh
+++ build/ReleaseManager/osg-release.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-USAGE_STR="$0 [-h] TAG USER
- TAG       - tag or branch ID in the Git repository (branch pointers change, use a branch name only for scratch builds)
- USER      - username on the OSG build machine
- -h --help - print this message and exit"
- 
-[ "$1" == "-h" ] || [ "$1" == "--help" ] && { echo "$USAGE_STR"; exit 0; } 
-
 if [ $# -ne 2 ]; then
     echo "ERROR: Missing arguments 'tag' and 'user'"
-    echo "$USAGE_STR"
-    exit 1
 fi
-
 gwms_tag=$1
 username=$2
 osg_buildmachine="library.cs.wisc.edu"
@@ -31,7 +21,6 @@ archive_gwms() {
     git clone $gwms_repo
     cd $gwms_location
     git checkout $gwms_tag
-    [ $? -ne 0 ] && { echo "ERROR: Failed to checkout $gwms_tag, aborting. Did you push your commit?"; exit 1; }
     git archive $gwms_tag --prefix='glideinwms/' | gzip > $gwms_tar
 }
 
@@ -40,10 +29,9 @@ archive_gwms
 
 ssh $username@$osg_buildmachine "mkdir -p $osg_uploaddir"
 
-if [ $? -eq 0 ]; then
+if [ "$?" = "0" ]; then
     scp $gwms_tar $username@$osg_buildmachine:$osg_uploaddir
-    [ $? -eq 0 ] && echo "Tarball Uploaded to $osg_buildmachine: $osg_uploaddir" || { echo "ERROR: failed to upload the tarball to $osg_buildmachine: $osg_uploaddir"; exit 1; }
+    echo "Tarball Uploaded to $osg_buildmachine: $osg_uploaddir"
 else
-    echo "ERROR: Failed to create directory $osg_uploaddir on the OSG build machine"
-    exit 1
+    echo "ERROR: Failed to create directory $osg_uploaddir"
 fi
