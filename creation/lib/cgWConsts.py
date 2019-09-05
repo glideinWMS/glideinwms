b4d467898d6501e51050648caad24b9817d9aa43
Leonardo Lai <leonardo.lai@live.com>
2019-09-05 11:22:12 -0500

    fixed path that wouldn't work with symlinks

--- creation/lib/cgWConsts.py
+++ creation/lib/cgWConsts.py
@@ -46,7 +46,7 @@ FRONTEND_DESCRIPT_FILE="frontend.descript"
 
 INITD_STARTUP_FILE="factory_startup"
 
-WEB_BASE_DIR=os.path.join(os.path.dirname(__file__), "..", "web_base")
+WEB_BASE_DIR=os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "web_base")
 
 ###################################################
 #
