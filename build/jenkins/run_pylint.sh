f29314712f7909f27a3ca7f5b141b4677e66a71a
Marco Mambelli <marcom@fnal.gov>
2019-10-09 18:14:00 -0500

    skipping python3 scripts

--- build/jenkins/run_pylint.sh
+++ build/jenkins/run_pylint.sh
@@ -129,7 +129,7 @@ process_branch() {
 
 
     # get list of python scripts without .py extension
-    scripts=`find glideinwms -readable -path glideinwms/.git -prune -o -exec file {} \; -a -type f | grep -i python | grep -vi '\.py' | cut -d: -f1 | grep -v "\.html$" | sed -e 's/glideinwms\///g'`
+    scripts=`find glideinwms -readable -path glideinwms/.git -prune -o -exec file {} \; -a -type f | grep -i python | grep -vi python3 | grep -vi '\.py' | cut -d: -f1 | grep -v "\.html$" | sed -e 's/glideinwms\///g'`
     cd "${GLIDEINWMS_SRC}"
     for script in $scripts; do
       #can't seem to get --ignore or --ignore-modules to work, so do it this way
