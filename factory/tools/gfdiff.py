cc79899e0a7d3fe129c13f8e7e7448c693fe998f
Marco Mascheroni <marco.mascheroni@cern.ch>
2020-03-09 12:49:47 -0500

    Pylint the gfdiff tool

--- factory/tools/gfdiff.py
+++ factory/tools/gfdiff.py
@@ -1,75 +1,101 @@
+""" This script allows to compare two different entries
+"""
+
 from __future__ import print_function
 
+import re
 import sys
-import logging
+import uuid
+import base64
+import hashlib
 import argparse
+import requests
 
 from glideinwms.creation.lib.xmlConfig import DictElement, ListElement
-from glideinwms.creation.lib.factoryXmlConfig import _parse, EntryElement, FactAttrElement, parse
+from glideinwms.creation.lib.factoryXmlConfig import _parse, EntryElement, FactAttrElement #, parse
 
+
+g_entry_a = None
+g_entry_b = None
+last_key = []
 tabs = 0
+
 def count_tabs(function_to_decorate):
+    """ Decorator function that keeps track of how many intentation level are required.
+    In other words, the decorator counts how many times the decorated function is called
+    """
     def wrapper(*args, **kw):
+        """The wrapper function
+        """
         global tabs
         tabs += 1
-        output = function_to_decorate(*args, **kw)
+        _ = function_to_decorate(*args, **kw)
         tabs -= 1
     return wrapper
 
-def check_list_diff(listA, listB):
+
+def check_list_diff(list_a, list_b):
+    """Scan the two list for differences
+    """
     SKIP_TAGS = ['infosys_ref']
-    for el in listA.children:
-        if el.tag in SKIP_TAGS:
+    for elem in list_a.children:
+        if elem.tag in SKIP_TAGS:
             continue
-        if type(el)==DictElement:
-            #print("\t"*tabs + "Checking %s" % el.tag)
-            if len(listA.children) > 2:
+        if isinstance(elem, DictElement):
+            #print("\t"*tabs + "Checking %s" % elem.tag)
+            if len(list_a.children) > 2:
                 return
             #TODO what if B does not have it
-            check_dict_diff(listA.children[0], listB.children[0],
+            check_dict_diff(list_a.children[0], list_b.children[0],
                             lambda e: e.children.items())
-        elif type(el)==FactAttrElement:
-            #print("\t"*tabs + "Checking %s" % el['name'])
-            elB = [ x for x in listB.children if x['name'] == el['name'] ]
-            if len(elB) == 1:
-                check_dict_diff(el, elB[0], FactAttrElement.items)
-            elif len(elB) == 0:
-                print("\t"*(tabs+1) + "%s: not present in %s" % (el['name'], entryB.getName()))
+        elif isinstance(elem, FactAttrElement):
+            #print("\t"*tabs + "Checking %s" % elem['name'])
+            elem_b = [x for x in list_b.children if x['name'] == elem['name']]
+            if len(elem_b) == 1:
+                check_dict_diff(elem, elem_b[0], FactAttrElement.items)
+            elif len(elem_b) == 0:
+                print("\t"*(tabs+1) + "%s: not present in %s" % (elem['name'], g_entry_b.getName()))
             else:
                 print('More than one FactAttrElement')
         else:
             print('Element type not DictElement or FactAttrElement')
-    for el in listB.children:
-        if type(el)==FactAttrElement:
-            elA = [ x for x in listA.children if x['name'] == el['name'] ]
-            if len(elA) == 0:
-                print("\t"*(tabs+1) + "%s: not present in %s" % (el['name'], entryA.getName()))
+    for elem in list_b.children:
+        if isinstance(elem, FactAttrElement):
+            elem_a = [x for x in list_a.children if x['name'] == elem['name']]
+            if len(elem_a) == 0:
+                print("\t"*(tabs+1) + "%s: not present in %s" % (elem['name'], g_entry_a.getName()))
 
 
 @count_tabs
-def check_dict_diff(dictA, dictB, itemfunc=EntryElement.items, printName=True):
-    tmpDictA = dict(itemfunc(dictA))
-    tmpDictB = dict(itemfunc(dictB))
+def check_dict_diff(dict_a, dict_b, itemfunc=EntryElement.items, print_name=True):
+    """Check differences between two dictionaries
+    """
+    tmp_dict_a = dict(itemfunc(dict_a))
+    tmp_dict_b = dict(itemfunc(dict_b))
     SKIP_KEYS = ['name', 'comment']#, 'gatekeeper']
-    for key, val in tmpDictA.items():
+    for key, val in tmp_dict_a.items():
+        last_key.append(key)
         #print("\t"*tabs + "Checking %s" % key)
         if key in SKIP_KEYS:
             continue
-        if key not in tmpDictB:
-            print("\t"*tabs + "Key %s(%s) not found in %s" % (key, val, entryB.getName()))
-        elif type(val)==ListElement:
-            check_list_diff(tmpDictA[key], tmpDictB[key])
-        elif type(val)==DictElement:
-            check_dict_diff(tmpDictA[key], tmpDictB[key], lambda e: e.children.items() if len(e.children)>0 else e.items())
-        elif tmpDictA[key] != tmpDictB[key]:
-            keystr = tmpDictA["name"] + ": " if printName else ""
+        if key not in tmp_dict_b:
+            print("\t"*tabs + "Key %s(%s) not found in %s" % (key, val, g_entry_b.getName()))
+        elif isinstance(val, ListElement):
+            check_list_diff(tmp_dict_a[key], tmp_dict_b[key])
+        elif isinstance(val, DictElement):
+            check_dict_diff(tmp_dict_a[key], tmp_dict_b[key],
+                            lambda e: e.children.items() if len(e.children) > 0 else e.items())
+        elif tmp_dict_a[key] != tmp_dict_b[key]:
+            keystr = (tmp_dict_a["name"] + ": " if print_name and "name" in tmp_dict_a
+                      else last_key[-2] + ": ")
             print("\t"*tabs + "%sKey %s is different: (%s vs %s)" %
-                  (keystr, key, tmpDictA[key], tmpDictB[key]))
-    for key, val in tmpDictB.items():
+                  (keystr, key, tmp_dict_a[key], tmp_dict_b[key]))
+        last_key.pop()
+    for key, val in tmp_dict_b.items():
         if key in SKIP_KEYS:
             continue
-        if key not in tmpDictA:
-            print("\t"*tabs + "Key %s(%s) not found in %s" % (key, val, entryA.getName()))
+        if key not in tmp_dict_a:
+            print("\t"*tabs + "Key %s(%s) not found in %s" % (key, val, g_entry_a.getName()))
 
 
 def parse_opts():
@@ -82,73 +108,106 @@ def parse_opts():
         formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument(
-        '--confA', type=str, action='store', dest='confA',
+        '--confA', type=str, action='store', dest='conf_a',
         default='/etc/gwms-factory/glideinWMS.xml',
         help='Configuration for the first entry')
 
     parser.add_argument(
-        '--confB', type=str, action='store', dest='confB',
+        '--confB', type=str, action='store', dest='conf_b',
         default='/etc/gwms-factory/glideinWMS.xml',
         help='Configuration for the first entry')
 
     parser.add_argument(
-        '--entryA', type=str, action='store', dest='entryA',
+        '--entryA', type=str, action='store', dest='entry_a',
         help='Configuration for the first entry')
 
     parser.add_argument(
-        '--entryB', type=str, action='store', dest='entryB',
+        '--entryB', type=str, action='store', dest='entry_b',
         help='Configuration for the first entry')
 
     parser.add_argument(
-        '--debug', action='store_true', dest='debug',
-        default=False,
-        help='Enable debug logging')
+        '--mergely', action='count',
+        help='Only print the mergely link')
 
     options = parser.parse_args()
 
-    #if options.entry_name is None:
-    #    logging.error('Missing required option "--entry-name"')
-    #    sys.exit(1)
-
-    # Initialize logging
-    if options.debug:
-        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
-    else:
-        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-
     return options
+
+
+def get_entry_text(entry, conf):
+    """Get an entry text from the xml configuration file
+    """
+    with open(conf) as fdesc:
+        text = fdesc.read()
+        return re.search(".*( +<entry name=\"%s\".*?</entry>)" % entry, text, re.DOTALL).group(1)
+
+
+def handle_mergely(entry_a, conf_a, entry_b, conf_b, mergely_only):
+    """Function that prints the link to the mergely website
+    """
+    url = 'http://www.mergely.com/ajax/handle_file.php'
+
+    # get a unique 8char key
+    unique_id = uuid.uuid4()
+    myhash = hashlib.sha1(str(unique_id).encode("UTF-8"))
+    key = base64.b32encode(myhash.digest())[0:8]
+
+    payload = {
+        "key" : key,
+        "name" : "lhs",
+        "content" : get_entry_text(entry_a, conf_a)
+    }
+    requests.post(url, data=payload)
+    payload["name"] = "rhs"
+    payload["content"] = get_entry_text(entry_b, conf_b)
+    requests.post(url, data=payload)
+    requests.get("http://www.mergely.com/ajax/handle_save.php?key=" + key)
+    if mergely_only:
+        print("http://www.mergely.com/" + key)
+    else:
+        print("Visualize differences at: http://www.mergely.com/" + key)
+        print()
 
 
 def main():
     """ The main
     """
-    global entryA
-    global entryB
+    global g_entry_a
+    global g_entry_b
     options = parse_opts()
 
-    entryA = options.entryA
-    entryB = options.entryB
+    if options.mergely:
+        handle_mergely(options.entry_a, options.conf_a, options.entry_b,
+                       options.conf_b, options.mergely)
+        return
+
+    entry_a = options.entry_a
+    entry_b = options.entry_b
 
     #conf = parse("/etc/gwms-factory/glideinWMS.xml")
-    confA = _parse(options.confA)
-    confB = _parse(options.confB)
+    conf_a = _parse(options.conf_a)
+    conf_b = _parse(options.conf_b)
 
-    entryA = [ e for e in confA.get_entries() if e.getName()==entryA ]
-    entryB = [ e for e in confB.get_entries() if e.getName()==entryB ]
-    if len(entryA) != 1:
-        print("Cannot find entry %s in the configuration file %s" % (options.entryA, options.confA))
+    # pylint: disable=no-member
+    entry_a = [e for e in conf_a.get_entries() if e.getName() == entry_a]
+    # pylint: disable=no-member
+    entry_b = [e for e in conf_b.get_entries() if e.getName() == entry_b]
+    if len(entry_a) != 1:
+        print("Cannot find entry %s in the configuration file %s" %
+              (options.entry_a, options.conf_a))
         sys.exit(1)
-    if len(entryB) != 1:
-        print("Cannot find entry %s in the configuration file %s" % (options.entryB, options.confB))
+    if len(entry_b) != 1:
+        print("Cannot find entry %s in the configuration file %s" %
+              (options.entry_b, options.conf_b))
         sys.exit(1)
-    entryA = entryA[0]
-    entryB = entryB[0]
+    g_entry_a = entry_a[0]
+    g_entry_b = entry_b[0]
 
 
     print("Checking entry attributes:")
-    check_dict_diff(entryA, entryB, printName=False)
+    check_dict_diff(entry_a, entry_b, print_name=False)
     print("Checking inner xml:")
-    check_dict_diff(entryA.children, entryB.children, dict.items)
+    check_dict_diff(entry_a.children, entry_b.children, dict.items)
 
 
 if __name__ == "__main__":
