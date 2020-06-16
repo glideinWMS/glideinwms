2d15d63494cb0352980d8bf7ef8a9bc1d28f4b4c
efajardo <efajardo@physics.ucsd.edu>
2020-06-16 09:44:01 -0700

    DO not check for pilots inside groups that are disables

--- creation/lib/cvWParamDict.py
+++ creation/lib/cvWParamDict.py
@@ -301,7 +301,7 @@ class frontendGroupDicts(cvWDictFile.frontendGroupDicts):
 
         # populate security data
         populate_main_security(self.client_security, params)
-        populate_group_security(self.client_security, params, sub_params)
+        populate_group_security(self.client_security, params, sub_params, self.sub_name)
 
     def reuse(self, other):
         """
@@ -930,7 +930,7 @@ def populate_main_security(client_security, params):
     client_security['collector_DNs']=collector_dns
 
 
-def populate_group_security(client_security, params, sub_params):
+def populate_group_security(client_security, params, sub_params, group_name):
     factory_dns=[]
     for collectors in (params.match.factory.collectors, sub_params.match.factory.collectors):
       for el in collectors:
@@ -953,6 +953,7 @@ def populate_group_security(client_security, params, sub_params):
 
     pilot_dns=[]
     for credentials in (params.security.credentials, sub_params.security.credentials):
+      if is_true(params.groups[group_name].enabled):
         for pel in credentials:
             if pel['pilotabsfname'] is None:
                 proxy_fname=pel['absfname']
