15394f164ca3bb988642b2f707f8464422f699da
Marco Mambelli <marcom@fnal.gov>
2020-09-03 12:31:32 -0500

    Moved BINARY_ENCODING to lib/defaults.py to make it easier to use in other parts of the code

--- creation/lib/cWDictFile.py
+++ creation/lib/cWDictFile.py
@@ -16,15 +16,7 @@ import copy
 import socket
 import io
 from glideinwms.lib import hashCrypto
-
-# GlideinWMS has to be compatible across versions running on different Python interpreters
-# Python 2 text files are the same as binary files except some newline handling
-# and strings are the same as bytes
-# To maintain this in Python 3 is possible to write binary files and use for the strings
-# any encoding that preserves the bytes (0x80...0xff) through round-tripping from byte
-# streams to Unicode and back, latin-1 is the best known of these (more compact).
-# TODO: alt evaluate the use of latin-1 text files
-BINARY_ENCODING="latin-1"
+from glideinwms.lib.defaults import BINARY_ENCODING
 
 
 ########################################
