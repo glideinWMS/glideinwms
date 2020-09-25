336802b8d9ce0b41731a925fd6e88f1dd8c437b1
Marco Mambelli <marcom@fnal.gov>
2020-09-25 14:45:05 -0500

    Fixed bats actions and paths

--- .github/actions/bats-in-sl7-docker/entrypoint.sh
+++ .github/actions/bats-in-sl7-docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PYVER=${1:-"3.6"}
 GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-`pwd`}
-glideinwms/build/jenkins/runtest.sh -vi pyunittest -a 
+glideinwms/build/jenkins/runtest.sh -vi bats -a 
 status=$?
 tar cvfj $GITHUB_WORKSPACE/logs.tar.bz2 output/*
-cat output/gwms.*.pyunittest
+cat output/gwms.*.bats
 exit $status
