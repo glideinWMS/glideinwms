03b1a93aebe1cb04dee6d9dad88533e6890734ff
Namratha Urs <nurs@fnal.gov>
2021-07-30 07:46:32 +0000

    refactoring code and recreating tarball with the changes

--- creation/web_base/cvmfs_setup.sh
+++ creation/web_base/cvmfs_setup.sh
@@ -5,27 +5,17 @@ glidein_config=$1
 
 # fetch the error reporting helper script
 error_gen=`grep '^ERROR_GEN_PATH ' $glidein_config | awk '{print $2}'`
-#echo $error_gen
 
 # get the cvmfsexec attribute switch value from the config file
 use_cvmfsexec=`grep '^GLIDEIN_USE_CVMFSEXEC ' $glidein_config | awk '{print $2}'`
 # TODO: int or string?? if string, make the attribute value case insensitive
 #use_cvmfsexec=${use_cvmfsexec,,}
-echo "GLIDEIN_USE_CVMFSEXEC attribute set to $use_cvmfsexec"
+echo "GLIDEIN_USE_CVMFSEXEC set to $use_cvmfsexec"
 
 if [[ $use_cvmfsexec -ne 1 ]]; then
-        "$error_gen" -ok "`basename $0`" "msg" "Not using cvmfsexec; skipping setup."
-        exit 0
+    "$error_gen" -ok "`basename $0`" "msg" "Not using cvmfsexec; skipping setup."
+    exit 0
 fi
-
-#test_repos=config-osg.opensciencegrid.org:singularity.opensciencegrid.org:atlas.cern.ch:glast.egi.eu:oasis.opensciencegrid.org:cms.cern.ch
-# using an array to unpack the names of additional CVMFS repositories from the colon-delimited string
-#declare -a repos
-#repos=($(echo $test_repos | tr ":" "\n"))
-#echo ${repos[@]}
-#for repo_name in "${repos[@]}"
-#do
-#done
 
 # if GLIDEIN_USE_CVMFSEXEC is set to 1 - check if CVMFS is locally available in the node
 # validate CVMFS by examining the directories within CVMFS... checking just one directory should be sufficient?
@@ -36,7 +26,6 @@ cvmfs_utils_dir=$work_dir/cvmfs_utils
 # $PWD=/tmp/glide_xxx and every path is referenced with respect to $PWD
 # source the helper script
 source $cvmfs_utils_dir/utils/cvmfs_helper_funcs.sh
-#source cvmfs_utils/utils/cvmfs_helper_funcs.sh
 
 variables_reset
 
@@ -44,36 +33,35 @@ detect_local_cvmfs
 
 # check if CVMFS is already locally mounted...
 if [[ $GWMS_IS_CVMFS_MNT -eq 0 ]]; then
-	# if it is so...
-	"$error_gen" -ok "`basename $0`" "msg" "CVMFS is locally mounted on the node; skipping setup."
-        exit 0
+    # if it is so...
+    "$error_gen" -ok "`basename $0`" "msg" "CVMFS is locally mounted on the node; skipping setup."
+    exit 0
 else
-	# if CVMFS is not found locally...
-	# get the CVMFS source information from <attr> in the glidein configuration 
-	cvmfs_source=`grep '^CVMFS_SRC ' $glidein_config | awk '{print $2}'`
-	
-	# get the CVMFS requirement setting passed as one of the factory attributes
-	glidein_cvmfs=`grep '^GLIDEIN_CVMFS ' $glidein_config | awk '{print $2}'`
-	
-	perform_system_check
-	
-	os_like=$GWMS_OS_DISTRO
-	os_ver=`echo $GWMS_OS_VERSION | awk -F'.' '{print $1}'`
-	arch=$GWMS_OS_KRNL_ARCH
-	dist_file=cvmfsexec-${cvmfs_source}-${os_like}${os_ver}-${arch}
-	
-	tar -xvzf $cvmfs_utils_dir/utils/cvmfs_distros.tar.gz -C $cvmfs_utils_dir distros/$dist_file
-	
-	. $cvmfs_utils_dir/utils/cvmfs_mount.sh
-	
-	if [[ $GWMS_IS_CVMFS -eq 0 ]]; then
-		# CVMFS is now available on the worker node"
-	        # mimicking the behavior of the glidein on the worker node (start the user job once the CVMFS repositories are mounted)
-		loginfo "Starting user job..."
-		# . user_job.sh
-	else
-	       	logerror "CVMFS is still unavailable on the worker node"
-		# Error occured during mount of CVMFS repositories"
-		exit 1
-	fi
+    # if CVMFS is not found locally...
+    # get the CVMFS source information from <attr> in the glidein configuration 
+    cvmfs_source=`grep '^CVMFS_SRC ' $glidein_config | awk '{print $2}'`
+    
+    # get the CVMFS requirement setting passed as one of the factory attributes
+    glidein_cvmfs=`grep '^GLIDEIN_CVMFS ' $glidein_config | awk '{print $2}'`
+    	
+    perform_system_check
+    
+    os_like=$GWMS_OS_DISTRO
+    os_ver=`echo $GWMS_OS_VERSION | awk -F'.' '{print $1}'`
+    arch=$GWMS_OS_KRNL_ARCH
+    dist_file=cvmfsexec-${cvmfs_source}-${os_like}${os_ver}-${arch}
+    
+    tar -xvzf $cvmfs_utils_dir/utils/cvmfs_distros.tar.gz -C $cvmfs_utils_dir distros/$dist_file
+    
+    . $cvmfs_utils_dir/utils/cvmfs_mount.sh	
+    
+    if [[ $GWMS_IS_CVMFS -eq 0 ]]; then
+        # CVMFS is now available on the worker node"
+        loginfo "Proceeding to execute user job..."
+    else
+        # Error occured during mount of CVMFS repositories"
+        logerror "Error occured during mount of CVMFS repositories."
+        "$error_gen" -error "`basename $0`" "WN_Resource" "Mount unsuccessful... CVMFS is still unavailable on the node."
+        exit 1
+    fi
 fi
--- creation/web_base/cvmfs_umount.sh
+++ creation/web_base/cvmfs_umount.sh
@@ -34,8 +34,8 @@ use_cvmfsexec=`grep '^GLIDEIN_USE_CVMFSEXEC ' $glidein_config | awk '{print $2}'
 echo "GLIDEIN_USE_CVMFSEXEC attribute set to $use_cvmfsexec"
 
 if [[ $use_cvmfsexec -ne 1 ]]; then
-        "$error_gen" -ok "`basename $0`" "msg" "Not using cvmfsexec; skipping cleanup."
-        exit 0
+    "$error_gen" -ok "`basename $0`" "umnt_msg1" "Not using cvmfsexec; skipping cleanup."
+    exit 0
 fi
 
 # get the glidein work directory location from glidein_config file
@@ -57,28 +57,30 @@ loginfo  "Start log for unmounting CVMFS"
 detect_local_cvmfs
 
 if [[ $GWMS_IS_CVMFS_MNT -eq 0 ]]; then
-	# CVMFS is mounted locally in the filesystem; DO NOT UNMOUNT!!
-	"$error_gen" -ok "`basename $0`" "msg" "CVMFS is locally mounted on the node; skipping cleanup."
-	exit 0
+    # CVMFS is mounted locally in the filesystem; DO NOT UNMOUNT!
+    loginfo "Skipping unmounting of CVMFS as it already is locally provisioned in the node!"
+    "$error_gen" -ok "`basename $0`" "umnt_msg2" "CVMFS is locally mounted on the node; skipping cleanup."
+    exit 0
 else
-	loginfo "Unmounting CVMFS..."
-	$cvmfs_utils_dir/distros/.cvmfsexec/umountrepo -a
-	
-	# check again to ensure all CVMFS repositories were unmounted by umountrepo
-	#df -h | grep /cvmfs &> /dev/null && logerror "One or more CVMFS repositories might not be completely unmounted" || loginfo "CVMFS repositories unmounted"
-	# searching for "/dev/fuse" since "/cvmfs" returns false positives (/etc/auto.fs /cvmfs line)
-	cat /proc/$$/mounts | grep /dev/fuse &> /dev/null && logerror "One or more CVMFS repositories might not be completely unmounted" || loginfo "CVMFS repositories unmounted"
-	
-	# returning 0 to indicate the unmount process was successful
-	true
-
+    loginfo "Unmounting CVMFS (that mounted by the glidein)..."
+    $cvmfs_utils_dir/distros/.cvmfsexec/umountrepo -a
+    
+    # check again to ensure all CVMFS repositories were unmounted by umountrepo
+    #df -h | grep /cvmfs &> /dev/null && logerror "One or more CVMFS repositories might not be completely unmounted" || loginfo "CVMFS repositories unmounted"
+    # searching for "/dev/fuse" since "/cvmfs" returns false positives (/etc/auto.fs /cvmfs line)
+    cat /proc/$$/mounts | grep /dev/fuse &> /dev/null && logerror "One or more CVMFS repositories might not be completely unmounted" || loginfo "CVMFS repositories unmounted"
+    
+    "$error_gen" -ok "`basename $0`" "umnt_msg3" "Glidein-based CVMFS unmount was successful."
+    # returning 0 to indicate the unmount process was successful
+    true
+    
 #else
-	# CVMFS mount points do not exist in the file system
-#	loginfo "No CVMFS repositories found mounted. Exiting the script..."
-	
-	# returning 1 to indicate that unmount process failed (i.e. nothing was unmounted as CVMFS was not previously mounted)
-#	false
-	
+    # CVMFS mount points do not exist in the file system
+#    loginfo "No CVMFS repositories found mounted. Exiting the script..."
+    
+    # returning 1 to indicate that unmount process failed (i.e. nothing was unmounted as CVMFS was not previously mounted)
+    #	false
+    
 fi
 
 ########################################################################################################
