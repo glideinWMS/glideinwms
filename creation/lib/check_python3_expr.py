8621c696d9b0cf4f557044fd3d3cdd9697db52cd
Bruno Coimbra <coimbra@fnal.gov>
2021-08-09 15:19:03 -0500

    Implement match_attr type check

--- creation/lib/check_python3_expr.py
+++ creation/lib/check_python3_expr.py
@@ -29,6 +29,7 @@ except:
 
 CONFIG_FILE = "/etc/gwms-frontend/frontend.xml"
 
+
 def check_syntax(code):
     """Validates the Python 3 syntax of a code.
 
@@ -47,6 +48,46 @@ def check_syntax(code):
 
     return error
 
+
+def check_types(expression, factory_attrs, job_attrs):
+    """Validates the types of match_attrs in a match_expr.
+
+    Args:
+        code (str): Code to validate.
+
+    Returns:
+        str: None if code is valid. Error message if the code is invalid.
+    """
+
+    # Mock job dictionary and
+    default_value = {
+        "string": "",
+        "int": 0,
+        "real": 0.0,
+        "bool": False,
+        "Expr": ""
+    }
+
+    try:
+        job = {attr: default_value[type] for (attr, type) in job_attrs}
+        glidein = {
+            "attrs":
+            {attr: default_value[type]
+             for (attr, type) in factory_attrs}
+        }
+    except KeyError as e:
+        return f"Invalid match_attr type: {e.args[0]}"
+
+    # Evaluate expression
+    error = None
+    try:
+        eval(expression)
+    except Exception as e:
+        error = e
+
+    return error
+
+
 def check_2to3(code, patch=False, refactoring_tool=rt):
     """Evaluates an expression using 2to3 and returns refactoring suggestions.
 
@@ -62,8 +103,12 @@ def check_2to3(code, patch=False, refactoring_tool=rt):
     suggestion = None
     if refactoring_tool:
         try:
-            suggested_code = str(refactoring_tool.refactor_string(f"{code}\n", None))[:-1]
-            diff = '\n'.join(difflib.unified_diff(code.split("\n"), suggested_code.split("\n"), lineterm=''))
+            suggested_code = str(
+                refactoring_tool.refactor_string(f"{code}\n", None))[:-1]
+            diff = '\n'.join(
+                difflib.unified_diff(code.split("\n"),
+                                     suggested_code.split("\n"),
+                                     lineterm=''))
             if len(diff) > 0:
                 if patch:
                     suggestion = diff
@@ -71,8 +116,9 @@ def check_2to3(code, patch=False, refactoring_tool=rt):
                     suggestion = suggested_code
         except:
             suggested_code = "could not parse the expression"
-    
+
     return suggestion
+
 
 def findall_path(root, tag, elements=[]):
     """Finds all elements in `root` of `tag` type preserving their paths.
@@ -99,8 +145,9 @@ def findall_path(root, tag, elements=[]):
         if child.tag == tag:
             elements.append(newElement)
         findall_path(newElement, tag, elements)
-    
+
     return elements
+
 
 def element_name(element):
     """Finds the name attribute of element. Returns `None` if nothing is found.
@@ -121,10 +168,28 @@ def element_name(element):
     except KeyError:
         return None
 
+
+def match_attrs_to_tuples(match_attrs):
+    """Converts a match_attrs element to a list of tuples.
+
+    Args:
+        match_attrs (Element): match_attrs element.
+
+    Returns:
+        list: List of tuples.
+    """
+
+    tuples = []
+    for attr in match_attrs.getchildren():
+        tuples.append((attr.attrib["name"], attr.attrib["type"]))
+    return tuples
+
+
 def _log(text, silent=False):
     if silent:
         return
     sys.stdout.write(text)
+
 
 def main(config_file, enforce_2to3=False, silent=False, refactoring_tool=rt):
     """Parse the Frontend configuration in config_file and validate Python code.
@@ -138,9 +203,9 @@ def main(config_file, enforce_2to3=False, silent=False, refactoring_tool=rt):
         list: List of results for every element evaluated
     """
 
-    _log("NOTE: This tool can't determine variable types a priori. "
-         "Python 3 has stricter type restrictions which may cause match expressions to fail in execution time. "
-         "Please, make sure match_attr types are appropriately defined.\n")
+    _log(
+        "NOTE: Python 3 has stricter type restrictions which may cause match expressions to fail in execution time. "
+        "Please, make sure match_attr types are appropriately defined.\n")
 
     if enforce_2to3 and not refactoring_tool:
         _log("2to3 not found and will not be enforced")
@@ -154,7 +219,7 @@ def main(config_file, enforce_2to3=False, silent=False, refactoring_tool=rt):
         return "Config file not readable: %s" % config_file
     except:
         return "Error parsing config file: %s" % config_file
-    
+
     # Recursively finds all <match> elements in the XML
     for element in findall_path(tree.getroot(), "match"):
         # Validates match expressions attributes
@@ -167,8 +232,15 @@ def main(config_file, enforce_2to3=False, silent=False, refactoring_tool=rt):
             result["type"] = "match_expr"
             result["value"] = expr
             result["location"] = location
-            error = check_syntax(expr)
             _log("\nSyntax check: ", silent)
+            error = check_syntax(expr)
+            if not error:
+                factory_attrs = match_attrs_to_tuples(
+                    element.data.find("./factory/match_attrs"))
+                job_attrs = match_attrs_to_tuples(
+                    element.data.find("./job/match_attrs"))
+                if factory_attrs or job_attrs:
+                    error = check_types(expr, factory_attrs, job_attrs)
             if not error:
                 _log("passed\n", silent)
                 result["valid"] = True
@@ -236,21 +308,35 @@ def main(config_file, enforce_2to3=False, silent=False, refactoring_tool=rt):
 
     return passed, report
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Validate expressions in frontend.xml for compatibility with Python 3.')
-    parser.add_argument("-f", "--file", metavar="PATH", type=str, help="path to the configuration file")
-    parser.add_argument("-s", "--silent", action="store_true", help="silent mode")
-    parser.add_argument("--enforce-2to3", action="store_true", help="treats 2to3 suggestions as errors")
+    parser = argparse.ArgumentParser(
+        description=
+        'Validate expressions in frontend.xml for compatibility with Python 3.'
+    )
+    parser.add_argument("-f",
+                        "--file",
+                        metavar="PATH",
+                        type=str,
+                        help="path to the configuration file")
+    parser.add_argument("-s",
+                        "--silent",
+                        action="store_true",
+                        help="silent mode")
+    parser.add_argument("--enforce-2to3",
+                        action="store_true",
+                        help="treats 2to3 suggestions as errors")
     args = parser.parse_args()
 
     if args.file:
         config_file = args.file
     else:
         config_file = CONFIG_FILE
-    
+
     passed, _ = main(config_file, args.enforce_2to3, args.silent)
     if passed:
-        _log("\n\nPassed (configuration compatible with python3)\n", args.silent)
+        _log("\n\nPassed (configuration compatible with python3)\n",
+             args.silent)
         exit(0)
     else:
         _log("\n\nFailed (invalid python3 in configuration)\n", args.silent)
