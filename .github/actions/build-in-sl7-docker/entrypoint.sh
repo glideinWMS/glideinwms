7aa12c769faccd163f16b061ea5e87edf8a30504
Marco Mambelli <marcom@fnal.gov>
2021-11-10 13:18:57 -0600

    Added RPM build w/o using mock

--- .github/actions/build-in-sl7-docker/entrypoint.sh
+++ .github/actions/build-in-sl7-docker/entrypoint.sh
@@ -3,10 +3,11 @@ export PYVER=${1:-"3.6"}
 GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-`pwd`}
 export SW_VER=${RELEASE_VERSION:-"master"}
 export RPM_VER=${RPM_VERSION:-"3.7.5"}
-export RPM_REL=${RPM_RELESE:-"55.post.nightly"}
+export RPM_REL=${RPM_RELESE:-"55"}
+export RPM_FULLREL="$RPM_REL.post.$(date +"%Y%m%d%H%M")"
 export HERE="`pwd`"
 mkdir rel
-glideinwms/build/ReleaseManager/release.py --release-version=$SW_VER --source-dir="$HERE"/glideinwms --release-dir="$HERE"/rel --rpm-release=$RPM_REL --rpm-version=$RPM_VER 
+glideinwms/build/ReleaseManager/release.py --no-mock --release-version=$SW_VER --source-dir="$HERE"/glideinwms --release-dir="$HERE"/rel --rpm-release=$RPM_FULLREL --rpm-version=$RPM_VER 
 status=$?
 mkdir gwms_rpms
 #mkdir rpm_logs
