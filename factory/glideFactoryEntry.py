d7eac90f67bf8b59ebc419c55ca29cd8a6d11b7a
Dennis D. Box <dbox@fnal.gov>
2021-12-23 18:32:31 +0000

    change encoding on write for scitoken

--- factory/glideFactoryEntry.py
+++ factory/glideFactoryEntry.py
@@ -1257,7 +1257,7 @@ def unit_work_v3(entry, work, client_name, client_int_name, client_int_req,
             try:
                 entry.log.info("frontend_scitoken supplied, writing to %s" % scitoken_file)
                 chmod(tmpnm,0o600)
-                os.write(fd, scitoken_data)
+                os.write(fd, scitoken_data.encode('utf-8'))
                 os.close(fd)
                 util.file_tmp2final(scitoken_file, tmpnm)
             except Exception as err:
