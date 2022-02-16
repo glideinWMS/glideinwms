9910870cd224c22978d8a092269046fac7d9e0d1
Marco Mascheroni <marco.mascheroni@cern.ch>
2022-02-16 15:59:52 +0100

    [v3_7] Enable submit attrs for ARC REST interface

--- creation/lib/cgWCreate.py
+++ creation/lib/cgWCreate.py
@@ -360,7 +360,7 @@ class GlideinSubmitDictFile(cgWDictFile.CondorJDLDictFile):
     def populate_submit_attrs(self, submit_attrs, gridtype, attr_prefix=''):
         for submit_attr in submit_attrs:
             if submit_attr.get(u'all_grid_types', 'False') == 'True' or gridtype.startswith(
-                    'batch ') or gridtype in ('condor', 'gce', 'ec2'):
+                    'batch ') or gridtype in ('condor', 'gce', 'ec2', 'arc'):
                 self.add(
                     '%s%s' %
                     (attr_prefix,
