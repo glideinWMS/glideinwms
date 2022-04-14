112227993eaad6a1eab48290299c79a6bc6d74ac
Dennis D. Box <dbox@fnal.gov>
2022-04-14 15:10:26 -0500

    Special handling of multiline condor config values to work around condor bug CB1032. GH#68

--- creation/lib/cvWCreate.py
+++ creation/lib/cvWCreate.py
@@ -78,11 +78,101 @@ def create_client_mapfile(mapfile_fname, my_DN, factory_DNs, schedd_DNs, collect
     return
 
 
+# TODO: CB1032 The following 2 functions are a workaround because of a "condor_config_val -dump" bug, to handle
+#       multiline attributes in condor_config. They will need changes once the bug is fixed
+#       This workaround is not working if multiline attributes are redefined w/ different number of lines
+#       https://opensciencegrid.atlassian.net/browse/HTCONDOR-1032
+def find_multilines(config_text):
+    """
+    Parses condor config file looking for multiline entries
+    Args:
+        config_text: string, contents of a condor configuration file
+    Returns:
+        multi: dictionary. keys are first line of multi line config
+                         values are the rest of the multi line config
+                         keeping original formatting
+
+        see parse_configs_for_multis() below for example muli dict
+    """
+    multi = {}
+    tag = None
+    dict_key = None
+    for line in config_text:
+        parts = line.split()
+        if tag is None:
+            for idx in range(len(parts)):
+                if parts[idx].startswith("@="):
+                    tag = parts[idx].replace("=", "").strip()
+                    dict_key = parts[idx - 1].strip()
+                    multi[dict_key] = "".join(parts[idx:]) + "\n"
+        else:
+            if "#" not in line:
+                multi[dict_key] += line
+            for idx in range(len(parts)):
+                if tag in parts[idx]:
+                    tag = None
+    return multi
+
+
+def parse_configs_for_multis(conf_list):
+    """
+    parse list of condor config files searching for multi line configurations
+    Args:
+       conf_list: string, output of condor_config_val -config
+    Returns:
+        multi: dictionary. keys are first line of multi line config
+                         values are the rest of the multi line config
+                         keeping original formatting
+
+        example: this paragraph in a  condor_configuration :
+
+        JOB_ROUTER_CREATE_IDTOKEN_atlas @=end
+            sub = "Atlasfetime = 900"
+            lifetime = 900
+            scope = "ADVERTISE_STARTD, ADVERTISE_MASTER, READ"
+            dir = "$(LOCAL_DIR)/jrtokens"
+            filename = "ce_atlas.idtoken"
+            owner = "atlas"
+        @end
+
+        would generate a multi entry like this:
+
+        multi["JOB_ROUTER_CREATE_IDTOKEN_atlas"] =
+            '@=end\n    sub = "Atlas"\n    lifetime = 900\n  .....   @end\n'
+
+       these entries will be rendered into the frontend.condor_config with proper spacing and line returns
+       unlike how they would be  rendered by  condor_config_val --dump
+
+       KNOWN PROBLEM: if condor config has two multi-line configs with same name and different
+       lines generated config file may be incorrect.  The condor config is probably incorrect
+       as well :)
+
+    """
+    multi = {}
+    for conf in conf_list:
+        conf = conf.strip()
+        if os.path.exists(conf):
+            with open(conf) as fd:
+                text = fd.readlines()
+                pdict = find_multilines(text)
+            multi.update(pdict)
+    return multi
+
+
 #########################################
 # Create frontend-specific condor_config
 def create_client_condor_config(config_fname, mapfile_fname, collector_nodes, classad_proxy):
-    attrs = condorExe.exe_cmd("condor_config_val", "-dump")
-    def_attrs = filter_unwanted_config_attrs(attrs)
+    config_files = condorExe.exe_cmd("condor_config_val", "-config")
+    # TODO: CB1032 Change once condor_config_val -dump is fixed.
+    # feeding [] into parse_configs_for_multis() or
+    # setting multi_line_config_dict to {} in filter_unwanted_cofig_attrs()
+    # would give desired behavior
+    multi_line_conf_dict = parse_configs_for_multis(config_files)
+    attrs = condorExe.exe_cmd("condor_config_val", "-dump ")
+    def_attrs = filter_unwanted_config_attrs(attrs, multi_line_conf_dict)
+    for tag in multi_line_conf_dict:
+        line = f"{tag}  {multi_line_conf_dict[tag]}"
+        def_attrs.append(line)
 
     with open(config_fname, "w") as fd:
         fd.write("############################################\n")
@@ -172,7 +262,39 @@ def create_client_condor_config(config_fname, mapfile_fname, collector_nodes, cl
     return
 
 
-def filter_unwanted_config_attrs(attrs):
+# TODO: CB1032 Undo changes for multiline attributes once condor bug is fixed
+def filter_unwanted_config_attrs(attrs, mlcd):
+    """
+    Places '#' in front of unwanted condor configuration settings
+    prior to printing it all to the frontend.condor_config file
+
+    Args:
+        attrs: list of strings, output from condor_config_val -dump
+        mlcd: multi line config dict
+             given multi line input in a condor config file like so:
+             CONDOR_SETTING_1  @=xx
+                 REQUIREMENTS regexp("^docker://[^/]+$", SingularityImage)
+                 COPY SingularityImage orig_SingularityImage
+                 EVALSET SingularityImage replace("^docker://(.+)", SingularityImage, "docker://docker.io/library/\\1")
+             @xx
+
+             condor_config_val -dump munges it into something indigestable in a config file:
+
+             CONDOR_SETTING_1 = REQUIREMENTS regexp("^docker://[^/]+$", SingularityImage)
+             COPY SingularityImage orig_SingularityImage
+             EVALSET SingularityImage replace("^docker://(.+)", SingularityImage, "docker://docker.io/library/\\1")
+
+             an mlcd entry will be
+
+             mlcd['CONDOR_SETTING_1'] = "@=xx\n   REQUIREMENTS regexp("^docker://[^/]+$", SingularityImage).......  @xx"
+
+             the (incorrect) settings from above going through condor_config_val -dump will be filtered out of
+             attrs, and replaced with correct formatting from contents of mlcd
+
+     Returns:
+        attrs: list of strings reformatted as valid condor config for frontend
+
+    """
     unwanted_attrs = []
 
     # Make sure there are no tool specific and other unwanted settings
@@ -204,6 +326,7 @@ def filter_unwanted_config_attrs(attrs):
             unwanted_attrs.append("SEC_%s_AUTHENTICATION_METHODS" % context)
             unwanted_attrs.append("SEC_%s_INTEGRITY" % context)
 
+    # comment out 'normal' (single line, unaffected by the bug CB1032) unwanted_attrs
     for uattr in unwanted_attrs:
         for i in range(0, len(attrs)):
             attr = ""
@@ -211,11 +334,43 @@ def filter_unwanted_config_attrs(attrs):
                 attr = ((attrs[i].split("="))[0]).strip()
             if attr == uattr:
                 attrs[i] = "#%s" % attrs[i]
+
+    # comment out  multi-line unwanted_attrs from attrs
+    # mlcd keys are the beginning of multiline macros, append them to unwanted_attrs
+    begin_mlcd = len(unwanted_attrs)
+    for key in mlcd:
+        unwanted_attrs.append(key)
+    # mlcd[key] = (all the lines of the multi line macro with spacing and punctuation)
+    # attrs currently has these lines output in a way that condor cannot ingest as
+    # a configuration file, so comment them out
+    for mlcd_key in unwanted_attrs[begin_mlcd:]:
+        for i in range(0, len(attrs)):
+            attr = ""
+            if len(attrs[i].split("=")) > 0:
+                attr = ((attrs[i].split("="))[0]).strip()
+            if attr == mlcd_key:
+                if attr in mlcd:
+                    # comment out key of mlcd in attrs
+                    attrs[i] = "#mlcd  %s" % attrs[i]
+                    # now have to comment contents of mlcd[key] out of attrs
+                    mlcd_values = mlcd[attr].split("\n")
+                    for ctr in range(len(mlcd_values)):
+                        for ctr2 in range(len(mlcd_values)):
+                            if attrs[i + ctr + 1].strip() == mlcd_values[ctr2].strip():
+                                # found it, comment it
+                                attrs[i + ctr + 1] = "#mlcd  %s" % attrs[i + ctr + 1]
+                                break
+    # we commented mlcd attrs in a way that they are easy to identify
+    # not strictly necessary to do this step but
+    # frontend.condor_config looks ugly if this  is not done
+    for attr in reversed(attrs):
+        if "#mlcd" in attr:
+            attrs.remove(attr)
+
     return attrs
 
 
 def get_template(template_name, glideinWMS_dir):
     with open(f"{glideinWMS_dir}/creation/templates/{template_name}") as template_fd:
         template_str = template_fd.read()
-
     return template_str
