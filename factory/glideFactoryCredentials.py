57f4443e84ef95f09038984ac44cdee2505645c8
Dennis Box <dbox@fnal.gov>
2022-04-27 16:03:45 -0500

    some code reformatting

--- factory/glideFactoryCredentials.py
+++ factory/glideFactoryCredentials.py
@@ -286,8 +286,9 @@ def check_security_credentials(auth_method, params, client_int_name, entry_name,
 
 
 
-    if 'frontend_scitoken' in params and scitoken_passthru:
-            return
+    if 'scitoken' in auth_method_list or 'frontend_scitoken' in params and scitoken_passthru:
+        #TODO check validity 
+        return
     if 'grid_proxy' in auth_method_list:
         if 'SubmitProxy' in params:
             # v3+ protocol
