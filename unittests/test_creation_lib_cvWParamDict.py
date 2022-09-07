be99943df92cd2df200c86ffe3c3e692d6d93d8d
terranovaa <terranovafrplus@gmail.com>
2022-09-07 16:56:21 -0500

    Solved pre-commit reported issues

--- unittests/test_creation_lib_cvWParamDict.py
+++ unittests/test_creation_lib_cvWParamDict.py
@@ -84,7 +84,7 @@ class TestFrontendMainDicts(unittest.TestCase):
         pd = self.fed.find_parent_dir("fixtures/frontend", "index.html")
         self.assertEqual(pd, "fixtures/frontend/web-base/frontend")
 
-    #TODO(F)
+    # TODO(F)
     def test_save_monitor(self):
         self.fed.save_monitor()
 
