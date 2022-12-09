6384edca43fe08ad6fcd24ddfef6ee30f60a2e19
Marco Mambelli <marcom@fnal.gov>
2022-12-08 20:17:23 -0600

    Fixed status reporting for gconfig.py and added utility function to replace error_gen in python scripts

--- creation/web_base/gconfig.py
+++ creation/web_base/gconfig.py
@@ -19,6 +19,8 @@ import re
 import sys
 import tempfile
 
+from datetime import datetime
+
 # Singleton glidein_config dictionary
 _GLIDEIN_CONFIG = None
 BINARY_ENCODING = "utf_8"  # Use latin_1 instead?
@@ -338,7 +340,93 @@ def _complete_args(mandatory, args, defaults):
     return args
 
 
+def _get_status_header(fname, status):
+    """Return the status header lines
+
+    Args:
+        fname (str): program name/ID
+        status(str): program status, OK or ERROR
+
+    Returns:
+        list: list of header lines (str)
+    """
+    return [
+        '<?xml version="1.0"?>',
+        '<OSGTestResult id="%s" version="4.3.1">' % fname,
+        "  <result>",
+        "    <status>%s</status>" % status,
+    ]
+
+
+def _get_status_metric(name, value, timestamp):
+    """Get a formatted string with a status metric
+
+    Args:
+        name (str): metric name
+        value (str): metric value
+        timestamp (str): timestamp string
+
+    Returns:
+        str: formatted status metric
+    """
+    return '    <metric name="%s" ts="%s" uri="local">%s</metric>' % (name, timestamp, value)
+
+
+def _get_status_footer(error_detail=None):
+    """Return the status footer lines
+
+    Args:
+        error_detail (str, None): error detail information
+
+    Returns:
+        list: list of status lines (str)
+    """
+    out = ["  </result>"]
+    if error_detail:
+        out += ["  <detail>", "    %s" % error_detail, "  </detail>"]
+    out.append("</OSGTestResult>")
+    return out
+
+
+def status_report(name, success, error_msg=None, error_detail=None, parameters=None, fname="otrb_output.xml"):
+    """Write an XML status report to "otrb_output.xml" like error_gen.sh
+
+    Same as invoking (with all parameters quoted to preserve spaces):
+        error_gen -ok key1 val1 key2 val2 ...
+        error_gen -error error_msg error_detail key1 val1 key2 val2 ...
+
+    Args:
+        name (str): ID, script invoking the report
+        success (bool): True for OK/-ok (script successful), False for ERROR/-error (reporting failure)
+        error_msg (str, None): error message, if any
+        error_detail (str, None): error detail, if any
+        parameters (dict): status parameters
+        fname (str): path of the status file
+    """
+    my_date = datetime.now()
+    timestamp = my_date.strftime("%Y-%m-%dT%H:%M:%S%:z")
+    if success:
+        out = _get_status_header(name, "OK")
+    else:
+        out = _get_status_header(name, "ERROR")
+        out.append(_get_status_metric("failure", error_msg, timestamp))
+    if parameters:
+        for k, v in parameters:
+            out.append(_get_status_metric(k, v, timestamp))
+    out += _get_status_footer(error_detail)
+    with open(fname, "w") as f:
+        f.writelines(["%s\n" % i for i in out])
+
+
 def main():
+    valid_commands = ["get", "add", "add_safe", "add_config_line", "add_condor_config_var"]
+    # Check if invoked by glidein_startup.sh at setup
+    if sys.argv[1] not in valid_commands and os.path.exists(sys.argv[1]):
+        # assuming that the script was called with glidien_config as parameter if it is a file and there is error_gen
+        error_gen = gconfig_get("ERROR_GEN_PATH", sys.argv[1])
+        if error_gen:
+            status_report("gconfig.py", True)
+            sys.exit(0)
     # Help text
     description = "Pure Python implementation of gconfig utils (add_config_line.source).\nIt expects 'glidein_config' to be defined in the environment."
     usage = "gconfig [-h] command [args]"
