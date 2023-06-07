34294945f8e16c1ba3e0ed8e00ff368a4b0e50cc
Namratha Urs <namrathaurs@my.unt.edu>
2023-06-07 09:48:14 -0500

    few more modifications
    - removed the previously existing version of cvmfs setup script since a new version is in place
    - updated the node validation script based on linting suggestions

--- creation/web_base/validate_node.sh
+++ creation/web_base/validate_node.sh
@@ -20,14 +20,14 @@ function check_df {
     chdf_reqmbs=$2
     chdf_id=$3
 
-    free=`df -kP $chdf_dir | awk '{if (NR==2) print $4}'`
+    free=$(df -kP $chdf_dir | awk '{if (NR==2) print $4}')
     let "chdf_reqkbs=$chdf_reqmbs * 1024"
     if [ $free -lt $chdf_reqkbs ]; then
         #echo "Space on '$chdf_dir' not enough." 1>&2
         #echo "At least $chdf_reqmbs MBs required, found $free KBs" 1>&2
         STR="Space on '$chdf_dir' not enough.\n"
         STR+="At least $chdf_reqmbs MBs required, found $free KBs"
-	STR1=`echo -e "$STR"`
+	STR1=$(echo -e "$STR")
         "$error_gen" -error "validate_node.sh" "WN_Resource" "$STR1" ${chdf_id}FreeKb $free  ${chdf_id}MinKb ${chdf_reqkbs}
         exit 1
     fi
@@ -41,18 +41,18 @@ function check_quotas {
     chq_reqmbs=$2
     chq_id=$3
 
-    fs=`df -kP $chq_dir | awk '{if (NR==2) print $1}'`
-    myquotastr=`quota 2>/dev/null | awk '{if (NR>2) {if (NF==1) {n=$1; getline; print n " " $2-$1} else {print $1 " " $3-$2}}}' |grep $fs`
+    fs=$(df -kP $chq_dir | awk '{if (NR==2) print $1}')
+    myquotastr=$(quota 2>/dev/null | awk '{if (NR>2) {if (NF==1) {n=$1; getline; print n " " $2-$1} else {print $1 " " $3-$2}}}' |grep $fs)
     if [ $? -eq 0 ]; then
 	# check only if there are any quotas, else ignore
-	myquota=`echo $myquotastr|awk '{print $2}'`
+	myquota=$(echo $myquotastr|awk '{print $2}')
 	let "blocks=$chdf_reqmbs * 1024 * 2"
 	if [ $myquota -lt $blocks ]; then
             #echo "Quota on '$chdf_dir' too small." 1>&2
             #echo "At least $chdf_reqmbs MBs required, found $myquota blocks" 1>&2
             STR="Quota on '$chdf_dir' too small.\n"
             STR+="At least $chdf_reqmbs MBs required, found $myquota blocks"
-	    STR1=`echo -e "$STR"`
+	    STR1=$(echo -e "$STR")
             "$error_gen" -error "validate_node.sh" "WN_Resource" "$STR1" ${chq_id}FreeBlocks $blocks ${chq_id}QuotaBlocks $myquota
 	    exit 1
 	fi
@@ -73,7 +73,7 @@ function check_quotas {
 config_file="$1"
 
 # import add_config_line and add_condor_vars_line functions
-add_config_line_source=$(grep -m1 '^ADD_CONFIG_LINE_SOURCE ' "$glidein_config" | cut -d ' ' -f 2-)
+add_config_line_source=$(grep -m1 '^ADD_CONFIG_LINE_SOURCE ' "$config_file" | cut -d ' ' -f 2-)
 # shellcheck source=./add_config_line.source
 . "$add_config_line_source"
 
@@ -82,14 +82,14 @@ error_gen=$(gconfig_get ERROR_GEN_PATH "$config_file")
 #
 # Check space on current directory
 #
-reqgbs=`grep -i "^MIN_DISK_GBS " "$config_file" | cut -d ' ' -f 2-`
+reqgbs=$(grep -i "^MIN_DISK_GBS " "$config_file" | cut -d ' ' -f 2-)
 if [ -n "$reqgbs" ]; then
  # can only check if defined
  let "reqmbs=$reqgbs * 1024"
 
  check_df . $reqmbs "Cwd"
 
- reqquotas=`grep -i "^CHECK_QUOTA " "$config_file" | cut -d ' ' -f 2-`
+ reqquotas=$(grep -i "^CHECK_QUOTA " "$config_file" | cut -d ' ' -f 2-)
  if [ "$reqquotas" == "1" ]; then
     check_quotas . $reqmbs "Cwd"
  fi
@@ -103,7 +103,7 @@ fi
 check_df /tmp 10 "Tmp"
 
 # and that I can create a temo dir in it
-tmp_dir=`mktemp -d "/tmp/wmsglide_XXXXXX"`
+tmp_dir=$(mktemp -d "/tmp/wmsglide_XXXXXX")
 if [ $? -ne 0 ]; then
     #echo "Cannot create a dir in /tmp" 1>&2
     STR="Cannot create a dir in /tmp"
