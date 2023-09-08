093cfb5fdc1abeffe9d146321becd497e50583a8
Marco Mascheroni <marco.mascheroni@cern.ch>
2023-09-08 15:49:34 -0700

    Add option to set num_factories in PSG_autoconf

--- factory/tools/OSG_autoconf.py
+++ factory/tools/OSG_autoconf.py
@@ -268,6 +268,8 @@ def get_entries_configuration(data):
                         entry_configuration["submit_attrs"] = get_submit_attr_str(entry_configuration["submit_attrs"])
                     else:
                         entry_configuration["submit_attrs"] = ""
+                    num_factories = entry_configuration.get("num_factories")
+                    entry_configuration["num_factories"] = "" if num_factories==None else f' num_factories="{num_factories}"'
                     entry_configuration["limits"] = get_limits_str(entry_configuration["limits"])
                     entry_configuration["submission_speed"] = get_submission_speed(
                         entry_configuration["submission_speed"]
