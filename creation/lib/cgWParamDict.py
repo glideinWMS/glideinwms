2034908463ccdde7f7cb836cc665f50deb0768fa
Namratha Urs <namrathaurs@my.unt.edu>
2024-07-09 12:34:02 -0500

    advertizing unprivileged user namespaces info
    - added lines of code to the cvmfs helper functions custom script to advertize details of unprivileged user namespaces
    to the Glidein classad
    - updated the approach to check whether unprivileged user namespaces is
      supported from (1) `sysctl` to `cat` command; found that on EL9, GWMS
    complains that `sysctl` command is not found which leads to an erroneous
    output, and (2) used the value instead of the presence of the file;
    using the value is the correct approach as suggested by Dave and Marco
    - removed the conditional download of the cvmfs helper functions custom
      script; better to have it available at all times for use when
    necessary (for instance, to fetch details about unprivileged user
    namespaces on the worker node without enabling the on-demand CVMFS
    provisioning feature)

--- creation/lib/cgWParamDict.py
+++ creation/lib/cgWParamDict.py
@@ -297,7 +297,8 @@ class glideinMainDicts(cgWDictFile.glideinMainDicts):
         self.dicts["file_list"].add_from_file(
             cvmfs_helper,
             cWDictFile.FileDictFile.make_val_tuple(
-                cWConsts.insert_timestr(cvmfs_helper), "exec", cond_download="GLIDEIN_USE_CVMFSEXEC"
+                cWConsts.insert_timestr(cvmfs_helper),
+                "exec",
             ),
             os.path.join(cgWConsts.WEB_BASE_DIR, cvmfs_helper),
         )
@@ -307,7 +308,8 @@ class glideinMainDicts(cgWDictFile.glideinMainDicts):
         self.dicts["file_list"].add_from_file(
             dist_select_script,
             cWDictFile.FileDictFile.make_val_tuple(
-                cWConsts.insert_timestr(dist_select_script), "exec", cond_download="GLIDEIN_USE_CVMFSEXEC"
+                cWConsts.insert_timestr(dist_select_script),
+                "exec",
             ),
             os.path.join(cgWConsts.WEB_BASE_DIR, dist_select_script),
         )
