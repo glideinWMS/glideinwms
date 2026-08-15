glideinWMS/glideinwms

commit 0db1bbb8a6c57d2705b7047d6f23aefdc6fa1d90

Fixed the IdTokenGenerator duration default handling and added PR feedback

frontend/glideinFrontendInterface.py
@@ -30,7 +30,7 @@ from glideinwms.lib.util import hash_nc
 ############################################################
 
 
-# TODO: Verify the difference form glideinFrontendConfig.FrontendConfig and if these can be unified
+# TODO: Verify the difference from glideinFrontendConfig.FrontendConfig and if these can be unified
 class FrontendConfig:
     """Configuration class for the Frontend component of GlideinWMS.
 

plugins/IdTokenGenerator.py
@@ -28,12 +28,13 @@ class IdTokenGenerator(CredentialGenerator):
     HOSTNAME is the fully qualified domain name of the host where the token is generated, and
     NAME is the GLIDEIN_Site attribute form the resource/entry classad and configuration in the Factory, or
     the name of the resource/entry from the classad and configuration in the Factory if GLIDEIN_Site is not specified.
-    This aims to create different subjects for different resources/entries to avoid the reuse of tokes from potentially
+    This aims to create different subjects for different resources/entries to avoid the reuse of tokens from potentially
     compromised resources.
 
     The issuer defaults to the HTCondor TRUST_DOMAIN when not specified in the context.
 
     A duration of 0 seconds means that the token will never expire. A negative value will generate expired tokens.
+    None or a missing value from the context is used to load the duration via the IDTokenLifetime attribute or use the default.
     The default duration (if not in the context or IDTokenLifetime) is 24 hours.
 
     The default password file name is the username (if not in the context or IDTokenKeyname), all uppercase.
@@ -44,10 +45,10 @@ class IdTokenGenerator(CredentialGenerator):
             {
                 "password": (str, ""),
                 "scope": (str, ""),
-                "duration": (int, 0),
+                "duration": ((int, None), None),
                 "minimum_lifetime": (int, 0),
                 "identity": (str, ""),
-                "issuer": (str, None),
+                "issuer": ((str, None), None),
             }
         )
         self.context["type"] = "idtoken"
@@ -71,9 +72,10 @@ class IdTokenGenerator(CredentialGenerator):
 
         scope = self.context["scope"] or "condor:/READ condor:/ADVERTISE_STARTD condor:/ADVERTISE_MASTER"
 
-        duration = (
-            self.context["duration"] or int(kwargs["elementDescript"].merged_data.get("IDTokenLifetime", 24)) * 3600
-        )
+        # Context validation will set duration to None if missing
+        duration = self.context["duration"]
+        if duration is None:
+            duration = int(kwargs["elementDescript"].merged_data.get("IDTokenLifetime", 24)) * 3600
 
         identity = self.context["identity"]
         if not identity:

factory/glideFactoryConfig.py
@@ -30,7 +30,7 @@ from glideinwms.lib import credentials
 ############################################################
 
 
-# TODO: Verify the difference form glideFactoryInterface.FactoryConfig and if these can be unified
+# TODO: Verify the difference from glideFactoryInterface.FactoryConfig and if these can be unified
 class FactoryConfig:
     """Holds default configuration file names for the factory.
 
@@ -225,7 +225,7 @@ class JoinConfigFile(ConfigFile):
 
 
 class GlideinDescript(ConfigFile):
-    """Represents the Glidein(Factory and HTCondor services) description configuration file.
+    """Represents the Glidein (Factory and HTCondor services) description configuration file.
 
     This class loads the glidein.descript file and processes its content,
     including handling of public key type values.

factory/glideFactoryInterface.py
@@ -40,7 +40,7 @@ advertiseGlobalCounter = 0
 #        pass
 
 
-# TODO: Verify the difference form glideFactoryConfig.FactoryConfig and if these can be unified
+# TODO: Verify the difference from glideFactoryConfig.FactoryConfig and if these can be unified
 class FactoryConfig:
     def __init__(self):
         """Initialize a FactoryConfig object with default values.

frontend/glideinFrontendConfig.py
@@ -18,7 +18,7 @@ from glideinwms.lib import hash_crypto, util
 ############################################################
 
 
-# TODO: Verify the difference form glideinFrontendInterface.FrontendConfig and if these can be unified
+# TODO: Verify the difference from glideinFrontendInterface.FrontendConfig and if these can be unified
 class FrontendConfig:
     """Configuration class for the Frontend component of GlideinWMS.
 

frontend/glideinFrontendPlugins.py
@@ -258,7 +258,7 @@ class CredentialsBasic(CredentialsPlugin):
 
 ###########################################################
 ### Proxy plugins are deprecated and should not be used ###
-## You should use plugins inheriting form CredentialsPlugin
+## You should use plugins inheriting from CredentialsPlugin
 ###########################################################
 
 # TODO: Some of the functionalities are no more needed with the new credentials and generators

lib/generators/generators.py
@@ -59,7 +59,7 @@ class GeneratorContext(dict):
 
         Example:
             context.validate({
-                "user": (str),  # Mandatory string with name
+                "user": (str),  # Mandatory string with the user's name
                 "group": (str,),  # Mandatory string with group
                 "permissions": ((list, None), []),  # Optional list with permissions, defaults to empty list if missing
                                                     # If set to None, it will stay None

commit 66a2f77d391c7dfe9af96874bd008eced9f55689

Fixed reference to Entry name in entry dictionary, bug introduced in PR #667
Improved docstring for the creation of the Entry dictionary

frontend/glideinFrontendInterface.py
@@ -308,9 +308,26 @@ def findGlideinClientMonitoring(factory_pool, factory_identity, my_name, additio
     return format_condor_dict(data)
 
 
+# TODO: This function deletes some elements form the input, then it makes a copy possibly not to delete those
+#  elements form the sub-dictionaries. This seems inconsistent. Check if input can be freely modified.
 def format_condor_dict(data):
-    """
-    Formats the data from the condor call.
+    """Formats the data from the condor call in a new dictionary.
+
+    It strips the names in frontendConfig.condor_reserved_names in the base and sub-dictionaries.
+    The stripping from `data` modifies the input parameter itself.
+    Then it splits the ClassAd attributes in 3 sub-dictionaries:
+    - the ones starting with frontendConfig.glidein_param_prefix are in `["params"]`
+    - the ones starting with frontendConfig.glidein_monitor_prefix are in `["monitor"]`
+    - all the others in `["attrs"]
+    The sub-dictionaries returned are a copy of the input sub-dictionaries.
+    Note that it is not a deepcopy because the components are not modified.
+
+    Args:
+        data (dict): The glidefactory ClassAd data returned from condor status
+
+    Returns:
+        dict: A formatted dictionary of the Entry data. It has 3 sub-dictionaries: params, monitor, and attrs.
+
     """
 
     reserved_names = frontendConfig.condor_reserved_names

plugins/IdTokenGenerator.py
@@ -79,11 +79,18 @@ class IdTokenGenerator(CredentialGenerator):
 
         identity = self.context["identity"]
         if not identity:
+            # Prefer optional "GLIDEIN_Site", otherwise use "EntryName" (always there, added from the Entry name)
             try:
                 identity = f"{kwargs['glidein_el']['attrs']['GLIDEIN_Site']}@{socket.gethostname()}"
             except KeyError:
-                # GLIDEIN_Site is not mandatory, the name is
-                identity = f"{kwargs['glidein_el']['name']}@{socket.gethostname()}"
+                try:
+                    # If there is an exception here, this should propagate and crash the execution
+                    identity = f"{kwargs['glidein_el']['attrs']['EntryName']}@{socket.gethostname()}"
+                except Exception as err:
+                    raise RuntimeError(
+                        "Unable to determine Entry identity in IdToken generation due to malformed"
+                        " glidefactory ClassAd or bad generator invocation."
+                    ) from err
 
         minimum_lifetime = self.context["minimum_lifetime"] or 0
 